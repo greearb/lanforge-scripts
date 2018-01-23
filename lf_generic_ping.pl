97adf85048d71f6f2b04e4148a16e53e324c3b1d
Jed Reynolds <jreynolds@candelatech.com>
2018-01-23 15:10:42 -0800

    lf_generic_ping: corrected unlimited process to 65535

--- lf_generic_ping.pl
+++ lf_generic_ping.pl
@@ -78,7 +78,7 @@ $0 --mgr {host-name | IP}
 
  If only a few of your generic commands start, check journalctl for
  errors containing: 'cgroup: fork rejected by pids controller'
- You want to set DefaultTasksMax=unlimited in /etc/systemd/system.conf
+ You want to set DefaultTasksMax=65535 in /etc/systemd/system.conf
  then do a systemctl daemon-restart.
  https://www.novell.com/support/kb/doc.php?id=7018594
 );
