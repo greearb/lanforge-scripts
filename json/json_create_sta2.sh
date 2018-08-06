4b61d079d0568ddea310431b7ad8b1b5cd999fe9
Jed Reynolds <jreynolds@candelatech.com>
2018-08-06 16:06:17 -0700

    updates create station script to create ports in admin-down mode, then bring them up

--- json/json_create_sta2.sh
+++ json/json_create_sta2.sh
@@ -9,54 +9,86 @@ cjson_h="Content-type: application/json"
 R=8
 W=wiphy0
 M=8000
-N=8050
+N=8019
 SSID="idtest-1100-wpa2"
 KEY="idtest-1100-wpa2"
 
+echo -n "Removing: "
 for n in `seq $M $N`; do
+   echo -n "sta$n "
    echo "shelf=1&resource=$R&port=sta$n" > /tmp/curl_data
-   curl -sqv -H "$ajson_h" -X POST -d '@/tmp/curl_data' http://localhost:8080/cli-form/rm_vlan ||:
+   curl -sq -H "$ajson_h" -X POST -d '@/tmp/curl_data' http://localhost:8080/cli-form/rm_vlan ||:
 done
+echo "."
+
+sleep 2
+echo -n "Adding: "
+for n in `seq $M $N`; do
+   echo -n "shelf=1&resource=$R&radio=$W" > /tmp/curl_data
+   echo -n "&sta_name=sta$n" >> /tmp/curl_data
+   echo -n "&flags=68727874560&ssid=idtest-1100-wpa2&key=idtest-1100-wpa2" >> /tmp/curl_data
+   echo -n "&mac=xx:xx:xx:xx:*:xx&mode=0&rate=DEFAULT" >> /tmp/curl_data
+   curl -sq -H "$ajson_h" -X POST -d '@/tmp/curl_data' http://localhost:8080/cli-form/add_sta ||:
+done
+
 sleep 2
 for n in `seq $M $N`; do
-   echo "shelf=1&resource=$R&radio=$W&sta_name=sta$n&flags=0x400&ssid=$SSID&key=$KEY&mac=xx:xx:xx:xx:*:*" > /tmp/curl_data
-   curl -sqv -H 'Accept: application/json' -X POST -d '@/tmp/curl_data' http://localhost:8080/cli-form/add_sta ||:
+   echo -n "shelf=1&resource=$R&port=sta$n" > /tmp/curl_data
+   echo -n '&current_flags=2147483648&interest=16384' >> /tmp/curl_data
+   curl -sq -H "$ajson_h" -X POST -d '@/tmp/curl_data' http://localhost:8080/cli-form/set_port ||:
 done
-sleep 3
+echo "."
+
+sleep 1
+echo -n "Bringing ports up "
 for n in `seq $M $N`; do
-   curl -sqv -H "$ajson_h" -X GET -o /tmp/response "http://localhost:8080/port/1/$R/sta$n"
+   echo -n "shelf=1&resource=$R&port=sta$n" > /tmp/curl_data
+   echo -n '&current_flags=0&interest=8388611' >> /tmp/curl_data
+   curl -sq -H "$ajson_h" -X POST -d '@/tmp/curl_data' http://localhost:8080/cli-form/set_port ||:
+done
+echo "."
+
+sleep 3
+echo -n "Querying ports "
+for n in `seq $M $N`; do
+   curl -sq -H "$ajson_h" -X GET -o /tmp/response "http://localhost:8080/port/1/$R/sta$n"
+   echo -n "."
    #if [ -s /tmp/response ]; then
    #   json_pp < /tmp/response
    #fi
 done
+echo "...done."
 exit
 
+
+
+
 echo 'shelf=1&resource=3&port=sta3100' > /tmp/curl_data
-curl -sqv -H "$ajson_h" -X POST -d '@/tmp/curl_data' http://localhost:8080/cli-form/rm_vlan
+curl -sq -H "$ajson_h" -X POST -d '@/tmp/curl_data' http://localhost:8080/cli-form/rm_vlan
 
 sleep 1
 echo 'shelf=1&resource=3&radio=wiphy1&sta_name=sta3100&flags=1024&ssid=idtest-1100-wpa2&nickname=sta3100&key=idtest-1100-wpa2&mac=XX:XX:XX:XX:*:*&flags_mask=1024' > /tmp/curl_data
-curl -sqv -H "$ajson_h" -X POST -d '@/tmp/curl_data' http://localhost:8080/cli-form/add_sta
+curl -sq -H "$ajson_h" -X POST -d '@/tmp/curl_data' http://localhost:8080/cli-form/add_sta
 
 
 sleep 1
 rm -f /tmp/response
-curl -sqv -H "$ajson_h" -X GET -o /tmp/response http://localhost:8080/port/1/3/sta3100
+curl -sq -H "$ajson_h" -X GET -o /tmp/response http://localhost:8080/port/1/3/sta3100
 if [ -s /tmp/response ]; then
    json_pp < /tmp/response
 fi
 
 sleep 2
 echo '{"shelf":1,"resource":3,"port":"sta3100"}' > /tmp/curl_data
-curl -sqv -H "$ajson_h" -H "$cjson_h" -X POST -d '@/tmp/curl_data' http://localhost:8080/cli-json/rm_vlan
+curl -sq -H "$ajson_h" -H "$cjson_h" -X POST -d '@/tmp/curl_data' http://localhost:8080/cli-json/rm_vlan
 
 sleep 2
 echo '{"shelf":1,"resource":3,"radio":"wiphy1","sta_name":"sta3100","flags":1024,"ssid":idtest-1100-wpa2","nickname":"sta3100","key":"idtest-1100-wpa2","mac":"XX:XX:XX:XX:*:XX","flags_mask":1024}' > /tmp/curl_data
-curl -sqv -H "$ajson_h" -H "$cjson_h" -X POST -d '@/tmp/curl_data' http://localhost:8080/cli-json/add_sta
+curl -sq -H "$ajson_h" -H "$cjson_h" -X POST -d '@/tmp/curl_data' http://localhost:8080/cli-json/add_sta
 
 sleep 1
 rm -f /tmp/response
-curl -sqv -H "$ajson_h" -X GET -o /tmp/response http://localhost:8080/port/1/3/sta3100
+curl -sq -H "$ajson_h" -X GET -o /tmp/response http://localhost:8080/port/1/3/sta3100
 if [ -s /tmp/response ]; then
    json_pp < /tmp/response
 fi
