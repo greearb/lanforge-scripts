cb5d866d24bf9646e07e9b4a6197e96d9371e3dd
Jed Reynolds <jreynolds@candelatech.com>
2019-06-14 08:58:10 -0700

    tested on debbie

--- four_million.sh
+++ four_million.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 export PATH=".:$PATH"
 FM="./lf_firemod.pl"
+mgr=idtest
+rsrc=3
 max=1000
+numvlan=199
 connections=()
 for c in `seq 1 $max`; do
   n=$(( (10 * $max) + $c ))
@@ -11,18 +14,18 @@ done
 index=0
 portnum=0
 for c in "${connections[@]}"; do
-  echo $FM --mgr localhost --resource 1 --action create_endp --endp_name ${c}-A --speed 25000 --endp_type lf_tcp --port_name "eth1#$portnum"
+   echo -n .
+  $FM --mgr $mgr --resource $rsrc --action create_endp --endp_name ${c}-A --speed 25000 --endp_type lf_tcp --port_name "eth1#$portnum"
 
-  echo $FM --mgr localhost --resource 1 --action create_endp --endp_name ${c}-B --speed 25000 --endp_type lf_tcp --port_name "eth2"
+  $FM --mgr $mgr --resource $rsrc --action create_endp --endp_name ${c}-B --speed 25000 --endp_type lf_tcp --port_name "eth2"
   
-  echo $FM --mgr localhost --resource 1 --action create_cx --cx_name ${c}  --endps ${c}-A,${c}-B
-
-  # set report timer
+  $FM --mgr $mgr --resource $rsrc --action create_cx --cx_name ${c} --cx_endps ${c}-A,${c}-B --report_timer 8000
+   echo -n o
 
   index=$((index + 1))
-  portnum=$((index % 100))
+  portnum=$((index % $numvlan))
 done
-echo "done"
+echo " done"
 
 
 #
