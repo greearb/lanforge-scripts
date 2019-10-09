f001c1df41a635abbef54785d18bdf530db4d720
Ben Greear <greearb@candelatech.com>
2019-10-09 07:51:51 -0700

    hostap-timestamp can now parse LANforge log timestamps as well.

--- hostap_timestamp.pl
+++ hostap_timestamp.pl
@@ -1,5 +1,7 @@
 #!/usr/bin/perl
 
+# Convert LANforge logs and hostapd log timestamps to human readable timestamp.
+
 use POSIX qw( strftime );
 
 while (<>) {
@@ -11,6 +13,15 @@ while (<>) {
     my $usec_pad = sprintf("%06d", $usec);
     my $dt = strftime("%Y-%m-%d %H:%M:%S", localtime($time_sec));
     print "$dt.$usec_pad $rest\n";
+  }
+  elsif ($ln =~ /^(\d+): (.*)/) {
+    my $tot_msec = $1;
+    my $rest = $2;
+    my $sec = int($tot_msec / 1000);
+    my $msec = $tot_msec % 1000;
+    my $msec_pad = sprintf("%03d", $msec);
+    my $dt = strftime("%Y-%m-%d %H:%M:%S", localtime($sec));
+    print "$dt.$msec_pad $rest\n";
   }
   else {
     print $ln;
