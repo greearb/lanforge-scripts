5b420a96cff6cbd2c6bf4bdaeec7e050d21cea5c
Logan Lipke <lolipke@gmail.com>
2020-05-21 13:12:34 -0700

    Script for automating GUI update + headless and running basic tests. Sends email with logs

---
+++ updateTest.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+export DISPLAY=:1
+
+GUILog="/home/lanforge/Documents/GUILog.txt"
+GUIUpdate="/home/lanforge/Documents/GUIUpdateLog.txt"
+CTLGUI="/home/lanforge/Documents/connectTestGUILog.txt"
+CTLH="/home/lanforge/Documents/connectTestHLog.txt"
+verNum="5.4.2"
+
+python3 /home/lanforge/Documents/lanforge-scripts/auto-install-gui.py --versionNumber $verNum &> $GUIUpdate
+python3 /home/lanforge/Documents/lanforge-scripts/connectTest.py &> $CTLGUI
+
+pgrep java | xargs kill
+
+/home/lanforge/LANforgeGUI_5.4.2/lfclient.bash -daemon -s localhost &> $GUILog &
+python3 /home/lanforge/Documents/lanforge-scripts/connectTest.py &> $CTLH
+
+echo "Logs Attached" | mail -s 'GUI Update Logs' -a $GUILog -a $GUIUpdate -a $CTLGUI -a $CTLH  test.notice@candelatech.com
