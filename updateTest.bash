515151f80b4fa0866d6310e9aa982a3f382b0fbd
Jed Reynolds <jed@candelatech.com>
2020-05-27 12:47:48 -0700

    updateTest: various improvements

--- updateTest.bash
+++ updateTest.bash
@@ -1,21 +1,29 @@
 #!/bin/bash
 export DISPLAY=:1
 
+HL="/home/lanforge"
+HLD="${HL}/Documents"
+scripts="${HLD}/lanforge-scripts"
 GUILog="/home/lanforge/Documents/GUILog.txt"
 GUIUpdate="/home/lanforge/Documents/GUIUpdateLog.txt"
 CTLGUI="/home/lanforge/Documents/connectTestGUILog.txt"
 CTLH="/home/lanforge/Documents/connectTestHLog.txt"
 verNum="5.4.2"
 
-python3 /home/lanforge/Documents/lanforge-scripts/auto-install-gui.py --versionNumber $verNum &> $GUIUpdate
+for f in $GUILog $GUIUpdate $CTLGUI $CTLH; do rm -f $f; done
 
+python3 ${scripts}/auto-install-gui.py --versionNumber $verNum &> $GUIUpdate
+sleep 5
 grep -q "Current GUI version up to date" $GUIUpdate && exit
 
-python3 /home/lanforge/Documents/lanforge-scripts/connectTest.py &> $CTLGUI
-
+python3 ${scripts}/connectTest.py &> $CTLGUI
+sleep 1
 pgrep java | xargs kill
+sleep 1
 
-/home/lanforge/LANforgeGUI_5.4.2/lfclient.bash -daemon -s localhost &> $GUILog &
-python3 /home/lanforge/Documents/lanforge-scripts/connectTest.py &> $CTLH
+${HL}/LANforgeGUI_5.4.2/lfclient.bash -daemon -s localhost &> $GUILog &
+sleep 5
+python3 ${scripts}/connectTest.py &> $CTLH
+sleep 1
 
-echo "Logs Attached" | mail -s 'GUI Update Logs' -a $GUILog -a $GUIUpdate -a $CTLGUI -a $CTLH  test.notice@candelatech.com
+echo "Logs Attached" | mail -s 'GUI Update Logs' -a $GUILog -a $GUIUpdate -a $CTLGUI -a $CTLH  "test.notice@candelatech.com"
