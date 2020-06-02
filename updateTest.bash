0275d18464aaf90683fe21eed77cbbb91b00a316
Logan Lipke <lolipke@gmail.com>
2020-06-02 14:41:20 -0700

    Changed script to help prevent multiple guis from running at once

--- updateTest.bash
+++ updateTest.bash
@@ -12,19 +12,27 @@ verNum="5.4.2"
 
 for f in $GUILog $GUIUpdate $CTLGUI $CTLH; do rm -f $f; done
 
+touch "${HL}/LANforgeGUI_${verNum}/NO_AUTOSTART"
+killall -9 java
 python3 ${scripts}/auto-install-gui.py --versionNumber $verNum &> $GUIUpdate
 sleep 5
 grep -q "Current GUI version up to date" $GUIUpdate && exit
 grep -q -i "fail" $GUIUpdate && exit
+rm "${HL}/LANforgeGUI_${verNum}/NO_AUTOSTART"
 
 python3 ${scripts}/connectTest.py &> $CTLGUI
 sleep 1
 killall -9 java
 sleep 1
 
-${HL}/LANforgeGUI_5.4.2/lfclient.bash -daemon -s localhost &> $GUILog &
+touch "${HL}/LANforgeGUI_${verNum}/DAEMON_MODE"
+killall -9 java
+#${HL}/LANforgeGUI_5.4.2/lfclient.bash -daemon -s localhost &> $GUILog &
 sleep 5
 python3 ${scripts}/connectTest.py &> $CTLH
 sleep 1
+rm "${HL}/LANforgeGUI_${verNum}/DAEMON_MODE"
 
-echo "Logs Attached" | mail -s 'GUI Update Logs' -a $GUILog -a $GUIUpdate -a $CTLGUI -a $CTLH  "test.notice@candelatech.com"
+
+
+echo "Logs Attached" | mail -s 'GUI Update Logs' -a $GUIUpdate -a $CTLGUI -a $CTLH  "test.notice@candelatech.com"
