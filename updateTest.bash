8e2771e4f2bfb409d58b067d6746a4548fa848a6
Jed Reynolds <jreynolds@candelatech.com>
2020-06-02 17:02:21 -0700

    updateTest: improvements, also trying to summarize run into email

--- updateTest.bash
+++ updateTest.bash
@@ -9,32 +9,41 @@ GUIUpdate="/home/lanforge/Documents/GUIUpdateLog.txt"
 CTLGUI="/home/lanforge/Documents/connectTestGUILog.txt"
 CTLH="/home/lanforge/Documents/connectTestHLog.txt"
 verNum="5.4.2"
+GUIDIR="${HL}/LANforgeGUI_${verNum}"
+ST="/tmp/summary.txt"
 
-for f in $GUILog $GUIUpdate $CTLGUI $CTLH; do rm -f $f; done
+rm -f $GUILog $GUIUpdate $CTLGUI $CTLH $ST
 
 touch "${HL}/LANforgeGUI_${verNum}/NO_AUTOSTART"
-killall -9 java
 python3 ${scripts}/auto-install-gui.py --versionNumber $verNum &> $GUIUpdate
 sleep 5
 grep -q "Current GUI version up to date" $GUIUpdate && exit
 grep -q -i "fail" $GUIUpdate && exit
+
+pgrep java &>/dev/null && killall -9 java
 rm "${HL}/LANforgeGUI_${verNum}/NO_AUTOSTART"
 
-nohup env RESTARTS=999999 ./lfclient.bash -s localhost &> $GUILog
-
+( cd $GUIDIR; nohup env RESTARTS=999999 ./lfclient.bash -s localhost &> $GUILog & )
+sleep 10
 python3 ${scripts}/connectTest.py &> $CTLGUI
-sleep 1
-killall -9 java
-sleep 1
 
 touch "${HL}/LANforgeGUI_${verNum}/DAEMON_MODE"
-killall -9 java
-#${HL}/LANforgeGUI_5.4.2/lfclient.bash -daemon -s localhost &> $GUILog &
-sleep 5
+pgrep java &>/dev/null && killall -9 java
+sleep 10
 python3 ${scripts}/connectTest.py &> $CTLH
-sleep 1
 rm "${HL}/LANforgeGUI_${verNum}/DAEMON_MODE"
 
+pgrep java &>/dev/null && killall -9 java
 
+echo "===============================================" > $ST
+head $GUILog >> $ST
+echo "===============================================" >> $ST
+head $GUIUpdate >> $ST
+echo "===============================================" >> $ST
+head $CTLGUI >> $ST
+echo "===============================================" >> $ST
+head $CTLH >> $ST
+echo "===============================================" >> $ST
 
-echo "Logs Attached" | mail -s 'GUI Update Logs' -a $GUILog -a $GUIUpdate -a $CTLGUI -a $CTLH  "test.notice@candelatech.com"
+cat $ST | mail -s 'GUI Update Test' -a $GUILog -a $GUIUpdate -a $CTLGUI -a $CTLH  "test.notice@candelatech.com"
+
