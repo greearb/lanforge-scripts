1592f768e0be6b673507c8cd6abc1b72ffc85b63
Logan Lipke <lolipke@gmail.com>
2020-11-13 15:48:18 -0800

    Fixed to work with updates to lfcli base

--- py-scripts/test_ipv4_variable_time.py
+++ py-scripts/test_ipv4_variable_time.py
@@ -174,9 +174,6 @@ test_ipv4_variable_time.py:
 --------------------
 Generic command layout:
 
-Note:   multiple --radio switches may be entered up to the number of radios available:
-                 --radio <radio 0> <stations> <ssid> <ssid password>  --radio <radio 01> <number of last station> <ssid> <ssid password>
-
  python3 ./test_ipv4_variable_time.py 
             --upstream_port eth1 
             --radio wiphy0 
@@ -197,11 +194,10 @@ Note:   multiple --radio switches may be entered up to the number of radios avai
 
     num_sta = 2
     if (args.num_stations is not None) and (int(args.num_stations) > 0):
-        num_stations_converted = int(args.num_stations)
-        num_sta = num_stations_converted
+        num_sta = int(args.num_stations)
 
     station_list = LFUtils.portNameSeries(prefix_="sta", start_id_=0, end_id_=num_sta-1, padding_number_=10000, radio=args.radio)
-
+    print(station_list)
     ip_var_test = IPV4VariableTime(args.mgr, lfjson_port, number_template="00", sta_list=station_list,
                                    name_prefix="VT",
                                    upstream=args.upstream_port,
