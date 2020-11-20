a66a0ac2a9b96f766f497407c4d15f8064899d5d
Syama <syama.devi@connectus.ai>
2020-11-20 11:25:40 -0500

    cicd changes with latest new files, ultiple client connectivity with WPA, WPA2, EAP auth across NAT and bridge mode with vlan
    Signed-off-by: Syama <syama.devi@connectus.ai>

---
+++ py-scripts/tip-cicd-sanity/cloudsdk.py
@@ -0,0 +1,152 @@
+#!/usr/bin/python3
+
+import base64
+import urllib.request
+from bs4 import BeautifulSoup
+import ssl
+import subprocess, os
+from artifactory import ArtifactoryPath
+import tarfile
+import paramiko
+from paramiko import SSHClient
+from scp import SCPClient
+import os
+import pexpect
+from pexpect import pxssh
+import sys
+import paramiko
+from scp import SCPClient
+import pprint
+from pprint import pprint
+from os import listdir
+import re
+import requests
+import json
+import testrail_api
+import logging
+import datetime
+import time
+
+user=os.getenv('CLOUDSDK_USER')
+password=os.getenv('CLOUDSDK_PWD')
+
+###Class for CloudSDK Interaction via RestAPI
+class CloudSDK:
+    def __init__(self):
+        self.user = user
+
+    def get_bearer(cloudSDK_url):
+        cloud_login_url = cloudSDK_url+"/management/v1/oauth2/token"
+        payload = '''
+        {
+        "userId": "'''+user+'''",
+        "password": "'''+password+'''"
+        }   
+        '''
+        headers = {
+            'Content-Type': 'application/json'
+        }
+        try:
+            token_response = requests.request("POST", cloud_login_url, headers=headers, data=payload)
+        except requests.exceptions.RequestException as e:
+            raise SystemExit("Exiting Script! Cloud not get bearer token for reason:",e)
+        token_data = token_response.json()
+        bearer_token = token_data['access_token']
+        return(bearer_token)
+
+    def ap_firmware(customer_id,equipment_id, cloudSDK_url, bearer):
+        equip_fw_url = cloudSDK_url+"/portal/status/forEquipment?customerId="+customer_id+"&equipmentId="+equipment_id
+        payload = {}
+        headers = {
+            'Authorization': 'Bearer ' + bearer
+        }
+        status_response = requests.request("GET", equip_fw_url, headers=headers, data=payload)
+        status_code = status_response.status_code
+        if status_code is 200:
+            status_data = status_response.json()
+            #print(status_data)
+            current_ap_fw = status_data[2]['details']['reportedSwVersion']
+            return current_ap_fw
+        else:
+            return("ERROR")
+
+    def CloudSDK_images(apModel, cloudSDK_url, bearer):
+        getFW_url = cloudSDK_url+"/portal/firmware/version/byEquipmentType?equipmentType=AP&modelId=" + apModel
+        payload = {}
+        headers = {
+            'Authorization': 'Bearer ' + bearer
+        }
+        response = requests.request("GET", getFW_url, headers=headers, data=payload)
+        ap_fw_details = response.json()
+        ###return ap_fw_details
+        fwlist = []
+        for version in ap_fw_details:
+           fwlist.append(version.get('versionName'))
+        return(fwlist)
+        #fw_versionNames = ap_fw_details[0]['versionName']
+        #return fw_versionNames
+
+    def firwmare_upload(commit, apModel,latest_image,fw_url,cloudSDK_url,bearer):
+        fw_upload_url = cloudSDK_url+"/portal/firmware/version"
+        payload = "{\n  \"model_type\": \"FirmwareVersion\",\n  \"id\": 0,\n  \"equipmentType\": \"AP\",\n  \"modelId\": \""+apModel+"\",\n  \"versionName\": \""+latest_image+"\",\n  \"description\": \"\",\n  \"filename\": \""+fw_url+"\",\n  \"commit\": \""+commit+"\",\n  \"validationMethod\": \"MD5_CHECKSUM\",\n  \"validationCode\": \"19494befa87eb6bb90a64fd515634263\",\n  \"releaseDate\": 1596192028877,\n  \"createdTimestamp\": 0,\n  \"lastModifiedTimestamp\": 0\n}\n\n"
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + bearer
+        }
+
+        response = requests.request("POST", fw_upload_url, headers=headers, data=payload)
+        #print(response)
+        upload_result = response.json()
+        return(upload_result)
+
+    def get_firmware_id(latest_ap_image, cloudSDK_url, bearer):
+        #print(latest_ap_image)
+        fw_id_url = cloudSDK_url+"/portal/firmware/version/byName?firmwareVersionName="+latest_ap_image
+
+        payload = {}
+        headers = {
+            'Authorization': 'Bearer ' + bearer
+        }
+        response = requests.request("GET", fw_id_url, headers=headers, data=payload)
+        fw_data = response.json()
+        latest_fw_id = fw_data['id']
+        return latest_fw_id
+
+    def update_firmware(equipment_id, latest_firmware_id, cloudSDK_url, bearer):
+        url = cloudSDK_url+"/portal/equipmentGateway/requestFirmwareUpdate?equipmentId="+equipment_id+"&firmwareVersionId="+latest_firmware_id
+
+        payload = {}
+        headers = {
+            'Authorization': 'Bearer ' + bearer
+        }
+
+        response = requests.request("POST", url, headers=headers, data=payload)
+        #print(response.text)
+        return response.json()
+
+    def set_ap_profile(equipment_id, test_profile_id, cloudSDK_url, bearer):
+        ###Get AP Info
+        url = cloudSDK_url+"/portal/equipment?equipmentId="+equipment_id
+        payload = {}
+        headers = {
+            'Authorization': 'Bearer ' + bearer
+        }
+
+        response = requests.request("GET", url, headers=headers, data=payload)
+        print(response)
+
+        ###Add Lab Profile ID to Equipment
+        equipment_info = response.json()
+        #print(equipment_info)
+        equipment_info["profileId"] = test_profile_id
+        #print(equipment_info)
+
+        ###Update AP Info with Required Profile ID
+        url = cloudSDK_url+"/portal/equipment"
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + bearer
+        }
+
+        response = requests.request("PUT", url, headers=headers, data=json.dumps(equipment_info))
+        #print(response)
---
+++ py-scripts/tip-cicd-sanity/lab_ap_info.py
@@ -0,0 +1,248 @@
+#!/usr/bin/python3
+
+ap_models = ["ec420","ea8300","ecw5211","ecw5410"]
+
+##AP Models for firmware upload
+cloud_sdk_models = {
+    "ec420": "EC420-G1",
+    "ea8300": "EA8300-CA",
+    "ecw5211": "ECW5211",
+    "ecw5410": "ECW5410"
+}
+
+mimo_5g = {
+    "ec420": "4x4",
+    "ea8300": "2x2",
+    "ecw5211": "2x2",
+    "ecw5410": "4x4"
+}
+
+mimo_2dot4g = {
+    "ec420": "2x2",
+    "ea8300": "2x2",
+    "ecw5211": "2x2",
+    "ecw5410": "4x4"
+}
+
+sanity_status = {
+    "ea8300": "failed",
+    "ecw5211": 'passed',
+    "ecw5410": 'failed',
+    "ec420": 'failed'
+}
+
+##Equipment IDs for Lab APs under test
+equipment_id_dict = {
+    "ea8300": "13",
+    "ecw5410": "12",
+    "ecw5211": "6",
+    "ec420": "11"
+}
+
+equipment_ip_dict = {
+    "ea8300": "10.10.10.103",
+    "ecw5410": "10.10.10.105",
+    "ec420": "10.10.10.104",
+    "ecw5211": "10.10.10.102"
+}
+
+eqiupment_credentials_dict = {
+    "ea8300": "openwifi",
+    "ecw5410": "openwifi",
+    "ec420": "openwifi",
+    "ecw5211": "admin123"
+}
+###Testing AP Profile Information
+profile_info_dict = {
+    "ecw5410": {
+        "profile_id": "2",
+        "fiveG_WPA2_SSID": "ECW5410_5G_WPA2",
+        "fiveG_WPA2_PSK": "Connectus123$",
+        "fiveG_WPA_SSID": "ECW5410_5G_WPA",
+        "fiveG_WPA_PSK": "Connectus123$",
+        "fiveG_OPEN_SSID": "ECW5410_5G_OPEN",
+        "fiveG_WPA2-EAP_SSID": "ECW5410_5G_WPA2-EAP",
+        "twoFourG_OPEN_SSID": "ECW5410_2dot4G_OPEN",
+        "twoFourG_WPA2_SSID": "ECW5410_2dot4G_WPA2",
+        "twoFourG_WPA2_PSK": "Connectus123$",
+        "twoFourG_WPA_SSID":"ECW5410_2dot4G_WPA",
+        "twoFourG_WPA_PSK": "Connectus123$",
+        "twoFourG_WPA2-EAP_SSID": "ECW5410_2dot4G_WPA2-EAP"
+        },
+
+    "ea8300": {
+        "profile_id": "153",
+        "fiveG_WPA2_SSID": "EA8300_5G_WPA2",
+        "fiveG_WPA2_PSK": "Connectus123$",
+        "fiveG_WPA_SSID": "EA8300_5G_WPA",
+        "fiveG_WPA_PSK": "Connectus123$",
+        "fiveG_OPEN_SSID": "EA8300_5G_OPEN",
+        "fiveG_WPA2-EAP_SSID": "EA8300_5G_WPA2-EAP",
+        "twoFourG_OPEN_SSID": "EA8300_2dot4G_OPEN",
+        "twoFourG_WPA2_SSID": "EA8300_2dot4G_WPA2",
+        "twoFourG_WPA2_PSK": "Connectus123$",
+        "twoFourG_WPA_SSID":"EA8300_2dot4G_WPA",
+        "twoFourG_WPA_PSK": "Connectus123$",
+        "twoFourG_WPA2-EAP_SSID": "EA8300_2dot4G_WPA2-EAP"
+        },
+
+    "ec420": {
+        "profile_id": "20",
+        "fiveG_WPA2_SSID": "EC420_5G_WPA2",
+        "fiveG_WPA2_PSK": "Connectus123$",
+        "fiveG_WPA_SSID": "EC420_5G_WPA",
+        "fiveG_WPA_PSK": "Connectus123$",
+        "fiveG_OPEN_SSID": "EC420_5G_OPEN",
+        "fiveG_WPA2-EAP_SSID": "EC420_5G_WPA2-EAP",
+        "twoFourG_OPEN_SSID": "EC420_2dot4G_OPEN",
+        "twoFourG_WPA2_SSID": "EC420_2dot4G_WPA2",
+        "twoFourG_WPA2_PSK": "Connectus123$",
+        "twoFourG_WPA_SSID":"EC420_2dot4G_WPA",
+        "twoFourG_WPA_PSK": "Connectus123$",
+        "twoFourG_WPA2-EAP_SSID": "EC420_2dot4G_WPA2-EAP"
+        },
+
+    "ecw5211": {
+        "profile_id": "27",
+        "fiveG_WPA2_SSID": "ECW5211_5G_WPA2",
+        "fiveG_WPA2_PSK": "Connectus123$",
+        "fiveG_WPA_SSID": "ECW5211_5G_WPA",
+        "fiveG_WPA_PSK": "Connectus123$",
+        "fiveG_OPEN_SSID": "ECW5211_5G_OPEN",
+        "fiveG_WPA2-EAP_SSID": "ECW5211_5G_WPA2-EAP",
+        "twoFourG_OPEN_SSID": "ECW5211_2dot4G_OPEN",
+        "twoFourG_WPA2_SSID": "ECW5211_2dot4G_WPA2",
+        "twoFourG_WPA2_PSK": "Connectus123$",
+        "twoFourG_WPA_SSID":"ECW5211_2dot4G_WPA",
+        "twoFourG_WPA_PSK": "Connectus123$",
+        "twoFourG_WPA2-EAP_SSID": "ECW5211_2dot4G_WPA2-EAP"
+        },
+    
+    "ecw5410_nat": {
+        "profile_id": "68",
+        "fiveG_WPA2_SSID": "ECW5410_5G_WPA2_NAT",
+        "fiveG_WPA2_PSK": "Connectus123$",
+        "fiveG_WPA_SSID": "ECW5410_5G_WPA_NAT",
+        "fiveG_WPA_PSK": "Connectus123$",
+        "fiveG_OPEN_SSID": "ECW5410_5G_OPEN_NAT",
+        "fiveG_WPA2-EAP_SSID": "ECW5410_5G_WPA2-EAP_NAT",
+        "twoFourG_OPEN_SSID": "ECW5410_2dot4G_OPEN_NAT",
+        "twoFourG_WPA2_SSID": "ECW5410_2dot4G_WPA2_NAT",
+        "twoFourG_WPA2_PSK": "Connectus123$",
+        "twoFourG_WPA_SSID":"ECW5410_2dot4G_WPA_NAT",
+        "twoFourG_WPA_PSK": "Connectus123$",
+        "twoFourG_WPA2-EAP_SSID": "ECW5410_2dot4G_WPA2-EAP_NAT"
+        },
+
+    "ea8300_nat": {
+        "profile_id": "67",
+        "fiveG_WPA2_SSID": "EA8300_5G_WPA2_NAT",
+        "fiveG_WPA2_PSK": "Connectus123$",
+        "fiveG_WPA_SSID": "EA8300_5G_WPA_NAT",
+        "fiveG_WPA_PSK": "Connectus123$",
+        "fiveG_OPEN_SSID": "EA8300_5G_OPEN_NAT",
+        "fiveG_WPA2-EAP_SSID": "EA8300_5G_WPA2-EAP_NAT",
+        "twoFourG_OPEN_SSID": "EA8300_2dot4G_OPEN_NAT",
+        "twoFourG_WPA2_SSID": "EA8300_2dot4G_WPA2_NAT",
+        "twoFourG_WPA2_PSK": "Connectus123$",
+        "twoFourG_WPA_SSID":"EA8300_2dot4G_WPA_NAT",
+        "twoFourG_WPA_PSK": "Connectus123$",
+        "twoFourG_WPA2-EAP_SSID": "EA8300_2dot4G_WPA2-EAP_NAT"
+        },
+
+    "ec420_nat": {
+        "profile_id": "70",
+        "fiveG_WPA2_SSID": "EC420_5G_WPA2_NAT",
+        "fiveG_WPA2_PSK": "Connectus123$",
+        "fiveG_WPA_SSID": "EC420_5G_WPA_NAT",
+        "fiveG_WPA_PSK": "Connectus123$",
+        "fiveG_OPEN_SSID": "EC420_5G_OPEN_NAT",
+        "fiveG_WPA2-EAP_SSID": "EC420_5G_WPA2-EAP_NAT",
+        "twoFourG_OPEN_SSID": "EC420_2dot4G_OPEN_NAT",
+        "twoFourG_WPA2_SSID": "EC420_2dot4G_WPA2_NAT",
+        "twoFourG_WPA2_PSK": "Connectus123$",
+        "twoFourG_WPA_SSID":"EC420_2dot4G_WPA_NAT",
+        "twoFourG_WPA_PSK": "Connectus123$",
+        "twoFourG_WPA2-EAP_SSID": "EC420_2dot4G_WPA2-EAP_NAT"
+        },
+
+    "ecw5211_nat": {
+        "profile_id": "69",
+        "fiveG_WPA2_SSID": "ECW5211_5G_WPA2_NAT",
+        "fiveG_WPA2_PSK": "Connectus123$",
+        "fiveG_WPA_SSID": "ECW5211_5G_WPA_NAT",
+        "fiveG_WPA_PSK": "Connectus123$",
+        "fiveG_OPEN_SSID": "ECW5211_5G_OPEN_NAT",
+        "fiveG_WPA2-EAP_SSID": "ECW5211_5G_WPA2-EAP_NAT",
+        "twoFourG_OPEN_SSID": "ECW5211_2dot4G_OPEN_NAT",
+        "twoFourG_WPA2_SSID": "ECW5211_2dot4G_WPA2_NAT",
+        "twoFourG_WPA2_PSK": "Connectus123$",
+        "twoFourG_WPA_SSID":"ECW5211_2dot4G_WPA_NAT",
+        "twoFourG_WPA_PSK": "Connectus123$",
+        "twoFourG_WPA2-EAP_SSID": "ECW5211_2dot4G_WPA2-EAP_NAT"
+        },
+
+    "ecw5410_vlan": {
+        "profile_id": "338",
+        "fiveG_WPA2_SSID": "ECW5410_5G_WPA2_VLAN",
+        "fiveG_WPA2_PSK": "Connectus123$",
+        "fiveG_WPA_SSID": "ECW5410_5G_WPA_VLAN",
+        "fiveG_WPA_PSK": "Connectus123$",
+        "fiveG_OPEN_SSID": "ECW5410_5G_OPEN_VLAN",
+        "fiveG_WPA2-EAP_SSID": "ECW5410_5G_WPA2-EAP_VLAN",
+        "twoFourG_OPEN_SSID": "ECW5410_2dot4G_OPEN_VLAN",
+        "twoFourG_WPA2_SSID": "ECW5410_2dot4G_WPA2_VLAN",
+        "twoFourG_WPA2_PSK": "Connectus123$",
+        "twoFourG_WPA_SSID":"ECW5410_2dot4G_WPA_VLAN",
+        "twoFourG_WPA_PSK": "Connectus123$",
+        "twoFourG_WPA2-EAP_SSID": "ECW5410_2dot4G_WPA2-EAP_VLAN"
+        },
+
+    "ea8300_vlan": {
+        "profile_id": "319",
+        "fiveG_WPA2_SSID": "EA8300_5G_WPA2_VLAN",
+        "fiveG_WPA2_PSK": "Connectus123$",
+        "fiveG_WPA_SSID": "EA8300_5G_WPA_VLAN",
+        "fiveG_WPA_PSK": "Connectus123$",
+        "fiveG_OPEN_SSID": "EA8300_5G_OPEN_VLAN",
+        "fiveG_WPA2-EAP_SSID": "EA8300_5G_WPA2-EAP_VLAN",
+        "twoFourG_OPEN_SSID": "EA8300_2dot4G_OPEN_VLAN",
+        "twoFourG_WPA2_SSID": "EA8300_2dot4G_WPA2_VLAN",
+        "twoFourG_WPA2_PSK": "Connectus123$",
+        "twoFourG_WPA_SSID":"EA8300_2dot4G_WPA_VLAN",
+        "twoFourG_WPA_PSK": "Connectus123$",
+        "twoFourG_WPA2-EAP_SSID": "EA8300_2dot4G_WPA2-EAP_VLAN"
+        },
+
+    "ec420_vlan": {
+        "profile_id": "357",
+        "fiveG_WPA2_SSID": "EC420_5G_WPA2_VLAN",
+        "fiveG_WPA2_PSK": "Connectus123$",
+        "fiveG_WPA_SSID": "EC420_5G_WPA_VLAN",
+        "fiveG_WPA_PSK": "Connectus123$",
+        "fiveG_OPEN_SSID": "EC420_5G_OPEN_VLAN",
+        "fiveG_WPA2-EAP_SSID": "EC420_5G_WPA2-EAP_VLAN",
+        "twoFourG_OPEN_SSID": "EC420_2dot4G_OPEN_VLAN",
+        "twoFourG_WPA2_SSID": "EC420_2dot4G_WPA2_VLAN",
+        "twoFourG_WPA2_PSK": "Connectus123$",
+        "twoFourG_WPA_SSID":"EC420_2dot4G_WPA_VLAN",
+        "twoFourG_WPA_PSK": "Connectus123$",
+        "twoFourG_WPA2-EAP_SSID": "EC420_2dot4G_WPA2-EAP_VLAN"
+        },
+
+    "ecw5211_vlan": {
+        "profile_id": "364",
+        "fiveG_WPA2_SSID": "ECW5211_5G_WPA2_VLAN",
+        "fiveG_WPA2_PSK": "Connectus123$",
+        "fiveG_WPA_SSID": "ECW5211_5G_WPA_VLAN",
+        "fiveG_WPA_PSK": "Connectus123$",
+        "fiveG_OPEN_SSID": "ECW5211_5G_OPEN_VLAN",
+        "fiveG_WPA2-EAP_SSID": "ECW5211_5G_WPA2-EAP_VLAN",
+        "twoFourG_OPEN_SSID": "ECW5211_2dot4G_OPEN_VLAN",
+        "twoFourG_WPA2_SSID": "ECW5211_2dot4G_WPA2_VLAN",
+        "twoFourG_WPA2_PSK": "Connectus123$",
+        "twoFourG_WPA_SSID":"ECW5211_2dot4G_WPA_VLAN",
+        "twoFourG_WPA_PSK": "Connectus123$",
+        "twoFourG_WPA2-EAP_SSID": "ECW5211_2dot4G_WPA2-EAP_VLAN"
+        }
+}
