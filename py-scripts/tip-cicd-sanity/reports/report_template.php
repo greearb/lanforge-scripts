7d94d974f9fe3a43190195e38f76e1852d0a37f0
bealler <alex.beall@netexperience.com>
2020-12-04 13:15:05 -0500

    Add test category column to report template

--- py-scripts/tip-cicd-sanity/reports/report_template.php
+++ py-scripts/tip-cicd-sanity/reports/report_template.php
@@ -44,22 +44,21 @@ $json = json_decode($results, true);
     <h1 style="color:green; font-size:40px; text-align: center">CICD Nightly Sanity Report - <?php echo basename(dirname(__FILE__)) ?></h1>
 </header>    
     
-<TABLE BORDER="1" WIDTH="1400px" CELLPADDING="4" CELLSPACING="3" Style="background-color: lightgray; font-size:16px">
+<TABLE BORDER="1" WIDTH="100%" CELLPADDING="4" CELLSPACING="3" Style="background-color: lightgray; font-size:16px">
    <TR>
-      <TH COLSPAN="6"><BR><H3>Test Results</H3>
+      <TH COLSPAN="7"><BR><H3>Test Results</H3>
       </TH>
    </TR>
    <TR>
-      <TH>Test Case</TH>
-      <TH>Description</TH>
-      <TH WIDTH="150px">EA8300 Result</TH>
+       <TH COLSPAN="2" ROWSPAN="7"></TH>
+       <TH></TH>
+       <TH WIDTH="150px">EA8300 Result</TH>
        <TH WIDTH="150px">ECW5211 Result</TH>
        <TH WIDTH="150px">ECW5410 Result</TH>
        <TH WIDTH="150px">EC420 Result</TH>
    </TR>
     
     <TR ALIGN="CENTER">
-       <TD></TD>
        <TD ALIGN="LEFT" style="font-weight:bold">New FW Available</TD>
        <TD style="font-weight:bold"><?php echo print_r($json['fw_available']['ea8300'],true) ?></TD>
        <TD style="font-weight:bold"><?php echo print_r($json['fw_available']['ecw5211'],true) ?></TD>
@@ -68,7 +67,6 @@ $json = json_decode($results, true);
    </TR>
        
        <TR ALIGN="CENTER" style="font-weight:bold">
-        <TD></TD>
         <TD ALIGN="LEFT" >FW Under Test</TD>
         <TD style="font-size:12px"><?php echo print_r($json['fw_under_test']['ea8300'],true) ?></TD>
         <TD style="font-size:12px"><?php echo print_r($json['fw_under_test']['ecw5211'],true) ?></TD>
@@ -77,7 +75,6 @@ $json = json_decode($results, true);
    </TR>
 
         <TR ALIGN="CENTER" style="font-weight:bold">
-        <TD></TD>
         <TD ALIGN="LEFT" >CloudSDK Commit Date</TD>
         <TD style="font-size:12px"><?php echo print_r($json['cloud_sdk']['ea8300']['date'],true) ?></TD>
         <TD style="font-size:12px"><?php echo print_r($json['cloud_sdk']['ecw5211']['date'],true) ?></TD>
@@ -86,7 +83,6 @@ $json = json_decode($results, true);
    </TR>
 
         <TR ALIGN="CENTER" style="font-weight:bold">
-        <TD></TD>
         <TD ALIGN="LEFT" >CloudSDK Commit ID</TD>
         <TD style="font-size:10px"><?php echo print_r($json['cloud_sdk']['ea8300']['commitId'],true) ?></TD>
         <TD style="font-size:10px"><?php echo print_r($json['cloud_sdk']['ecw5211']['commitId'],true) ?></TD>
@@ -95,7 +91,6 @@ $json = json_decode($results, true);
    </TR>
 
         <TR ALIGN="CENTER" style="font-weight:bold">
-        <TD></TD>
         <TD ALIGN="LEFT" >CloudSDK Project Version</TD>
         <TD style="font-size:12px"><?php echo print_r($json['cloud_sdk']['ea8300']['projectVersion'],true) ?></TD>
         <TD style="font-size:12px"><?php echo print_r($json['cloud_sdk']['ecw5211']['projectVersion'],true) ?></TD>
@@ -104,7 +99,6 @@ $json = json_decode($results, true);
    </TR>
        
        <TR ALIGN="CENTER" style="font-weight:bold">
-        <TD></TD>
         <TD ALIGN="LEFT">Test Pass Rate</TD>
         <TD style="font-size:14px"><?php echo print_r($json['pass_percent']['ea8300'],true) ?></TD>
         <TD style="font-size:14px"><?php echo print_r($json['pass_percent']['ecw5211'],true) ?></TD>
@@ -113,7 +107,17 @@ $json = json_decode($results, true);
    </TR>
 
       <TR ALIGN="CENTER">
+      <TH>Test Case</TH>
+      <TH WIDTH= 7%">Category</TH>
+      <TH>Description</TH>
+      <TH></TH>
+      <TH></TH>
+      <TH></TH>
+      <TH></TH>
+
+      <TR ALIGN="CENTER">
       <TD>5540</TD>
+      <TD>CloudSDK</TD>
       <TD ALIGN="LEFT">Get CloudSDK Version with API</TD>
       <TD><?php echo print_r($json['tests']['ea8300']['5540'],true) ?></TD>
        <TD><?php echo print_r($json['tests']['ecw5211']['5540'],true) ?></TD>
@@ -123,6 +127,7 @@ $json = json_decode($results, true);
 
     <TR ALIGN="CENTER">
       <TD>5548</TD>
+      <TD>CloudSDK</TD>
       <TD ALIGN="LEFT">Create FW version on CloudSDK using API</TD>
       <TD><?php echo print_r($json['tests']['ea8300']['5548'],true) ?></TD>
        <TD><?php echo print_r($json['tests']['ecw5211']['5548'],true) ?></TD>
@@ -132,6 +137,7 @@ $json = json_decode($results, true);
 
        <TR ALIGN="CENTER">
       <TD>5547</TD>
+      <TD>CloudSDK</TD>
       <TD ALIGN="LEFT">Request AP Upgrade using API</TD>
       <TD><?php echo print_r($json['tests']['ea8300']['5547'],true) ?></TD>
        <TD><?php echo print_r($json['tests']['ecw5211']['5547'],true) ?></TD>
@@ -141,6 +147,7 @@ $json = json_decode($results, true);
 
     <TR ALIGN="CENTER">
       <TD>2233</TD>
+      <TD>AP</TD>
       <TD ALIGN="LEFT">AP Upgrade Successful</TD>
       <TD><?php echo print_r($json['tests']['ea8300']['2233'],true) ?></TD>
        <TD><?php echo print_r($json['tests']['ecw5211']['2233'],true) ?></TD>
@@ -150,6 +157,7 @@ $json = json_decode($results, true);
        
     <TR ALIGN="CENTER">
       <TD>5247</TD>
+      <TD>CloudSDK</TD>
       <TD ALIGN="LEFT">CloudSDK Reports Correct FW</TD>
        <TD><?php echo print_r($json['tests']['ea8300']['5247'],true) ?></TD>
        <TD><?php echo print_r($json['tests']['ecw5211']['5247'],true) ?></TD>
@@ -159,6 +167,7 @@ $json = json_decode($results, true);
        
     <TR ALIGN="CENTER">
       <TD>5222</TD>
+      <TD>CloudSDK</TD>
       <TD ALIGN="LEFT">AP-CloudSDK Connection Active </TD>
       <TD><?php echo print_r($json['tests']['ea8300']['5222'],true) ?></TD>
         <TD><?php echo print_r($json['tests']['ecw5211']['5222'],true) ?></TD>
@@ -168,6 +177,7 @@ $json = json_decode($results, true);
 
        <TR ALIGN="CENTER">
       <TD>5541</TD>
+      <TD>CloudSDK</TD>
       <TD ALIGN="LEFT">CloudSDK Pushes Correct AP Profile - Bridge Mode </TD>
       <TD><?php echo print_r($json['tests']['ea8300']['5541'],true) ?></TD>
         <TD><?php echo print_r($json['tests']['ecw5211']['5541'],true) ?></TD>
@@ -177,6 +187,7 @@ $json = json_decode($results, true);
 
    <TR ALIGN="CENTER">
       <TD>5544</TD>
+      <TD>AP</TD>
       <TD ALIGN="LEFT">AP Applies Correct AP Profile - Bridge Mode </TD>
       <TD><?php echo print_r($json['tests']['ea8300']['5544'],true) ?></TD>
         <TD><?php echo print_r($json['tests']['ecw5211']['5544'],true) ?></TD>
@@ -186,7 +197,8 @@ $json = json_decode($results, true);
 
     <TR ALIGN="CENTER">
       <TD>5214</TD>
-      <TD ALIGN="LEFT">Client connectivity to 2.4 GHz WPA2-EAP - Bridge Mode</TD>
+      <TD>AP</TD>
+      <TD ALIGN="LEFT">Client connects to 2.4 GHz WPA2-EAP - Bridge Mode</TD>
       <TD><?php echo print_r($json['tests']['ea8300']['5214'],true) ?></TD>
         <TD><?php echo print_r($json['tests']['ecw5211']['5214'],true) ?></TD>
         <TD><?php echo print_r($json['tests']['ecw5410']['5214'],true) ?></TD>
@@ -194,7 +206,8 @@ $json = json_decode($results, true);
    </TR>
     <TR ALIGN="CENTER">
       <TD>2237</TD>
-      <TD ALIGN="LEFT">Client connectivity to 2.4 GHz WPA2 - Bridge Mode</TD>
+      <TD>AP</TD>
+      <TD ALIGN="LEFT">Client connects to 2.4 GHz WPA2 - Bridge Mode</TD>
       <TD><?php echo print_r($json['tests']['ea8300']['2237'],true) ?></TD>
         <TD><?php echo print_r($json['tests']['ecw5211']['2237'],true) ?></TD>
         <TD><?php echo print_r($json['tests']['ecw5410']['2237'],true) ?></TD>
@@ -202,7 +215,8 @@ $json = json_decode($results, true);
    </TR>
     <TR ALIGN="CENTER">
       <TD>2420</TD>
-      <TD ALIGN="LEFT">Client connectivity to 2.4 GHz WPA - Bridge Mode</TD>
+      <TD>AP</TD>
+      <TD ALIGN="LEFT">Client connects to 2.4 GHz WPA - Bridge Mode</TD>
       <TD><?php echo print_r($json['tests']['ea8300']['2420'],true) ?></TD>
         <TD><?php echo print_r($json['tests']['ecw5211']['2420'],true) ?></TD>
         <TD><?php echo print_r($json['tests']['ecw5410']['2420'],true) ?></TD>
@@ -210,7 +224,8 @@ $json = json_decode($results, true);
    </TR>
     <TR ALIGN="CENTER">
       <TD>5215</TD>
-      <TD ALIGN="LEFT">Client connectivity to 5 GHz WPA2-EAP - Bridge Mode</TD>
+      <TD>AP</TD>
+      <TD ALIGN="LEFT">Client connects to 5 GHz WPA2-EAP - Bridge Mode</TD>
       <TD><?php echo print_r($json['tests']['ea8300']['5215'],true) ?></TD>
         <TD><?php echo print_r($json['tests']['ecw5211']['5215'],true) ?></TD>
         <TD><?php echo print_r($json['tests']['ecw5410']['5215'],true) ?></TD>
@@ -218,7 +233,8 @@ $json = json_decode($results, true);
    </TR>
     <TR ALIGN="CENTER">
       <TD>2236</TD>
-      <TD ALIGN="LEFT">Client connectivity to 5 GHz WPA2 - Bridge Mode</TD>
+      <TD>AP</TD>
+      <TD ALIGN="LEFT">Client connects to 5 GHz WPA2 - Bridge Mode</TD>
       <TD><?php echo print_r($json['tests']['ea8300']['2236'],true) ?></TD>
         <TD><?php echo print_r($json['tests']['ecw5211']['2236'],true) ?></TD>
         <TD><?php echo print_r($json['tests']['ecw5410']['2236'],true) ?></TD>
@@ -226,7 +242,8 @@ $json = json_decode($results, true);
    </TR>
     <TR ALIGN="CENTER">
       <TD>2419</TD>
-      <TD ALIGN="LEFT">Client connectivity to 5 GHz WPA - Bridge Mode</TD>
+      <TD>AP</TD>
+      <TD ALIGN="LEFT">Client connects to 5 GHz WPA - Bridge Mode</TD>
       <TD><?php echo print_r($json['tests']['ea8300']['2419'],true) ?></TD>
         <TD><?php echo print_r($json['tests']['ecw5211']['2419'],true) ?></TD>
         <TD><?php echo print_r($json['tests']['ecw5410']['2419'],true) ?></TD>
@@ -234,6 +251,7 @@ $json = json_decode($results, true);
 
         <TR ALIGN="CENTER">
       <TD>5542</TD>
+      <TD>CloudSDK</TD>
       <TD ALIGN="LEFT">CloudSDK Pushes Correct AP Profile - NAT Mode </TD>
       <TD><?php echo print_r($json['tests']['ea8300']['5542'],true) ?></TD>
         <TD><?php echo print_r($json['tests']['ecw5211']['5542'],true) ?></TD>
@@ -243,6 +261,7 @@ $json = json_decode($results, true);
 
    <TR ALIGN="CENTER">
       <TD>5545</TD>
+      <TD>AP</TD>
       <TD ALIGN="LEFT">AP Applies Correct AP Profile - NAT Mode </TD>
       <TD><?php echo print_r($json['tests']['ea8300']['5545'],true) ?></TD>
         <TD><?php echo print_r($json['tests']['ecw5211']['5545'],true) ?></TD>
@@ -251,7 +270,8 @@ $json = json_decode($results, true);
    </TR>
         <TR ALIGN="CENTER">
       <TD>5216</TD>
-      <TD ALIGN="LEFT">Client connectivity to 2.4 GHz WPA2-EAP - NAT Mode</TD>
+      <TD>AP</TD>
+      <TD ALIGN="LEFT">Client connects to 2.4 GHz WPA2-EAP - NAT Mode</TD>
       <TD><?php echo print_r($json['tests']['ea8300']['5216'],true) ?></TD>
         <TD><?php echo print_r($json['tests']['ecw5211']['5216'],true) ?></TD>
         <TD><?php echo print_r($json['tests']['ecw5410']['5216'],true) ?></TD>
@@ -259,7 +279,8 @@ $json = json_decode($results, true);
    </TR>
     <TR ALIGN="CENTER">
       <TD>4325</TD>
-      <TD ALIGN="LEFT">Client connectivity to 2.4 GHz WPA2 - NAT Mode</TD>
+      <TD>AP</TD>
+      <TD ALIGN="LEFT">Client connects to 2.4 GHz WPA2 - NAT Mode</TD>
       <TD><?php echo print_r($json['tests']['ea8300']['4325'],true) ?></TD>
         <TD><?php echo print_r($json['tests']['ecw5211']['4325'],true) ?></TD>
         <TD><?php echo print_r($json['tests']['ecw5410']['4325'],true) ?></TD>
@@ -267,7 +288,8 @@ $json = json_decode($results, true);
    </TR>
     <TR ALIGN="CENTER">
       <TD>4323</TD>
-      <TD ALIGN="LEFT">Client connectivity to 2.4 GHz WPA - NAT Mode</TD>
+      <TD>AP</TD>
+      <TD ALIGN="LEFT">Client connects to 2.4 GHz WPA - NAT Mode</TD>
       <TD><?php echo print_r($json['tests']['ea8300']['4323'],true) ?></TD>
         <TD><?php echo print_r($json['tests']['ecw5211']['4323'],true) ?></TD>
         <TD><?php echo print_r($json['tests']['ecw5410']['4323'],true) ?></TD>
@@ -275,7 +297,8 @@ $json = json_decode($results, true);
    </TR>
     <TR ALIGN="CENTER">
       <TD>5217</TD>
-      <TD ALIGN="LEFT">Client connectivity to 5 GHz WPA2-EAP - NAT Mode</TD>
+      <TD>AP</TD>
+      <TD ALIGN="LEFT">Client connects to 5 GHz WPA2-EAP - NAT Mode</TD>
       <TD><?php echo print_r($json['tests']['ea8300']['5217'],true) ?></TD>
         <TD><?php echo print_r($json['tests']['ecw5211']['5217'],true) ?></TD>
         <TD><?php echo print_r($json['tests']['ecw5410']['5217'],true) ?></TD>
@@ -283,7 +306,8 @@ $json = json_decode($results, true);
    </TR>
     <TR ALIGN="CENTER">
       <TD>4326</TD>
-      <TD ALIGN="LEFT">Client connectivity to 5 GHz WPA2 - NAT Mode</TD>
+      <TD>AP</TD>
+      <TD ALIGN="LEFT">Client connects to 5 GHz WPA2 - NAT Mode</TD>
       <TD><?php echo print_r($json['tests']['ea8300']['4326'],true) ?></TD>
         <TD><?php echo print_r($json['tests']['ecw5211']['4326'],true) ?></TD>
         <TD><?php echo print_r($json['tests']['ecw5410']['4326'],true) ?></TD>
@@ -291,7 +315,8 @@ $json = json_decode($results, true);
    </TR>
     <TR ALIGN="CENTER">
       <TD>4324</TD>
-      <TD ALIGN="LEFT">Client connectivity to 5 GHz WPA - NAT Mode</TD>
+      <TD>AP</TD>
+      <TD ALIGN="LEFT">Client connects to 5 GHz WPA - NAT Mode</TD>
       <TD><?php echo print_r($json['tests']['ea8300']['4324'],true) ?></TD>
         <TD><?php echo print_r($json['tests']['ecw5211']['4324'],true) ?></TD>
         <TD><?php echo print_r($json['tests']['ecw5410']['4324'],true) ?></TD>
@@ -299,6 +324,7 @@ $json = json_decode($results, true);
    </TR>
         <TR ALIGN="CENTER">
       <TD>5543</TD>
+      <TD>CloudSDK</TD>
       <TD ALIGN="LEFT">CloudSDK Pushes Correct AP Profile - Custom VLAN </TD>
       <TD><?php echo print_r($json['tests']['ea8300']['5543'],true) ?></TD>
         <TD><?php echo print_r($json['tests']['ecw5211']['5543'],true) ?></TD>
@@ -308,6 +334,7 @@ $json = json_decode($results, true);
 
    <TR ALIGN="CENTER">
       <TD>5546</TD>
+      <TD>AP</TD>
       <TD ALIGN="LEFT">AP Applies Correct AP Profile - Custom VLAN </TD>
       <TD><?php echo print_r($json['tests']['ea8300']['5546'],true) ?></TD>
         <TD><?php echo print_r($json['tests']['ecw5211']['5546'],true) ?></TD>
@@ -317,7 +344,8 @@ $json = json_decode($results, true);
 
         <TR ALIGN="CENTER">
       <TD>5253</TD>
-      <TD ALIGN="LEFT">Client connectivity to 2.4 GHz WPA2-EAP - Custom VLAN</TD>
+      <TD>AP</TD>
+      <TD ALIGN="LEFT">Client connects to 2.4 GHz WPA2-EAP - Custom VLAN</TD>
       <TD><?php echo print_r($json['tests']['ea8300']['5253'],true) ?></TD>
         <TD><?php echo print_r($json['tests']['ecw5211']['5253'],true) ?></TD>
         <TD><?php echo print_r($json['tests']['ecw5410']['5253'],true) ?></TD>
@@ -325,7 +353,8 @@ $json = json_decode($results, true);
    </TR>
     <TR ALIGN="CENTER">
       <TD>5251</TD>
-      <TD ALIGN="LEFT">Client connectivity to 2.4 GHz WPA2 - Custom VLAN</TD>
+      <TD>AP</TD>
+      <TD ALIGN="LEFT">Client connects to 2.4 GHz WPA2 - Custom VLAN</TD>
       <TD><?php echo print_r($json['tests']['ea8300']['5251'],true) ?></TD>
         <TD><?php echo print_r($json['tests']['ecw5211']['5251'],true) ?></TD>
         <TD><?php echo print_r($json['tests']['ecw5410']['5251'],true) ?></TD>
@@ -333,7 +362,8 @@ $json = json_decode($results, true);
    </TR>
     <TR ALIGN="CENTER">
       <TD>5252</TD>
-      <TD ALIGN="LEFT">Client connectivity to 2.4 GHz WPA - Custom VLAN</TD>
+      <TD>AP</TD>
+      <TD ALIGN="LEFT">Client connects to 2.4 GHz WPA - Custom VLAN</TD>
       <TD><?php echo print_r($json['tests']['ea8300']['5252'],true) ?></TD>
         <TD><?php echo print_r($json['tests']['ecw5211']['5252'],true) ?></TD>
         <TD><?php echo print_r($json['tests']['ecw5410']['5252'],true) ?></TD>
@@ -341,7 +371,8 @@ $json = json_decode($results, true);
    </TR>
     <TR ALIGN="CENTER">
       <TD>5250</TD>
-      <TD ALIGN="LEFT">Client connectivity to 5 GHz WPA2-EAP - Custom VLAN</TD>
+      <TD>AP</TD>
+      <TD ALIGN="LEFT">Client connects to 5 GHz WPA2-EAP - Custom VLAN</TD>
       <TD><?php echo print_r($json['tests']['ea8300']['5250'],true) ?></TD>
         <TD><?php echo print_r($json['tests']['ecw5211']['5250'],true) ?></TD>
         <TD><?php echo print_r($json['tests']['ecw5410']['5250'],true) ?></TD>
@@ -349,7 +380,8 @@ $json = json_decode($results, true);
    </TR>
     <TR ALIGN="CENTER">
       <TD>5248</TD>
-      <TD ALIGN="LEFT">Client connectivity to 5 GHz WPA2 - Custom VLAN</TD>
+      <TD>AP</TD>
+      <TD ALIGN="LEFT">Client connects to 5 GHz WPA2 - Custom VLAN</TD>
       <TD><?php echo print_r($json['tests']['ea8300']['5248'],true) ?></TD>
         <TD><?php echo print_r($json['tests']['ecw5211']['5248'],true) ?></TD>
         <TD><?php echo print_r($json['tests']['ecw5410']['5248'],true) ?></TD>
@@ -357,7 +389,8 @@ $json = json_decode($results, true);
    </TR>
     <TR ALIGN="CENTER">
       <TD>5249</TD>
-      <TD ALIGN="LEFT">Client connectivity to 5 GHz WPA - Custom VLAN</TD>
+      <TD>AP</TD>
+      <TD ALIGN="LEFT">Client connects to 5 GHz WPA - Custom VLAN</TD>
       <TD><?php echo print_r($json['tests']['ea8300']['5249'],true) ?></TD>
         <TD><?php echo print_r($json['tests']['ecw5211']['5249'],true) ?></TD>
         <TD><?php echo print_r($json['tests']['ecw5410']['5249'],true) ?></TD>
