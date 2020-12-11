2c5d45b9d293a0016173841e423391c4b7156fd5
Dipti <dipti.dhond@candelatech.com>
2020-12-10 23:18:39 -0800

    same realm lfcli changes

--- py-scripts/example_wep_connection.py
+++ py-scripts/example_wep_connection.py
@@ -54,10 +54,10 @@ class IPv4Test(LFCliBase):
         self.station_profile.admin_up()
         if self.local_realm.wait_for_ip(station_list=self.sta_list, debug=self.debug, timeout_sec=30):
             self._pass("Station build finished")
-            self.passed()
+            self.exit_success()
         else:
             self._fail("Stations not able to acquire IP. Please check network input.")
-            self.failed()
+            self.exit_fail()
 
     def cleanup(self, sta_list):
         self.station_profile.cleanup(sta_list)
@@ -85,13 +85,10 @@ def main():
         --host localhost (optional) \\
         --port 8080  (optional) \\
         --num_stations 3 \\
-        --security {open|wep|wpa|wpa2|wpa3} \\
+        --radio wiphy1
         --ssid jedway-wep-48 \\
         --passwd jedway-wep-48 \\
         --debug 
-
-    Note:   multiple --radio switches may be entered up to the number of radios available:
-                     --radio wiphy0 <stations> <ssid> <ssid password>  --radio <radio 01> <number of last station> <ssid> <ssid password>
             ''')
 
     args = parser.parse_args()
@@ -106,7 +103,7 @@ def main():
                                         end_id_=num_sta-1,
                                         padding_number_=10000)
     ip_test = IPv4Test(lfjson_host, lfjson_port, ssid=args.ssid, password=args.passwd,
-                       security=args.security, radio=args.radio, sta_list=station_list)
+                       security="wep", radio=args.radio, sta_list=station_list)
     ip_test.cleanup(station_list)
     ip_test.timeout = 60
     ip_test.build()
