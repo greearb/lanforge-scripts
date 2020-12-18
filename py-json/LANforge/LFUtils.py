c1bd64adb9ed3eed52004ee7c5a9f8165e5868f2
Jed Reynolds <jed@candelatech.com>
2020-12-18 12:36:41 -0800

    uses non-deprecated method

--- py-json/LANforge/LFUtils.py
+++ py-json/LANforge/LFUtils.py
@@ -459,7 +459,7 @@ def wait_until_ports_disappear(base_url="http://localhost:8080", port_list=[], d
             if debug:
                 print("checking:" + check_url)
             lf_r = LFRequest.LFRequest(base_url, check_url)
-            json_response = lf_r.getAsJson(debug_=debug)
+            json_response = lf_r.get_as_json(debug_=debug)
             if (json_response != None):
                 found_stations.append(port_name)
         if len(found_stations) > 0:
