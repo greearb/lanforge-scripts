803758a8ba633497e1ff6744f39aef539f074e51
Matthew Stidham <stidmatt@protonmail.com>
2021-02-17 10:14:12 -0800

    Correcting errors in regression_test and adding pyarrow to update_dependencies:
    Signed-off-by: Matthew Stidham <stidmatt@protonmail.com>

--- py-scripts/update_dependencies.py
+++ py-scripts/update_dependencies.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import subprocess
 def main():
-    command = "pip3 install pandas seaborn plotly numpy dash paramiko bokeh websocket-client --upgrade"
+    command = "pip3 install pandas seaborn plotly numpy dash paramiko bokeh websocket-client pyarrow --upgrade"
     res = subprocess.call(command, shell = True)
 
     print("Returned Value: ", res)
