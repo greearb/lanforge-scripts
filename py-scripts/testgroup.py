1ff7b5df50c7074270236d9e3dba7df63c59f4bf
Matthew Stidham <stidmatt@protonmail.com>
2021-02-24 13:57:00 -0800

    Correct the rm_group command in testgroup.py
    Signed-off-by: Matthew Stidham <stidmatt@protonmail.com>

--- py-scripts/testgroup.py
+++ py-scripts/testgroup.py
@@ -64,7 +64,7 @@ class TestGroup(LFCliBase):
         if self.tg_action == 'del':
             print("Removing %s" % self.tg_profile.group_name)
             if self.tg_profile.check_group_exists():
-                self.tg_profile.remove_group()
+                self.tg_profile.rm_group()
             else:
                 print("%s not found, no action taken" % self.tg_profile.group_name)
 
