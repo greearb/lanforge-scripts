980a9435d6394ac07559c49bf1d47ac0c931b49b
Matthew Stidham <stidmatt@protonmail.com>
2021-03-24 10:05:04 -0700

    Adding missing dependency to update_dependencies
    Signed-off-by: Matthew Stidham <stidmatt@protonmail.com>

--- py-scripts/update_dependencies.py
+++ py-scripts/update_dependencies.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import subprocess
 def main():
-    command = "pip3 install pandas plotly numpy paramiko bokeh websocket-client pyarrow xlsxwriter --upgrade"
+    command = "pip3 install pandas plotly numpy paramiko bokeh websocket-client pyarrow xlsxwriter pyshark --upgrade"
     res = subprocess.call(command, shell = True)
 
     print("Returned Value: ", res)
