d15be3b4a79f85e2cc35688763a358d8b4541533
Matthew Stidham <stidmatt@protonmail.com>
2021-04-12 10:54:02 -0700

    add influxdb to update_dependencies
    Signed-off-by: Matthew Stidham <stidmatt@protonmail.com>

--- py-scripts/update_dependencies.py
+++ py-scripts/update_dependencies.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import subprocess
 def main():
-    command = "pip3 install pandas plotly numpy paramiko bokeh websocket-client pyarrow xlsxwriter pyshark --upgrade"
+    command = "pip3 install pandas plotly numpy paramiko bokeh websocket-client pyarrow xlsxwriter pyshark influxdb --upgrade"
     res = subprocess.call(command, shell = True)
 
     print("Returned Value: ", res)
