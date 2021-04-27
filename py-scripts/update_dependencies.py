ea1d3c9d222d3336302dbcd009ac783ab7e9b16c
Matthew Stidham <stidmatt@protonmail.com>
2021-04-27 10:08:17 -0700

    Adding libraries to update_dependencies
    Signed-off-by: Matthew Stidham <stidmatt@protonmail.com>

--- py-scripts/update_dependencies.py
+++ py-scripts/update_dependencies.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import subprocess
 def main():
-    command = "pip3 install pandas plotly numpy paramiko bokeh websocket-client pyarrow xlsxwriter pyshark influxdb influxdb-client --upgrade"
+    command = "pip3 install pandas plotly numpy paramiko bokeh websocket-client pyarrow xlsxwriter pyshark influxdb influxdb-client matplotlib pdfkit pip-search --upgrade"
     res = subprocess.call(command, shell = True)
 
     print("Returned Value: ", res)
