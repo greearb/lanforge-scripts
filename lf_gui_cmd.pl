5399796755a82e1559d45f05faa450a390314a97
Jed Reynolds <jed@candelatech.com>
2021-05-17 13:21:00 -0700

    lf_gui_cmd.pl: adds switch for setting default report verbosity
    Signed-off-by: Jed Reynolds <jed@candelatech.com>

--- lf_gui_cmd.pl
+++ lf_gui_cmd.pl
@@ -32,6 +32,7 @@ my $scenario = "";
 my $tconfig = "";  # test config
 my $rpt_dest = "";
 my $show_help = 0;
+my $verbosity = -1;
 my @modifiers_key = ();
 my @modifiers_val = ();
 
@@ -50,6 +51,7 @@ my $usage = qq($0  [--manager { hostname or address of LANforge GUI machine } ]
                  [--tconfig {test configuration name, use defaults if not specified} ]
                  [--rpt_dest {Copy report to destination once it is complete} ]
                  [--cmd { command to send to the GUI } ]
+                 [--verbosity { report verbosity 1 - 11 } ]
                  [--modifier "
 
 Example:
@@ -75,6 +77,7 @@ GetOptions (
    'rpt_dest=s'            => \$rpt_dest,
    'port=s'                => \$port,
    'cmd|c=s'               => \$cmd,
+    'verbosity|v=i'         => \$verbosity,
 ) || die("$usage");
 
 if ($show_help) {
@@ -141,6 +144,9 @@ if ($ttype ne "") {
   if ($tconfig ne "") {
     print doCmd("cv load '$tname' '$tconfig'");
   }
+    if ($verbosity >= 1) {
+        print doCmd("cv set '$tname' 'VERBOSITY' '$verbosity'");
+    }
   print doCmd("cv click '$tname' 'Auto Save Report'");
 
   for ($i = 0; $i<@modifiers_key; $i++) {
