4dbdf2eaad89c506012cfd2f29c082eb5ba923e4
Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>
2021-06-29 14:39:42 -0600

    cv_test_reports.py : found that was clobbering the local_path
    Signed-off-by: Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>

--- py-json/cv_test_reports.py
+++ py-json/cv_test_reports.py
@@ -12,6 +12,6 @@ class lanforge_reports:
         ssh.connect(hostname=hostname, username=username, password=password, port=port, allow_agent=False, look_for_keys=False)
 
         with SCPClient(ssh.get_transport()) as scp:
-            scp.get(remote_path=report_location, report_dir=report_dir, recursive=True)
+            scp.get(remote_path=report_location, local_path=report_dir, recursive=True)
             scp.close()
 
