0f1c703cb3ff84bf5a5568c28e492e294840812e
Jed Reynolds <jed@candelatech.com>
2021-07-06 16:40:58 -0700

    test_histogram.py example of how to use expand_endp_histogram
    Signed-off-by: Jed Reynolds <jed@candelatech.com>

---
+++ py-json/test_histogram.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+""" ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+    internal test driving LFUtils.expand_endp_histogram
+----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- """
+import LANforge
+from LANforge import LFUtils
+import pprint
+
+distrib_load = {
+    "histo_category_width" : 3,
+    "histogram" : [
+          221,
+          113,
+          266,
+          615,
+          16309,
+          56853,
+          7954,
+          1894,
+          29246,
+          118,
+          12,
+          2,
+          0,
+          0,
+          0,
+          0
+       ],
+    "time window ms" : 300000,
+    "window avg" : 210.285,
+    "window max" : 228,
+    "window min" : 193
+}
+
+if __name__ == '__main__':
+    LFUtils.expand_endp_histogram(distrib_load)
+
+
