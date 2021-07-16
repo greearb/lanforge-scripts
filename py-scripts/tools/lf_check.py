2ef3406de7b977ff515cfe53773a74e275c4b451
Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>
2021-07-15 18:57:28 -0600

    lf_check.py : bug fix for setting the DUT_BSSID_5G
    Signed-off-by: Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>

--- py-scripts/tools/lf_check.py
+++ py-scripts/tools/lf_check.py
@@ -139,14 +139,14 @@ class lf_check():
         self.lf_mgr_pass =  "lanforge"
 
         # dut configuration 
-        self.dut_name = "DUT_NAME" # "ASUSRT-AX88U" note this is not dut_set_name
-        self.dut_hw = "DUT_HW"
-        self.dut_sw = "DUT_SW"
-        self.dut_model = "DUT_MODEL"
-        self.dut_serial = "DUT_SERIAL"
-        self.dut_bssid_2g = "BSSID_2G" #"3c:7c:3f:55:4d:64" - this is the mac for the 2.4G radio this may be seen with a scan 
-        self.dut_bssid_5g = "BSSID_5G" #"3c:7c:3f:55:4d:64" - this is the mac for the 5G radio this may be seen with a scan 
-        self.dut_bssid_6g = "BSSID_6G" #"3c:7c:3f:55:4d:64" - this is the mac for the 6G radio this may be seen with a scan 
+        self.dut_name = "DUT_NAME_NOT_SET" # "ASUSRT-AX88U" note this is not dut_set_name
+        self.dut_hw = "DUT_HW_NOT_SET"
+        self.dut_sw = "DUT_SW_NOT_SET"
+        self.dut_model = "DUT_MODEL_NOT_SET"
+        self.dut_serial = "DUT_SERIAL_NOT_SET"
+        self.dut_bssid_2g = "BSSID_2G_NOT_SET" #"3c:7c:3f:55:4d:64" - this is the mac for the 2.4G radio this may be seen with a scan 
+        self.dut_bssid_5g = "BSSID_5G_NOT_SET" #"3c:7c:3f:55:4d:64" - this is the mac for the 5G radio this may be seen with a scan 
+        self.dut_bssid_6g = "BSSID_6G_NOT_SET" #"3c:7c:3f:55:4d:64" - this is the mac for the 6G radio this may be seen with a scan 
         #NOTE:  My influx token is unlucky and starts with a '-', but using the syntax below # with '=' right after the argument keyword works as hoped.
         # --influx_token=
 
@@ -478,11 +478,11 @@ blog: http://{blog}:2368
         else:
             self.logger.info("dut_serial not in test_parameters json")
         if "dut_bssid_2g" in self.json_data["test_parameters"]:
-            self.dut_bssid_2G = self.json_data["test_parameters"]["dut_bssid_2G"]
+            self.dut_bssid_2g = self.json_data["test_parameters"]["dut_bssid_2g"]
         else:
             self.logger.info("dut_bssid_2G not in test_parameters json")
         if "dut_bssid_5g" in self.json_data["test_parameters"]:
-            self.dut_bssid_5G = self.json_data["test_parameters"]["dut_bssid_5g"]
+            self.dut_bssid_5g = self.json_data["test_parameters"]["dut_bssid_5g"]
         else:
             self.logger.info("dut_bssid_5g not in test_parameters json")
         if "dut_bssid_6g" in self.json_data["test_parameters"]:
