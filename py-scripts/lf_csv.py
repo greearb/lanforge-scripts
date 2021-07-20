a71e84d524f81a97f2293aecd500c375afa6a753
Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>
2021-07-20 12:47:31 -0600

    lf_csv.py : added the kpi values
    Signed-off-by: Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>

--- py-scripts/lf_csv.py
+++ py-scripts/lf_csv.py
@@ -43,6 +43,26 @@ class lf_csv:
         csv_df.to_csv(self.filename, index=False, encoding='utf-8', na_rep='NA', float_format='%.2f')
 
 # this layout may need to change
+'''
+kpi.csv : specific file that is used for the database, dashboard and blog post
+A blank entry is a valid entry in some cases. 
+
+    Date: date of run 
+    test-rig : testbed that the tests are run on for example ct_us_001
+    test-tag : test specific information to differenciate the test,  LANforge radios used, security modes (wpa2 , open)
+    dut-hw-version : hardware version of the device under test
+    dut-sw-version : software version of the device under test
+    dut-model-num : model number / name of the device under test
+    test-priority : ??
+    test-id : script or test name ,  AP Auto, wifi capacity, data plane, dfs
+    short-description : short description of the test
+    pass/fail : set blank for performance tests
+    numeric-score : this is the value for the y-axis (x-axis is a timestamp),  numeric value of what was measured
+    test-details : what was measured in the numeric-score,  e.g. bits per second, bytes per second, upload speed, minimum cx time (ms)
+    Units : units used for the numeric-scort
+    Graph-Group - For the dashboard the graph / panel to put the resutls in . Currently the dashboard is Grafana
+
+'''
 class lf_kpi_csv:
     def __init__(self,
                 _kpi_headers = ['Date','test-rig','test-tag','dut-hw-version','dut-sw-version','dut-model-num',
