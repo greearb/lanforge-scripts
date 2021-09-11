03a4f951538eb51bc670c4a790d6bce7e94210ab
erinnerim <erin.grimes@candelatech.com>
2021-09-10 17:57:48 -0700

    Revert "importlib refactor ../ typo"
    This reverts commit 34afad944fb9876a4a1bea5a52babdd86f165a74.

--- py-scripts/lf_dataplane_test.py
+++ py-scripts/lf_dataplane_test.py
@@ -106,7 +106,7 @@ if sys.version_info[0] != 3:
     exit(1)
 
 if 'lanforge-scripts' not in sys.path:
-    sys.path.append(os.path.join(os.path.abspath(__file__ + "../../../../")))
+    sys.path.append(os.path.join(os.path.abspath(__file__ + "../../../")))
 
 cv_test_manager = importlib.import_module("lanforge-scripts.py-json.cv_test_manager")
 cv_test = cv_test_manager.cv_test
