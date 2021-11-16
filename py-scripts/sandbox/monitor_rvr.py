cf8a5f8d839340697de4dbbe4b3685967a8c8248
Matthew Stidham <stidmatt@gmail.com>
2021-11-16 11:01:52 -0800

    monitor_rvr: remove invalid comparisons
    Signed-off-by: Matthew Stidham <stidmatt@gmail.com>

--- py-scripts/sandbox/monitor_rvr.py
+++ py-scripts/sandbox/monitor_rvr.py
@@ -51,7 +51,7 @@ class RunCvScenario(LFCliBase):
         }
         self.json_post("/cli-json/load", data)
         sleep(1)
-        port_counter = 0;
+        port_counter = 0
         attempts = 6
         while (attempts > 0) and (port_counter > 0):
             sleep(1)
@@ -69,7 +69,7 @@ class RunCvScenario(LFCliBase):
 
         if (port_counter != 0) and (attempts == 0):
             print("There appears to be a vAP in this database, quitting.")
-            pprint(alias_map);
+            pprint(alias_map)
             exit(1)
 
         data = {
