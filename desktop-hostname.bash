b83e8676fd921ae3cfe888d0df80fd2aa1994ea7
Jed Reynolds <jed@bitratchet.com>
2021-11-17 15:00:05 -0800

    desktop-hostname.bash: bases fill color on end of mac address
    Signed-off-by: Jed Reynolds <jed@bitratchet.com>

--- desktop-hostname.bash
+++ desktop-hostname.bash
@@ -12,10 +12,11 @@ my_hostname=`hostname`
 my_dev=`ip ro sho | awk '/default via/{print $5}'`
 my_ip=`ip a sho $my_dev | awk '/inet /{print $2}'`
 my_mac=`ip a sho | grep -B1 "$my_dev" | awk '/ether /{print $2}'`
-
+fill_color=${my_mac//:/}
+fill_color=${fill_color:6:12}
 X=220
 Y=150
-convert -pointsize 80 -fill cyan \
+convert -pointsize 80 -fill "#$fill_color" \
   -draw "text $X,$Y \"$my_hostname\"" \
   -draw "text $X,$(( Y + 75 )) \"$my_dev $my_ip\"" \
   -draw "text $X,$(( Y + 150 )) \"$my_mac\"" \
