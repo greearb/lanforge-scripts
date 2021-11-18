3d45174f3bb204c8cd73b7bd559ea00b532cde11
Matthew Stidham <stidmatt@gmail.com>
2021-11-17 17:33:51 -0800

    regression_test: improve sta_connect and sta_connect_example queries
    Signed-off-by: Matthew Stidham <stidmatt@gmail.com>

--- py-scripts/regression_test.sh
+++ py-scripts/regression_test.sh
@@ -162,30 +162,8 @@ function create_bridge_and_station() {
 if [[ ${#SHORT} -gt 0 ]]; then
   testCommands=(
       # run_l3_longevity
-      "./test_ip_connection.py --radio $RADIO_USED --num_stations $NUM_STA --ssid $SSID_USED --passwd $PASSWD_USED --security $SECURITY --debug --mgr $MGR"
-      "./test_ip_variable_time.py --radio $RADIO_USED --ssid $SSID_USED --passwd $PASSWD_USED --security $SECURITY --test_duration 15s --output_format excel --layer3_cols $COL_NAMES --debug --mgr $MGR  --traffic_type lf_udp"
-      "./test_ip_variable_time.py --radio $RADIO_USED --ssid $SSID_USED --passwd $PASSWD_USED --security $SECURITY --test_duration 15s --output_format csv --layer3_cols $COL_NAMES --debug --mgr $MGR  --traffic_type lf_udp"
-      "./test_ip_connection.py --radio $RADIO_USED --ssid $SSID_USED --passwd $PASSWD_USED --security $SECURITY --debug --mgr $MGR --ipv6"
-      "./test_ip_variable_time.py --radio $RADIO_USED --ssid $SSID_USED --passwd $PASSWD_USED --security $SECURITY --test_duration 15s --debug --mgr $MGR --ipv6 --traffic_type lf_udp"
-    	"./lf_ap_auto_test.py --mgr ${MGR} --port 8080 --lf_user lanforge --lf_password lanforge --instance_name ap-auto-instance \
-       --config_name test_con \
-       --upstream 1.1.eth2 \
-       --dut5_0 'linksys-8450 Default-SSID-5gl c4:41:1e:f5:3f:25 (2)' \
-       --dut2_0 'linksys-8450 Default-SSID-2g c4:41:1e:f5:3f:24 (1)' \
-       --max_stations_2 100 \
-       --max_stations_5 100 \
-       --max_stations_dual 200 \
-       --radio2 1.1.wiphy0 \
-       --radio2 1.1.wiphy1 --set 'Basic Client Connectivity' 1 --set 'Multi Band Performance' 1 --set 'Skip 2.4Ghz Tests' 1 \
-       --set 'Skip 5Ghz Tests' 1 --set 'Throughput vs Pkt Size' 0 --set 'Capacity' 0 --set 'Stability' 0 \
-       --set 'Band-Steering' 0  --set 'Multi-Station Throughput vs Pkt Size' 0 --set 'Long-Term' 0 \
-       --pull_report \
-       --influx_host c7-graphana \
-       --influx_port 8086 \
-       --influx_org Candela \
-       --influx_token=-u_Wd-8o992701QF0c5UmqEp7w7Z7YOMaWLxOMgmHfATJGnQbbmYyNxHBR9PgD6taM_tcxqJl6U8DjU1xINFQ== \
-       --influx_bucket ben \
-       --influx_tag testbed Ferndale-01"
+      "./sta_connect_example.py --mgr $MGR --ssid $SSID_USED --passwd $PASSWD_USED --security $SECURITY --radio $RADIO_USED --upstream_port $UPSTREAM --test_duration 30s"
+      "./sta_connect.py --mgr $MGR --ssid $SSID_USED --passwd $PASSWD_USED --security $SECURITY --radio $RADIO_USED --upstream_port $UPSTREAM --test_duration 15s"
   )
 else
   testCommands=(
@@ -315,8 +293,8 @@ else
       #scenario.py
       #"./sta_connect2.py --dest 192.168.100.209 --dut_ssid OpenWrt-2 --dut_bssid 24:F5:A2:08:21:6C"
       #./sta_connect_bssid_mac.py
-       "./sta_connect_example.py --mgr $MGR --ssid $SSID_USED --passwd $PASSWD_USED --security $SECURITY --radio $RADIO_USED"
-      #sta_connect.py
+      "./sta_connect_example.py --mgr $MGR --ssid $SSID_USED --passwd $PASSWD_USED --security $SECURITY --radio $RADIO_USED --upstream_port $UPSTREAM --test_duration 15s"
+      "./sta_connect.py --mgr $MGR --ssid $SSID_USED --passwd $PASSWD_USED --security $SECURITY --radio $RADIO_USED --upstream_port $UPSTREAM --test_duration 15s"
       "./sta_scan_test.py --ssid $SSID_USED --security $SECURITY --passwd $PASSWD_USED --radio $RADIO_USED"
       #station_layer3.py
       #stations_connected.py
