dccb115e71bf41c79b2e0af622238acdc89b42f1
matthew <stidmatt@gmail.com>
2021-12-03 08:07:27 -0800

    show_ports: Fix whitespace
    Signed-off-by: matthew <stidmatt@gmail.com>

--- py-json/show_ports.py
+++ py-json/show_ports.py
@@ -12,25 +12,24 @@ if sys.version_info[0] != 3:
     print("This script requires Python 3")
     exit()
 
- 
 sys.path.append(os.path.join(os.path.abspath(__file__ + "../../../")))
 
 LFRequest = importlib.import_module("py-json.LANforge.LFRequest")
 
 
 def main():
-   url = "http://localhost:8080/port/1/1/list"
-   timeout = 5 # seconds
+    url = "http://localhost:8080/port/1/1/list"
 
-   lf_r = LFRequest.LFRequest(url)
-   json_response = lf_r.getAsJson(True)
-   j_printer = pprint.PrettyPrinter(indent=2)
-   j_printer.pprint(json_response)
-   
-   #for record in json_response['interfaces']:
-        #j_printer.pprint(record)
+    lf_r = LFRequest.LFRequest(url)
+    json_response = lf_r.getAsJson(True)
+    j_printer = pprint.PrettyPrinter(indent=2)
+    j_printer.pprint(json_response)
+
+    # for record in json_response['interfaces']:
+    # j_printer.pprint(record)
+
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 if __name__ == "__main__":
-   main()
+    main()
 #
