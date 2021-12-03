0a79bbf77753efc1b6187787e797c978ed0d6207
matthew <stidmatt@gmail.com>
2021-12-03 08:10:21 -0800

    test_utility: Fix whitespace
    Signed-off-by: matthew <stidmatt@gmail.com>

--- py-json/test_utility.py
+++ py-json/test_utility.py
@@ -14,7 +14,6 @@ import threading
 import re
 import json
 
-
 sys.path.append(os.path.join(os.path.abspath(__file__ + "../../../")))
 
 lfcli_base = importlib.import_module("py-json.LANforge.lfcli_base")
@@ -28,13 +27,14 @@ updates_path = webconsole_dir + "/web_json/updates.js"
 
 
 class ClientVisualization(LFCliBase, threading.Thread):
-    def __init__(self, lfclient_host="localhost", lfclient_port=8080, num_clients= 64, max_data= 120, thread_id=None, _debug_on=False, _exit_on_error=False, _exit_on_fail=False):
+    def __init__(self, lfclient_host="localhost", lfclient_port=8080, num_clients=64, max_data=120, thread_id=None,
+                 _debug_on=False, _exit_on_error=False, _exit_on_fail=False):
         super().__init__(lfclient_host, lfclient_port, _debug=_debug_on, _exit_on_fail=_exit_on_fail)
         threading.Thread.__init__(self)
         self.num_clients = num_clients
         self.max_data = max_data
         self._stop_event = threading.Event()
-        self.client_data = {"down":[], "phantom":[], "ip":[], "scanning":[]}
+        self.client_data = {"down": [], "phantom": [], "ip": [], "scanning": []}
 
     def stop(self):
         self._stop_event.set()
@@ -51,12 +51,13 @@ class ClientVisualization(LFCliBase, threading.Thread):
             self.ip = 0
             self.down = 0
             self.phantom = 0
-            for i in self.json_get("/port/list?fields=port,alias,parent%20dev,down,phantom,ip,port%20type")['interfaces']:
+            for i in self.json_get("/port/list?fields=port,alias,parent%20dev,down,phantom,ip,port%20type")[
+                'interfaces']:
 
                 for j in i:
                     print(i[j]['port type'])
                     if i[j]['port type'] == "WIFI-STA" and i[j]['parent dev'] == "wiphy1" and i[j]['alias'] != 'wlan1':
-                        #print(j)
+                        # print(j)
                         if i[j]['down'] is False and i[j]['phantom'] is False and i[j]['ip'] == '0.0.0.0':
                             self.scanning += 1
                         elif i[j]['down'] is False and i[j]['phantom'] is True:
@@ -75,7 +76,6 @@ class ClientVisualization(LFCliBase, threading.Thread):
             self.client_data['down'].append(self.down)
             self.client_data['ip'].append(self.ip)
 
-
             for i in self.client_data:
                 if len(self.client_data[i]) >= self.max_data:
                     self.client_data[i].pop(0)
@@ -85,19 +85,20 @@ class ClientVisualization(LFCliBase, threading.Thread):
 
 
 class CreateHTML():
-    def __init__(self, path="", test_name="", time_snap="", dut_ssid="", test_conf_data={}, objective="", test_results={}, chart_data={}, chart_params={}):
+    def __init__(self, path="", test_name="", time_snap="", dut_ssid="", test_conf_data={}, objective="",
+                 test_results={}, chart_data={}, chart_params={}):
         self.head = """
                       <html>
                         <head>
-                            <title>"""+test_name+"""</title>
+                            <title>""" + test_name + """</title>
                         </head>
                         <body>
                         <div class='Section report_banner-1000x205' style='background-image:url("../../assets/brand/banner.jpg");background-repeat:no-repeat;padding:0;margin:0;min-width:1000px; min-height:205px;width:1000px; height:205px;max-width:1000px; max-height:205px;'>
                             <img align='right' style='padding:25;margin:5;width:200px;' src="../../assets/brand/logo.png" border='0' />
                                 <div class='HeaderStyle'>
                                     <br>
-                                    <h1 class='TitleFontPrint' style='color:darkgreen;'>"""+test_name+"""</h1>
-                                    <h3 class='TitleFontPrint' style="color:darkgreen;">"""+time_snap+"""</h3>
+                                    <h1 class='TitleFontPrint' style='color:darkgreen;'>""" + test_name + """</h1>
+                                    <h3 class='TitleFontPrint' style="color:darkgreen;">""" + time_snap + """</h3>
                                 </div>
                         </div>
                         <br>
@@ -119,16 +120,16 @@ class CreateHTML():
                                             <td>
                                                 SSID
                                             </td>
-                                            <td colspan="3">"""+dut_ssid+"""
+                                            <td colspan="3">""" + dut_ssid + """
                                             </td>
                                         </tr>
                                         """
 
         for i in test_conf_data:
             self.test_conf = self.test_conf + """<tr>
-                                                    <td>"""+str(i)+"""
+                                                    <td>""" + str(i) + """
                                                     </td>
-                                                    <td colspan="3">"""+test_conf_data[i]+"""
+                                                    <td colspan="3">""" + test_conf_data[i] + """
                                                     </td>
                                                  </tr>
                                                """
@@ -141,13 +142,13 @@ class CreateHTML():
 
         self.objective = """
                             <br><h2 align="left">Objective</h2> <p align="left" width="900">
-                                """+objective+"""
+                                """ + objective + """
                             </p>
                             <br>
                             """
 
         if str(test_results['summary']).__contains__("PASS"):
-            self.summary_results ="""
+            self.summary_results = """
                                <br>
                                     <table width="700px" border="1" cellpadding="2" cellspacing="0" style="border-top-color: gray; border-top-style: solid; border-top-width: 1px; border-right-color: gray; border-right-style: solid; border-right-width: 1px; border-bottom-color: gray; border-bottom-style: solid; border-bottom-width: 1px; border-left-color: gray; border-left-style: solid; border-left-width: 1px">
                                         <tr>
@@ -180,14 +181,11 @@ class CreateHTML():
                                                 </table>
                                            <br>
                                            """
-        chart_d =[]
-        chart_label =[]
+        chart_d = []
+        chart_label = []
         for i in chart_data:
             chart_label.append(i)
             chart_d.append(chart_data[i])
-
-
-
 
         self.detail_result = """<table width="1000px" border="1" cellpadding="2" cellspacing="0" >
                                         <tr><th colspan="2">Detailed Results</th></tr>
@@ -195,8 +193,8 @@ class CreateHTML():
                                             <tr>
                              """
         for index in test_results['detail']['keys']:
-            self.detail_result =  self.detail_result+"<th colspan='2'>"+index+"</th>"
-        self.detail_result = self.detail_result +"</tr>"
+            self.detail_result = self.detail_result + "<th colspan='2'>" + index + "</th>"
+        self.detail_result = self.detail_result + "</tr>"
 
         for data in test_results['detail']['data']:
             self.detail_result = self.detail_result + "<tr align='center'>"
@@ -205,12 +203,14 @@ class CreateHTML():
             for i in data:
                 print(data[i])
                 if str(data[i]).__contains__("PASS"):
-                    self.detail_result = self.detail_result + "<th colspan='2' bgcolor='#90EE90'>" + str(data[i]) + "</th>"
+                    self.detail_result = self.detail_result + "<th colspan='2' bgcolor='#90EE90'>" + str(
+                        data[i]) + "</th>"
                 elif str(data[i]).__contains__("FAIL"):
-                    self.detail_result = self.detail_result + "<th colspan='2' bgcolor='orange'>" + str(data[i]) + "</th>"
+                    self.detail_result = self.detail_result + "<th colspan='2' bgcolor='orange'>" + str(
+                        data[i]) + "</th>"
                 else:
                     self.detail_result = self.detail_result + "<th colspan='2'>" + str(data[i]) + "</th>"
-            self.detail_result = self.detail_result +"</tr>"
+            self.detail_result = self.detail_result + "</tr>"
 
         self.chart_data = chart_data
         chart_values = []
@@ -231,14 +231,12 @@ class CreateHTML():
 
         self.chart = """<img align='center' style='padding:25;margin:5;width:600px;' src="plot.png" border='0' />"""
 
-
         self.end = """</table>
                       </table>
                       </body>
                       </html>
                    """
-        self.report = self.head + self.test_conf + self.objective + self.summary_results + self.chart +self.detail_result + self.end
-
+        self.report = self.head + self.test_conf + self.objective + self.summary_results + self.chart + self.detail_result + self.end
 
 
 class RuntimeUpdates():
@@ -306,22 +304,14 @@ class StatusSession(LFCliBase):
         Method to read all the messages for a particular session
         """
         keys = []
-        for i in self.json_get("/status-msg/"+self.session_id)['messages']:
+        for i in self.json_get("/status-msg/" + self.session_id)['messages']:
             keys.append(i['key'])
-        json_uri = "/status-msg/"+self.session_id + "/"
+        json_uri = "/status-msg/" + self.session_id + "/"
         for i in keys:
             json_uri = json_uri + i + ","
         return self.json_get(json_uri)['messages']
 
 
-
-
-
-
-
-
 if __name__ == "__main__":
     obj = StatusMsg(lfclient_host="localhost", lfclient_port=8090, session_id="01_18_21_20_04_20")
     print(obj.read())
-
-
