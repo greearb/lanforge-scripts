4ae239c7c535a461ccde8687528aa713ea292dc9
Matthew Stidham <stidmatt@gmail.com>
2021-12-07 18:03:23 -0800

    mine_regression_test: Move ips to argparser so a user can choose which devices they want to data mine. If none are selected, default to the devices we are currently using in the lab.
    Signed-off-by: Matthew Stidham <stidmatt@gmail.com>

--- py-scripts/tools/mine_regression_results.py
+++ py-scripts/tools/mine_regression_results.py
@@ -6,23 +6,23 @@ import argparse
 class MineRegression:
     def __init__(self,
                  system_information=None,
-                 save_csv=False):
+                 save_csv=False,
+                 ips=None):
         self.df = None
+        self.ips = ips
         self.system_info = system_information
         self.save_csv = save_csv
 
     def generate_csv(self):
-        ips = ['192.168.92.18', '192.168.92.12', '192.168.93.51', '192.168.92.15']
-        results = [pd.read_html('http://%s/html-reports/latest.html' % url, attrs={'id': 'myTable2'})[0] for url in ips]
+        results = [pd.read_html('http://%s/html-reports/latest.html' % url, attrs={'id': 'myTable2'})[0] for url in self.ips]
         systems = [pd.read_html('http://%s/html-reports/latest.html' % url, attrs={'id': 'SystemInformation'})[0] for
-                   url in ips]
-        for df in range(0, len(ips)):
-            results[df]['IP'] = ips[df]
-            systems[df]['IP'] = ips[df]
-        dfs = [pd.merge(results[n], systems[n], on='IP') for n in range(len(ips))]
+                   url in self.ips]
+        for df in range(0, len(self.ips)):
+            results[df]['IP'] = self.ips[df]
+            systems[df]['IP'] = self.ips[df]
+        dfs = [pd.merge(results[n], systems[n], on='IP') for n in range(len(self.ips))]
         self.df = pd.concat(dfs)
         self.df = self.df[self.df['STDOUT'] == 'STDOUT']
-        print(self.df['IP'].value_counts())
 
     def generate_report(self):
         system_variations = self.df[
@@ -45,11 +45,15 @@ def main():
     parser = argparse.ArgumentParser(description='Compare regression results from different systems')
     parser.add_argument('--system_info', help='location of system information csv', default=None)
     parser.add_argument('--save_csv', help='save CSV of results', default=False)
-
+    parser.add_argument('--ip', help='IP addresses of LANforge devices you want to probe', action='append')
     args = parser.parse_args()
 
+    if args.ip is None:
+        args.ip = ['192.168.92.18', '192.168.92.12', '192.168.93.51', '192.168.92.15']
+
     Miner = MineRegression(system_information=args.system_info,
-                           save_csv=args.save_csv)
+                           save_csv=args.save_csv,
+                           ips=args.ip)
 
     Miner.generate_csv()
 
