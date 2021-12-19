e1bf36624ab2db2666135ee0e1b2a57bedc301eb
Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>
2021-12-18 18:48:35 -0700

    Revert "py-scripts.__init__.py: define __init__ in file, it's just easier"
    This reverts commit 01c2e3ec4c54cf5119ff2ff2ec874cc2ffdbe444.

--- to_pip.sh
+++ to_pip.sh
@@ -88,6 +88,74 @@ sed -i -- 's/from py_json/from .py_json/g' *.py
 
 cd py_scripts || exit 1
 
+echo "#from .connection_test import ConnectionTest
+from .create_bond import CreateBond
+from .create_bridge import CreateBridge
+from .create_chamberview import CreateChamberview
+from .create_l3 import CreateL3
+from .create_l4 import CreateL4
+from .create_macvlan import CreateMacVlan
+from .create_qvlan import CreateQVlan
+from .create_station import CreateStation
+from .create_vap import CreateVAP
+from .csv_convert import CSVParcer
+from .csv_processor import L3CSVParcer
+from .csv_to_influx import CSVtoInflux
+from .csv_to_grafana import UseGrafana
+from .example_security_connection import IPv4Test
+from .grafana_profile import UseGrafana
+from .influx import RecordInflux
+from .lf_ap_auto_test import ApAutoTest
+from .lf_atten_mod_test import CreateAttenuator
+from .lf_csv import lf_csv
+from .lf_dataplane_test import DataplaneTest
+from .lf_dfs_test import FileAdapter, CreateCtlr, L3VariableTime
+from .lf_dut_sta_vap_test import Login_DUT, LoadScenario, CreateSTA_CX
+from .lf_ftp import FtpTest
+from .lf_graph import lf_bar_graph, lf_stacked_graph, lf_horizontal_stacked_graph, lf_scatter_graph, lf_line_graph
+from .lf_mesh_test import MeshTest
+from .lf_multipsk import MultiPsk
+from .lf_report import lf_report
+from .lf_rvr_test import RvrTest
+from .lf_rx_sensitivity_test import RxSensitivityTest
+from .lf_sniff_radio import SniffRadio
+#from .lf_snp_test import  SAME CLASS NAMES AS LF_DFS_TEST
+from .lf_tr398_test import TR398Test
+from .lf_webpage import HttpDownload
+from .lf_wifi_capacity_test import WiFiCapacityTest
+from .measure_station_time_up import MeasureTimeUp
+from .modify_station import ModifyStation
+from .modify_vap import ModifyVAP
+from .run_cv_scenario import RunCvScenario
+from .sta_connect import StaConnect
+from .sta_connect2 import StaConnect2
+from .sta_connect_bssid_mac import client_connect
+from .station_layer3 import STATION
+from .stations_connected import StationsConnected
+from .test_1k_clients_jedtest import Test1KClients
+from .test_client_admission import LoadLayer3
+from .test_fileio import FileIOTest
+from .test_generic import GenTest
+from .test_ip_connection import ConnectTest
+from .test_ip_variable_time import IPVariableTime
+from .test_ipv4_ttls import TTLSTest
+from .test_ipv4_ps import IPV4VariableTime
+#from .test_l3_longevity import L3VariableTime ALSO IN LF_DFS_TEST
+from .test_l3_powersave_traffic import L3PowersaveTraffic
+#from .test_l3_scenario_throughput import
+from .test_l3_unicast_traffic_gen import L3VariableTimeLongevity
+from .test_l3_WAN_LAN import VRTest
+from .test_l4 import IPV4L4
+from .test_status_msg import TestStatusMessage
+#from .test_wanlink import LANtoWAN
+#from .test_wpa_passphrases import WPAPassphrases
+from .testgroup import TestGroup
+from .testgroup2 import TestGroup2
+from .tip_station_powersave import TIPStationPowersave
+from .video_rates import VideoRates
+from .wlan_capacity_calculator import main as WlanCapacityCalculator
+from .ws_generic_monitor_test import WS_Listener" > __init__.py
+
 # Fix files in py_scripts
 sed -i -- 's/import realm/ /g' create_vap.py lf_dut_sta_vap_test.py lf_sniff_radio.py run_cv_scenario.py sta_connect.py station_layer3.py test_client_admission.py
 sed -i -- 's/import realm/from realm import Realm/g' lf_atten_mod_test.py lf_multipsk.py test_fileio.py test_ip_connection.py test_ipv4_ttls.py test_l3_WAN_LAN.py test_l3_unicast_traffic_gen.py test_l4.py testgroup.py
