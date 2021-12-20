8f5d163fd7a1a4acf8cdee9627f679c42d2da76f
Matthew Stidham <stidmatt@gmail.com>
2021-12-20 14:58:59 -0800

    create_virtualenv: Make it executable, fix a typo
    Signed-off-by: Matthew Stidham <stidmatt@gmail.com>

--- create_virtualenv.sh
+++ create_virtualenv.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sudo dnf install virtualenv -y
 su -l lanforge -c "python3 -m venv lanforge_env"
-su -l lanforge -c "echo 'source ~/lanforge_env/bin/actcivate' >> ~/.bashrc"
+su -l lanforge -c "echo 'source ~/lanforge_env/bin/activate' >> ~/.bashrc"
+echo "Please restart your terminal, your virtualenv will activate automtically"
