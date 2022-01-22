f8bf1244d0aec18c28b5075527d7bc43a6b15f45
Matthew Stidham <stidmatt@gmail.com>
2022-01-21 17:10:51 -0800

    test_ip_variable_time: Pass debug to rm_port
    Signed-off-by: Matthew Stidham <stidmatt@gmail.com>

--- py-scripts/test_ip_variable_time.py
+++ py-scripts/test_ip_variable_time.py
@@ -181,7 +181,7 @@ class IPVariableTime(Realm):
         # do not clean up station if existed prior to test
         if not self.use_existing_sta:
             for sta in self.sta_list:
-                self.rm_port(sta, check_exists=True, debug_=False)
+                self.rm_port(sta, check_exists=True, debug_=self.debug)
 
     def cleanup(self):
         self.cx_profile.cleanup()
