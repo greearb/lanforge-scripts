b2333e2e6d350564330409ed08df432747f2f3ed
Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>
2022-04-08 10:34:24 -0600

    lf_tx_power.py : updated the controller location
    Signed-off-by: Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>

--- lf_tx_power.py
+++ lf_tx_power.py
@@ -746,7 +746,7 @@ def main():
     worksheet.write(row, col, 'Regulatory\nDomain', dblue_bold)
     col += 1
     worksheet.set_column(col, col, 16)  # Set width
-    worksheet.write(row, col, 'Controller\n{test_rig}'.format(test_rig=args.test_rig), dblue_bold)
+    worksheet.write(row, col, 'Controller\n{test_rig}\n{location}'.format(test_rig=args.test_rig,location=args.testbed_location), dblue_bold)
     col += 1
     worksheet.set_column(col, col, 20)  # Set width
     worksheet.write(row, col, 'Controller\nChannel', dblue_bold)
