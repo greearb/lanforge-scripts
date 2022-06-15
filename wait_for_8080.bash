3d7d0ba4a3c897c7827af5d28f2d3ed78de48827
Jed Reynolds <jreynolds@candelatech.com>
2022-06-15 11:48:12 -0700

    wait_for_8080: use this file to pause until the GUI can connect
    Signed-off-by: Jed Reynolds <jreynolds@candelatech.com>

---
+++ wait_for_8080.bash
@@ -0,0 +1,38 @@
+#!/bin/bash
+# ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+#  wait_for_8080.bash
+#
+#  Use as a trigger for starting a script that connects to the GUI
+#  Usage: ./wait_for_8080.bash SECONDS URL
+#  Example:
+#   ./wait_for_8080.bash $((60 * 2)) http://localhost:8080/
+#  The URL must be exact and include the http port in the URL.
+# ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+# set -vux
+
+DEFAULT_WAIT_SECS=$(( 60 * 5 )) # 5 minutes
+WAIT_SECS=${1:-$DEFAULT_WAIT_SECS}
+URL=${2:-http://localhost:8080/}
+
+if [[ x$WAIT_SECS = x ]] || (( $WAIT_SECS < 1 )); then
+  echo "Usage: $0 SECONDS URL"
+  echo "The URL must be exact and include the http port in the URL."
+  echo "Example:"
+  echo "    $0 600 http://localhost:8080/"
+  exit 1
+fi
+
+SECS_WAITED=0
+CONNECTED=0
+echo "Waiting up to $WAIT_SECS for $URL" | tee >( logger -t lfclient)
+while (( $SECS_WAITED <= $WAIT_SECS )); do
+  curl -o /dev/null -sq $URL
+  if (( $? == 0 )); then
+    echo "Connected to $URL in $SECS_WAITED" | tee >( logger -t lfclient)
+    exit 0
+  fi
+  sleep 1
+  SECS_WAITED=$(( SECS_WAITED+=1 ))
+done
+echo "Did not connect to $URL within $WAIT_SEC" | tee >( logger -t lfclient)
+exit 1
