d1d68366a7c54c71dc0b6c77c6773e74c0f808f6
Logan Lipke <lolipke@gmail.com>
2022-09-30 15:18:30 -0700

    test_generic.py:  Added the use of standardize_json_results for  simpler monitor results parsing

--- py-scripts/test_generic.py
+++ py-scripts/test_generic.py
@@ -141,68 +141,35 @@ class GenTest(Realm):
         generic_fields = ",".join(generic_cols)
         
         gen_url = "/generic/%s?fields=%s" % (",".join(monitor_endps), generic_fields)
-        endps = self.json_get(gen_url)
-
-        # protects against single endpoint field name being different
-        if 'endpoints' in endps:
-            endpoint_plural = 'endpoints'
-            endps = endps[endpoint_plural]
-            data = {}
-            for endp in endps:
-                data[list(endp.keys())[0]] = list(endp.values())[0]
-            for endpoint in data:
-                kpi_csv.kpi_csv_get_dict_update_time()
-                kpi_csv.kpi_dict['short-description'] = "{endp_name}".format(
-                    endp_name=data[endpoint]['name'])
-                kpi_csv.kpi_dict['numeric-score'] = "{endp_tx}".format(
-                    endp_tx=data[endpoint]['tx bytes'])
-                kpi_csv.kpi_dict['Units'] = "bps"
-                kpi_csv.kpi_dict['Graph-Group'] = "Endpoint TX bytes"
-                kpi_csv.kpi_csv_write_dict(kpi_csv.kpi_dict)
-
-                kpi_csv.kpi_dict['short-description'] = "{endp_name}".format(
-                    endp_name=data[endpoint]['name'])
-                kpi_csv.kpi_dict['numeric-score'] = "{endp_rx}".format(
-                    endp_rx=data[endpoint]['rx bytes'])
-                kpi_csv.kpi_dict['Units'] = "bps"
-                kpi_csv.kpi_dict['Graph-Group'] = "Endpoint RX bytes"
-
-                kpi_csv.kpi_dict['short-description'] = "{endp_name}".format(
-                    endp_name=data[endpoint]['name'])
-                kpi_csv.kpi_dict['numeric-score'] = "{last_results}".format(
-                    last_results=data[endpoint]['last results'])
-                kpi_csv.kpi_dict['Units'] = ""
-                kpi_csv.kpi_dict['Graph-Group'] = "Endpoint Last Results"
-                kpi_csv.kpi_csv_write_dict(kpi_csv.kpi_dict)
-        else:
-            endpoint_plural = 'endpoint'
-            endps = endps[endpoint_plural]
-            data = endps
+        endps = self.generic_endps_profile.standardize_json_results(gen_url)
+        print(endps)
+        data = {}
+        for endp in endps:
+            data[list(endp.keys())[0]] = list(endp.values())[0]
+        for endpoint in data:
             kpi_csv.kpi_csv_get_dict_update_time()
             kpi_csv.kpi_dict['short-description'] = "{endp_name}".format(
-                endp_name=data['name'])
+                endp_name=data[endpoint]['name'])
             kpi_csv.kpi_dict['numeric-score'] = "{endp_tx}".format(
-                endp_tx=data['tx bytes'])
+                endp_tx=data[endpoint]['tx bytes'])
             kpi_csv.kpi_dict['Units'] = "bps"
             kpi_csv.kpi_dict['Graph-Group'] = "Endpoint TX bytes"
             kpi_csv.kpi_csv_write_dict(kpi_csv.kpi_dict)
 
             kpi_csv.kpi_dict['short-description'] = "{endp_name}".format(
-                endp_name=data['name'])
+                endp_name=data[endpoint]['name'])
             kpi_csv.kpi_dict['numeric-score'] = "{endp_rx}".format(
-                endp_rx=data['rx bytes'])
+                endp_rx=data[endpoint]['rx bytes'])
             kpi_csv.kpi_dict['Units'] = "bps"
             kpi_csv.kpi_dict['Graph-Group'] = "Endpoint RX bytes"
 
             kpi_csv.kpi_dict['short-description'] = "{endp_name}".format(
-                endp_name=data['name'])
+                endp_name=data[endpoint]['name'])
             kpi_csv.kpi_dict['numeric-score'] = "{last_results}".format(
-                last_results=data['last results'])
+                last_results=data[endpoint]['last results'])
             kpi_csv.kpi_dict['Units'] = ""
             kpi_csv.kpi_dict['Graph-Group'] = "Endpoint Last Results"
-            kpi_csv.kpi_csv_write_dict(kpi_csv.kpi_dict)
-
-        
+            kpi_csv.kpi_csv_write_dict(kpi_csv.kpi_dict)      
 
         if csv_outfile is not None:
             current_time = time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime())
