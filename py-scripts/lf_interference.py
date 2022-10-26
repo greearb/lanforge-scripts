327cb0f47af37a06c475f3ee8709462bc0db4413
sushant <sushant.bawiskar@candelatech.com>
2022-10-27 02:40:36 +0530

    - reformatted example - removed unwanted comments - replaced test_ip_variable_time2 function call with test_ip_variable_time - made changes in profile flags values to work with recent changes in lf_add_profile.py - refactored code to work with test_ip_variable_time.py script
    Signed-off-by: Sushant Bawiskar <sushant.bawiskar@candelatech.com>

--- py-scripts/lf_interference.py
+++ py-scripts/lf_interference.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
 """
-example:         ./lf_interference.py --mgr 192.168.200.38 --port 8080 --lf_user lanforge --lf_password lanforge
-        --dut_upstream 1.1.eth1 --batch_size 1 --loop_iter 1 --protocol UDP-IPv4 --duration 240000 --download_rate 1Gbps
-         --upload_rate 10Mbps --sort interleave --stations 1.1.sta0000 --create_stations --pull_report --radio "wiphy3"
-         --ssid "NETGEAR-2G" --security "wpa2" --paswd "[BLANK]" --delete_old_scenario --scenario_name "Automation"
-         --vap_radio "wiphy0" --vap_freq "2437" --vap_ssid "routed-AP" --vap_passwd "something" --vap_security "wpa2"
-         --vap_sta_number 1 --vap_sta_radio "wiphy2" --vap_sta_name sta0000 --vap_sta_traffic_type lf_udp
-         --vap_sta_upstream_port 1.1.vap0000 --vap_sta_ssid "routed-AP" --vap_sta_passwd "something"
-         --vap_sta_security "wpa2" --vap_sta_test_duration "60s" --vap_sta_a_min 600000000 --vap_sta_b_min 600000000
-         --vap_sta_mode 5 --vap_sta_cleanup --vap_sta_monitor_interval 10s
+example: ./lf_interference.py --mgr 192.168.200.38 --port 8080 --lf_user lanforge --lf_password lanforge --dut_upstream 1.1.eth1
+        --batch_size 1 --loop_iter 1 --pull_report_flag --protocol UDP-IPv4 --duration 240000 --download_rate 1Gbps
+        --upload_rate 10Mbps --sort interleave --stations 1.1.sta0000 --create_stations --radio "wiphy3" --ssid "ASUS_2G"
+        --security "wpa2" --paswd "Password@123" --delete_old_scenario --scenario_name "Automation" --vap_radio "wiphy0"
+        --vap_freq "2437" --vap_ssid "routed-AP" --vap_passwd "something" --vap_security "wpa2" --vap_sta_number 1
+        --vap_sta_radio "wiphy2" --vap_sta_name sta0000 --vap_sta_traffic_type lf_udp --vap_sta_upstream_port 1.1.vap0000
+        --vap_sta_ssid "routed-AP" --vap_sta_passwd "something" --vap_sta_security "wpa2" --vap_sta_test_duration "60s"
+        --vap_sta_a_min 600000000 --vap_sta_b_min 600000000 --vap_sta_mode 5 --vap_sta_cleanup --vap_sta_monitor_interval 10s
 
 Note: Use this script to create co-channel and adjacent channel interference, by changing frequency argument.
 
@@ -21,269 +20,6 @@ How does it work?
 4. we can notice drop in wifi capacity throughput once layer3 between vap-sta starts
 
 
-sel_port-0: 1.1.eth1
-sel_port-1: 1.1.sta00000
-sel_port-2: 1.1.sta00001
-sel_port-3: 1.1.sta00002
-sel_port-4: 1.1.sta00003
-sel_port-5: 1.1.sta00004
-sel_port-6: 1.1.sta00005
-sel_port-7: 1.1.sta00006
-sel_port-8: 1.1.sta00007
-sel_port-9: 1.1.sta00008
-sel_port-10: 1.1.sta00009
-sel_port-11: 1.1.sta00010
-sel_port-12: 1.1.sta00011
-sel_port-13: 1.1.sta00012
-sel_port-14: 1.1.sta00013
-sel_port-15: 1.1.sta00014
-sel_port-16: 1.1.sta00015
-sel_port-17: 1.1.sta00016
-sel_port-18: 1.1.sta00017
-sel_port-19: 1.1.sta00018
-sel_port-20: 1.1.sta00019
-sel_port-21: 1.1.sta00020
-sel_port-22: 1.1.sta00021
-sel_port-23: 1.1.sta00022
-sel_port-24: 1.1.sta00023
-sel_port-25: 1.1.sta00024
-sel_port-26: 1.1.sta00025
-sel_port-27: 1.1.sta00026
-sel_port-28: 1.1.sta00027
-sel_port-29: 1.1.sta00028
-sel_port-30: 1.1.sta00029
-sel_port-31: 1.1.sta00030
-sel_port-32: 1.1.sta00031
-sel_port-33: 1.1.sta00032
-sel_port-34: 1.1.sta00033
-sel_port-35: 1.1.sta00034
-sel_port-36: 1.1.sta00035
-sel_port-37: 1.1.sta00036
-sel_port-38: 1.1.sta00037
-sel_port-39: 1.1.sta00038
-sel_port-40: 1.1.sta00039
-sel_port-41: 1.1.sta00040
-sel_port-42: 1.1.sta00041
-sel_port-43: 1.1.sta00042
-sel_port-44: 1.1.sta00043
-sel_port-45: 1.1.sta00044
-sel_port-46: 1.1.sta00045
-sel_port-47: 1.1.sta00046
-sel_port-48: 1.1.sta00047
-sel_port-49: 1.1.sta00048
-sel_port-50: 1.1.sta00049
-sel_port-51: 1.1.sta00500
-sel_port-52: 1.1.sta00501
-sel_port-53: 1.1.sta00502
-sel_port-54: 1.1.sta00503
-sel_port-55: 1.1.sta00504
-sel_port-56: 1.1.sta00505
-sel_port-57: 1.1.sta00506
-sel_port-58: 1.1.sta00507
-sel_port-59: 1.1.sta00508
-sel_port-60: 1.1.sta00509
-sel_port-61: 1.1.sta00510
-sel_port-62: 1.1.sta00511
-sel_port-63: 1.1.sta00512
-sel_port-64: 1.1.sta00513
-sel_port-65: 1.1.sta00514
-sel_port-66: 1.1.sta00515
-sel_port-67: 1.1.sta00516
-sel_port-68: 1.1.sta00517
-sel_port-69: 1.1.sta00518
-sel_port-70: 1.1.sta00519
-sel_port-71: 1.1.sta00520
-sel_port-72: 1.1.sta00521
-sel_port-73: 1.1.sta00522
-sel_port-74: 1.1.sta00523
-sel_port-75: 1.1.sta00524
-sel_port-76: 1.1.sta00525
-sel_port-77: 1.1.sta00526
-sel_port-78: 1.1.sta00527
-sel_port-79: 1.1.sta00528
-sel_port-80: 1.1.sta00529
-sel_port-81: 1.1.sta00530
-sel_port-82: 1.1.sta00531
-sel_port-83: 1.1.sta00532
-sel_port-84: 1.1.sta00533
-sel_port-85: 1.1.sta00534
-sel_port-86: 1.1.sta00535
-sel_port-87: 1.1.sta00536
-sel_port-88: 1.1.sta00537
-sel_port-89: 1.1.sta00538
-sel_port-90: 1.1.sta00539
-sel_port-91: 1.1.sta00540
-sel_port-92: 1.1.sta00541
-sel_port-93: 1.1.sta00542
-sel_port-94: 1.1.sta00543
-sel_port-95: 1.1.sta00544
-sel_port-96: 1.1.sta00545
-sel_port-97: 1.1.sta00546
-sel_port-98: 1.1.sta00547
-sel_port-99: 1.1.sta00548
-sel_port-100: 1.1.sta00549
-sel_port-101: 1.1.sta01000
-sel_port-102: 1.1.sta01001
-sel_port-103: 1.1.sta01002
-sel_port-104: 1.1.sta01003
-sel_port-105: 1.1.sta01004
-sel_port-106: 1.1.sta01005
-sel_port-107: 1.1.sta01006
-sel_port-108: 1.1.sta01007
-sel_port-109: 1.1.sta01008
-sel_port-110: 1.1.sta01009
-sel_port-111: 1.1.sta01010
-sel_port-112: 1.1.sta01011
-sel_port-113: 1.1.sta01012
-sel_port-114: 1.1.sta01013
-sel_port-115: 1.1.sta01014
-sel_port-116: 1.1.sta01015
-sel_port-117: 1.1.sta01016
-sel_port-118: 1.1.sta01017
-sel_port-119: 1.1.sta01018
-sel_port-120: 1.1.sta01019
-sel_port-121: 1.1.sta01020
-sel_port-122: 1.1.sta01021
-sel_port-123: 1.1.sta01022
-sel_port-124: 1.1.sta01023
-sel_port-125: 1.1.sta01024
-sel_port-126: 1.1.sta01025
-sel_port-127: 1.1.sta01026
-sel_port-128: 1.1.sta01027
-sel_port-129: 1.1.sta01028
-sel_port-130: 1.1.sta01029
-sel_port-131: 1.1.sta01030
-sel_port-132: 1.1.sta01031
-sel_port-133: 1.1.sta01032
-sel_port-134: 1.1.sta01033
-sel_port-135: 1.1.sta01034
-sel_port-136: 1.1.sta01035
-sel_port-137: 1.1.sta01036
-sel_port-138: 1.1.sta01037
-sel_port-139: 1.1.sta01038
-sel_port-140: 1.1.sta01039
-sel_port-141: 1.1.sta01040
-sel_port-142: 1.1.sta01041
-sel_port-143: 1.1.sta01042
-sel_port-144: 1.1.sta01043
-sel_port-145: 1.1.sta01044
-sel_port-146: 1.1.sta01045
-sel_port-147: 1.1.sta01046
-sel_port-148: 1.1.sta01047
-sel_port-149: 1.1.sta01048
-sel_port-150: 1.1.sta01049
-sel_port-151: 1.1.sta01500
-sel_port-152: 1.1.sta01501
-sel_port-153: 1.1.sta01502
-sel_port-154: 1.1.sta01503
-sel_port-155: 1.1.sta01504
-sel_port-156: 1.1.sta01505
-sel_port-157: 1.1.sta01506
-sel_port-158: 1.1.sta01507
-sel_port-159: 1.1.sta01508
-sel_port-160: 1.1.sta01509
-sel_port-161: 1.1.sta01510
-sel_port-162: 1.1.sta01511
-sel_port-163: 1.1.sta01512
-sel_port-164: 1.1.sta01513
-sel_port-165: 1.1.sta01514
-sel_port-166: 1.1.sta01515
-sel_port-167: 1.1.sta01516
-sel_port-168: 1.1.sta01517
-sel_port-169: 1.1.sta01518
-sel_port-170: 1.1.sta01519
-sel_port-171: 1.1.sta01520
-sel_port-172: 1.1.sta01521
-sel_port-173: 1.1.sta01522
-sel_port-174: 1.1.sta01523
-sel_port-175: 1.1.sta01524
-sel_port-176: 1.1.sta01525
-sel_port-177: 1.1.sta01526
-sel_port-178: 1.1.sta01527
-sel_port-179: 1.1.sta01528
-sel_port-180: 1.1.sta01529
-sel_port-181: 1.1.sta01530
-sel_port-182: 1.1.sta01531
-sel_port-183: 1.1.sta01532
-sel_port-184: 1.1.sta01533
-sel_port-185: 1.1.sta01534
-sel_port-186: 1.1.sta01535
-sel_port-187: 1.1.sta01536
-sel_port-188: 1.1.sta01537
-sel_port-189: 1.1.sta01538
-sel_port-190: 1.1.sta01539
-sel_port-191: 1.1.sta01540
-sel_port-192: 1.1.sta01541
-sel_port-193: 1.1.sta01542
-sel_port-194: 1.1.sta01543
-sel_port-195: 1.1.sta01544
-sel_port-196: 1.1.sta01545
-sel_port-197: 1.1.wlan4
-sel_port-198: 1.1.wlan5
-sel_port-199: 1.1.wlan6
-sel_port-200: 1.1.wlan7
-show_events: 1
-show_log: 0
-port_sorting: 0
-kpi_id: WiFi Capacity
-bg: 0xE0ECF8
-test_rig:
-show_scan: 1
-auto_helper: 1
-skip_2: 0
-skip_5: 0
-skip_5b: 1
-skip_dual: 0
-skip_tri: 1
-batch_size: 1
-loop_iter: 1
-duration: 6000
-test_groups: 0
-test_groups_subset: 0
-protocol: UDP-IPv4
-dl_rate_sel: Total Download Rate:
-dl_rate: 1000000000
-ul_rate_sel: Total Upload Rate:
-ul_rate: 10000000
-prcnt_tcp: 100000
-l4_endp:
-pdu_sz: -1
-mss_sel: 1
-sock_buffer: 0
-ip_tos: 0
-multi_conn: -1
-min_speed: -1
-ps_interval: 60-second Running Average
-fairness: 0
-naptime: 0
-before_clear: 5000
-rpt_timer: 1000
-try_lower: 0
-rnd_rate: 1
-leave_ports_up: 0
-down_quiesce: 0
-udp_nat: 1
-record_other_ssids: 0
-clear_reset_counters: 0
-do_pf: 0
-pf_min_period_dl: 0
-pf_min_period_ul: 0
-pf_max_reconnects: 0
-use_mix_pdu: 0
-pdu_prcnt_pps: 1
-pdu_prcnt_bps: 0
-pdu_mix_ln-0:
-show_scan: 1
-show_golden_3p: 0
-save_csv: 0
-show_realtime: 1
-show_pie: 1
-show_per_loop_totals: 1
-show_cx_time: 1
-show_dhcp: 1
-show_anqp: 1
-show_4way: 1
-show_latency: 1
 
 """
 import subprocess
@@ -319,7 +55,7 @@ lfcli_base = importlib.import_module("py-json.LANforge.lfcli_base")
 LFCliBase = lfcli_base.LFCliBase
 LFRequest = importlib.import_module("py-json.LANforge.LFRequest")
 
-ipvt = importlib.import_module("py-scripts.test_ip_variable_time2")
+ipvt = importlib.import_module("py-scripts.test_ip_variable_time")
 IPVariableTime = ipvt.IPVariableTime
 
 logger = logging.getLogger(__name__)
@@ -366,7 +102,7 @@ class interference(cv_test):
             _mac_pattern=None,  # Optional MAC-Address pattern, for instance: xx:xx:xx:*:*:xx
             _name=scenario_name,  # Profile Name. [R]
             _passwd=vap_pawd,  # WiFi Password to be used (AP Mode), [BLANK] means no password.
-            _profile_flags=4105,  # Flags for this profile, see above.
+            _profile_flags="0x1009",  # Flags for this profile, see above.
             _profile_type="routed_ap",  # Profile type: See above. [W]
             _ssid=vap_ssid,  # WiFi SSID to be used, [BLANK] means any.
             _vid=None,  # Vlan-ID (only valid for vlan profiles).
@@ -523,15 +259,13 @@ class interference(cv_test):
                                      traffic_type=traffic_type,
                                      _debug_on=debug)
 
-        for i in range(len(radio)):
-            ip_var_test.build(i)
-            ip_var_test.create_cx_profile(i)
+        ip_var_test.build()
+
 
         return ip_var_test
 
     def run_layer3(self, ip_var_test):
-        ip_var_test.run()
-
+        ip_var_test.run_only()
 
 def main():
     parser = argparse.ArgumentParser(
@@ -747,7 +481,6 @@ def main():
     proc = subprocess.Popen([cmd_wifi_capacity], shell=True, stdin=None, stdout=None, stderr=None, close_fds=True)
     time.sleep(120)
     lf_interference.run_layer3(obj)
-
 
 if __name__ == "__main__":
     main()
