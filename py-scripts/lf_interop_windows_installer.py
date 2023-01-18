e2c580f09d52290cfa91d5cf87d51b068953f0ff
Amrit Raj <rajamrit.cs@gmail.com>
2023-01-18 17:21:21 +0530

    lf_interop_windows_installer.py : Added upgrade option, version selection and removed java auto installation

--- py-scripts/lf_interop_windows_installer.py
+++ py-scripts/lf_interop_windows_installer.py
@@ -5,14 +5,14 @@ NAME: lf_interop_windows_installer.py
 
 
 PURPOSE:
-    This program is used Installin the Lanforge InterOp Windows app in Windows Laptop that are connected to Lanforge using ethernet.
+    This program is used Installing the Lanforge InterOp Server Windows app in Windows Laptop that are connected to Lanforge using ethernet.
 
-EXAMPLE: ./python3 lf_interop_windows_installer.py  --host 192.168.200.253 --manager_ip 192.168.200.232 --realm=75
-         --resource=4 --machine_user Test2 --machine_pass 9090
+EXAMPLE: ./ python3 lf_interop_windows_installer.py  --host 192.168.200.253 --manager_ip 192.168.200.232 --realm=75
+            --resource=4 --version=5.4.6 --action=install --machine_user Test2 --machine_pass 90900
 
 Note: To Run this script
     The Windows machine should have ssh installed on the machine before running this script
-    The Windows machine should have  jdk and VLC installed on it
+    The Windows machine should also have jdk and VLC pre installed on it
 
 LICENSE:
     Copyright 2021 Candela Technologies Inc
@@ -35,13 +35,18 @@ class WinAppInstaller:
                  machine_user='test1',
                  machine_password='password',
                  host="localhost",
+                 version="5.4.6",
+                 action="install",
                  _exit_on_fail=False):
+        self.mgt_dev = None
+        self.version = version
         self.manager_ip = manager_ip
         self.resource = resource
         self.realm = realm
         self.host = host
         self.machine_user = machine_user
         self.machine_password = machine_password
+        self.action = action
 
         if self.host == "localhost":
             hostname = socket.gethostname()
@@ -52,25 +57,34 @@ class WinAppInstaller:
         self.client.connect(self.host, username=self.machine_user, password=self.machine_password)
 
     def start(self):
-        pass
+        if self.action == "install":
+            self.install_apk()
+            self.configure()
+        if self.action == "uninstall":
+            self.uninstall()
+        if self.action == "upgrade":
+            self.upgrade(self.version)
+        if self.action == "configure":
+            self.configure()
 
     def install_apk(self):
-        stdin, stdout, stderr = self.client.exec_command(
-            "powershell.exe wget https://download.oracle.com/java/19/latest/jdk-19_windows-x64_bin.exe -O 'C:\jdk.exe'")
-        output = stdout.readline().strip()
-        print("[INSTALLATION] ", output)
+        # Removed Installing JDK as default As of now
+        # stdin, stdout, stderr = self.client.exec_command(
+        #     "powershell.exe wget https://download.oracle.com/java/19/latest/jdk-19_windows-x64_bin.exe -O 'C:\jdk.exe'")
+        # output = stdout.readline().strip()
+        # print("[INSTALLATION] ", output)
+        #
+        # stdin, stdout, stderr = self.client.exec_command(
+        #     "powershell.exe Start-Process -Wait -FilePath 'C:\jdk.exe' -ArgumentList '/S','/v','/qn' -passthru")
+        # output = stdout.readline().strip()
+        # print("[INSTALLATION] ", output)
+        #
+        # stdin, stdout, stderr = self.client.exec_command("del  'C:\jdk.exe'")
+        # output = stdout.readline().strip()
+        # print("[INSTALLATION] ", output)
 
         stdin, stdout, stderr = self.client.exec_command(
-            "powershell.exe Start-Process -Wait -FilePath 'C:\jdk.exe' -ArgumentList '/S','/v','/qn' -passthru")
-        output = stdout.readline().strip()
-        print("[INSTALLATION] ", output)
-
-        stdin, stdout, stderr = self.client.exec_command("del  'C:\jdk.exe'")
-        output = stdout.readline().strip()
-        print("[INSTALLATION] ", output)
-
-        stdin, stdout, stderr = self.client.exec_command(
-            "powershell.exe wget http://candelatech.com/private/downloads/r5.4.6/LANforge-Server-5.4.6-Installer.exe -O 'C:\Lanforge.exe'")
+            "powershell.exe wget http://candelatech.com/private/downloads/r5.4.6/LANforge-Server-" + self.version + "-Installer.exe -O 'C:\Lanforge.exe'")
         output = stdout.readline().strip()
         print("[INSTALLATION] ", output)
 
@@ -81,22 +95,39 @@ class WinAppInstaller:
 
         stdin, stdout, stderr = self.client.exec_command("del 'C:\\Lanforge.exe'")
         output = stdout.readline().strip()
-        print("[INSTALLATION] ", output)
+        # print("[DELETION] Removing the exe file form the machine", output)
 
         print("[SUCCESS] Installation Success\n")
-        # self.client.close()
+
+    def upgrade(self, version):
+        print("[UPGRADE] Uninstalling the LANForge server")
+        self.uninstall()
+        stdin, stdout, stderr = self.client.exec_command(
+            "powershell.exe wget http://candelatech.com/private/downloads/r5.4.6/LANforge-Server-" + version + "-Installer.exe -O 'C:\Lanforge.exe'")
+        output = stdout.readline().strip()
+        print("[UP-GRADATION] ", output)
+
+        stdin, stdout, stderr = self.client.exec_command(
+            "powershell.exe Start-Process -Wait -FilePath 'C:\\Lanforge.exe' -ArgumentList '/S','/v','/qn' -passthru ")
+        output = stdout.readline().strip()
+        # print("[UP-GRADATION] ", output)
+
+        stdin, stdout, stderr = self.client.exec_command("del 'C:\\Lanforge.exe'")
+        output = stdout.readline().strip()
+        print("[UP-GRADATION] ", output)
+
+        print("[UPGRADE] Upgraded Successfully to version " + version + "\n")
 
     def configure(self):
         stdin, stdout, stderr = self.client.exec_command("echo %USERNAME%")
-        self.user = (stdout.readline()).strip()
-        configure_file = "C:\\Users\\" + self.user + "\\AppData\\Roaming\\config.values"
-        configure_file1 = "C:\\Users\\" + self.user + "\\AppData\\Roaming\\config1.txt"
+        user = (stdout.readline()).strip()
+        configure_file = "C:\\Users\\" + user + "\\AppData\\Roaming\\config.values"
         # C:\Users\test2\AppData\Roaming\config.values
         # Getting management devices
         self.mgt_dev = ""
         stdin, stdout, stderr = self.client.exec_command("more " + configure_file)
-        user = (stdout.readlines())
-        for i in user:
+        pre_config_text = (stdout.readlines())
+        for i in pre_config_text:
             if "mgt_dev" in i.strip():
                 self.mgt_dev = i.strip().split(" ")[1]
 
@@ -110,7 +141,7 @@ class WinAppInstaller:
         stdin, stdout, stderr = self.client.exec_command("echo " + "gps_dev none" + " >> " + configure_file)
         stdin, stdout, stderr = self.client.exec_command("echo " + "keepalive 30000" + " >> " + configure_file)
         stdin, stdout, stderr = self.client.exec_command(
-            "echo " + "log_dir c:\\users\\" + self.user + "\\appdata\\local\\temp" + " >> " + configure_file)
+            "echo " + "log_dir c:\\users\\" + user + "\\appdata\\local\\temp" + " >> " + configure_file)
         stdin, stdout, stderr = self.client.exec_command("echo " + "log_file_len 0" + " >> " + configure_file)
         stdin, stdout, stderr = self.client.exec_command("echo " + "log_level 7" + " >> " + configure_file)
         stdin, stdout, stderr = self.client.exec_command("echo " + "max_tx 5" + " >> " + configure_file)
@@ -118,38 +149,39 @@ class WinAppInstaller:
         stdin, stdout, stderr = self.client.exec_command("echo " + "mgt_dev " + self.mgt_dev + " >> " + configure_file)
         stdin, stdout, stderr = self.client.exec_command("echo " + "mode resource" + " >> " + configure_file)
         print("[CONFIG] Configuring realm id")
-        stdin, stdout, stderr = self.client.exec_command("echo " + "realm " + self.realm + " >> " + configure_file)
+        stdin, stdout, stderr = self.client.exec_command("echo " + "realm " + str(self.realm) + " >> " + configure_file)
         print("[CONFIG] Configuring resource id")
         stdin, stdout, stderr = self.client.exec_command(
-            "echo " + "resource " + self.resource + " >> " + configure_file)
+            "echo " + "resource " + str(self.resource) + " >> " + configure_file)
         print("[CONFIG] Configuring shelf id")
         stdin, stdout, stderr = self.client.exec_command("echo " + "shelf 1" + " >> " + configure_file)
         stdin, stdout, stderr = self.client.exec_command("echo " + "wl_probe_timer 50" + " >> " + configure_file)
+        print("[SUCCESS] Successfully configured")
 
     def copy(self):
         sftp = self.client.open_sftp()
         sftp.put('/home/lanforge/Desktop/MonkeyRemote-0.5.jar', 'C:\\Users\\Test2\\Desktop')
         sftp.close()
 
-    def Uninstall(self):
-        print("Trying to Uninstall the LANforge Server")
+    def uninstall(self):
+        print("[UNINSTALLING] Trying to Uninstall the LANforge Server from machine with ip " + self.host)
         try:
             stdin, stdout, stderr = self.client.exec_command(
                 "powershell.exe Start-Process -Wait -FilePath 'C:\\Program Files (x86)\\LANforge-Server\\Uninstall.exe' -ArgumentList '/S','/v','/qn' -passthru")
-            user = (stdout.readline()).strip()
-            print(user)
-            print("Success fully Uninstalled")
+            output = (stderr.readline()).strip()
+            # Start-Process : This command cannot be run due to the error: The system cannot find the file specified.
+            print(output)
+            print("[UNINSTALLED] Success fully Uninstalled")
         except:
             print("App Not installed")
 
-    def start(self):
-        stdin, stdout, stderr = self.client.exec_command(
-            "powershell.exe Start-Process -Wait -FilePath 'C:\\Program Files (x86)\\LANforge-Server\\Uninstall.exe'")
-        user = (stdout.readline()).strip()
-        print(user)
 
     def get_all_windows_ip(self):
         pass
+
+    def close_session(self):
+        print("[END] Closing the ssh Session")
+        self.client.close()
 
 
 def main():
@@ -157,15 +189,17 @@ def main():
                                                  "all the phones before using this script)")
     parser.add_argument('--host', help='Lanforge IP address on which all the phones are connected', required=True,
                         default='localhost')
-
     parser.add_argument('--manager_ip', help='Manager IP address for clustering Eg. 192.168.200.14', required=True,
                         default='localhost')
-    parser.add_argument('--realm', help='Realm address for clustering Eg. 75', required=True, default='25')
-    parser.add_argument('--resource', help='Resource address for clustering Eg. 4', required=True, default='2')
-
-    parser.add_argument('--machine_user', help='Windows machine username Eg. user1', required=True, default='user')
+    parser.add_argument('--version', help='Version of the LANforge address for clustering Eg. 5.4.6', required=True,
+                        default='2')
+    parser.add_argument('--action', help='What action you want to perform Eg. install, uninstall, upgrade ',
+                        required=True, default='2')
     parser.add_argument('--machine_pass', help='Windows machine Password Eg. password', required=True,
                         default='password')
+    parser.add_argument('--machine_user', help='Windows machine username Eg. user1', required=True, default='user')
+    parser.add_argument('--realm', help='Realm address for clustering Eg. 75', default='25')
+    parser.add_argument('--resource', help='Resource address for clustering Eg. 4', default='2')
 
     args = parser.parse_args()
     app_installer = WinAppInstaller(host=args.host,
@@ -173,8 +207,11 @@ def main():
                                     realm=args.realm,
                                     resource=args.resource,
                                     machine_user=args.machine_user,
-                                    machine_password=args.machine_pass)
-    app_installer.install_apk()
+                                    machine_password=args.machine_pass,
+                                    version=args.version,
+                                    action=args.action)
+    app_installer.start()
+    app_installer.close_session()
 
 
 if __name__ == "__main__":
