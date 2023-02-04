cb3f934b4f88faec2951cd16b29acd0c61505f75
Jed Reynolds <jed@bitratchet.com>
2023-02-03 17:39:49 -0800

    example_turntable_ctrl.py: is now able to set turntable position an print turntable data
    Signed-off-by: Jed Reynolds <jed@bitratchet.com>

--- py-scripts/sandbox/example_turntable_ctrl.py
+++ py-scripts/sandbox/example_turntable_ctrl.py
@@ -3,6 +3,7 @@ import logging
 import os
 import sys
 import time
+
 """
 Script is a demonstration of how to operate a turn-table using the CLI admin command.
 """
@@ -13,6 +14,10 @@ if sys.version_info[0] != 3:
 import importlib
 import argparse
 import pprint
+import sys
+import logging
+
+sys.path.insert(1, "../")
 sys.path.insert(1, "../../")
 
 if "SHELL" in os.environ.keys():
@@ -20,33 +25,146 @@ if "SHELL" in os.environ.keys():
     from lanforge_client.lanforge_api import LFSession
     from lanforge_client.lanforge_api import LFJsonCommand
     from lanforge_client.lanforge_api import LFJsonQuery
+    realm = importlib.import_module("py-json.realm")
+    Realm = realm.Realm
 else:
     import lanforge_api
     from lanforge_api import LFJsonCommand
     from lanforge_api import LFJsonQuery
+    import realm
+    from realm import Realm
+
+lf_logger_config = importlib.import_module("py-scripts.lf_logger_config")
+logger = logging.getLogger(__name__)
 
 
-
-class turntable:
+class Turntable(Realm):
     UNKNOWN_POSITION : int = -999
+    CURRENT_ANGLE : str = "reported rotation (deg)"
 
     def __init__(self,
-                 api_session : lanforge_api.LFSession = None,
+                 api_session: lanforge_api.LFSession = None,
                  debug=False):
+        super().__init__(lfclient_host=api_session.get_lfclient_host(),
+                         lfclient_port=api_session.get_lfclient_port(),
+                         debug_=debug,
+                         _exit_on_error=True,
+                         _exit_on_fail=False)
+
         self.api_session = api_session
         self.api_command = api_session.get_command()
         self.api_query = api_session.get_query()
-        self.chamber_name = None
-        self.turntable_position = turntable.UNKNOWN_POSITION
+        self.chamber_name: str = None
+        self.turntable_position: float = Turntable.UNKNOWN_POSITION
         self.errors_warnings = []
-        self.located_chamber = None
+        self.located_chamber: dict = None
+        self.speed: float = self.UNKNOWN_POSITION
+        self.angle: float = self.UNKNOWN_POSITION
+        self.adjust: float = self.UNKNOWN_POSITION
+        self.found_chamber: bool = None
+        self.no_settle: bool = False
 
-    def locate_chamber(self, chamber_name : str = None) -> bool:
+    def build(self):
+        pass
+
+    def start(self, no_settle=None):
+        self.api_session.logger.info("Locating chamber [%s]" % self.chamber_name)
+        if not self.locate_chamber(chamber_name=self.chamber_name):
+            self.api_session.logger.error("Unable to locate chamber, bye")
+            sys.exit(1)
+        # pprint.pprint(self.found_chamber)
+        if not self.speed or self.speed == Turntable.UNKNOWN_POSITION:
+            self.speed = float(self.found_chamber["rpm"])
+
+        if self.adjust is None or self.adjust == Turntable.UNKNOWN_POSITION:
+            self.adjust = 0
+
+        if self.angle is None or self.angle == Turntable.UNKNOWN_POSITION:
+            print("self.angle[%s] assigning angle to [%s][%s]" %
+                (self.angle,
+                 self.found_chamber[Turntable.CURRENT_ANGLE],
+                 float(self.found_chamber[Turntable.CURRENT_ANGLE])))
+            self.angle = float(self.found_chamber[Turntable.CURRENT_ANGLE])
+
+        if self.adjust != 0:
+            if (self.angle + self.adjust) < 0:
+                self.angle = 360 + (self.angle + self.adjust)
+            elif (self.angle + self.adjust) > 359.9:
+                if (self.angle + self.adjust) == 360:
+                    self.angle = 0
+                else:
+                    self.angle = (self.angle + self.adjust) - 360
+            else:
+                self.angle += self.adjust
+        else:
+            if self.angle < 0:
+                self.angle = 360 + self.angle
+            elif self.angle > 359.9:
+                if self.angle == 360:
+                    self.angle = 0
+                else:
+                    self.angle = self.angle - 360
+        self.api_session.logger.info("Setting new chamber position")
+        self.api_command.post_set_chamber(chamber=self.chamber_name,
+                                          speed_rpm=self.speed,
+                                          position=self.angle)
+        time.sleep(0.125)
+        if no_settle is False:
+            logging.warning("not waiting for chamber settings to take effect")
+            return
+        if float(self.found_chamber[Turntable.CURRENT_ANGLE]) == float(self.angle):
+            logging.warning("requested angle %s, current reported angle is %s" % (self.angle, self.found_chamber[Turntable.CURRENT_ANGLE]))
+            return
+
+        max_wait_ms: int = 20000
+        check_ms: int = 250
+        last_angle: float = float(self.found_chamber[Turntable.CURRENT_ANGLE])
+        start_ms: int = lanforge_api._now_ms()
+        until_ms: int = start_ms + max_wait_ms
+        now_ms: int = start_ms
+        while now_ms <= until_ms:
+            if not self.locate_chamber(self.chamber_name):
+                print("chamber %s disappeared" % self.chamber_name)
+                sys.exit(1)
+            self.api_session.logger.info("Position %s dT %s" %
+                         (float(self.found_chamber[Turntable.CURRENT_ANGLE]),
+                          until_ms - now_ms))
+            if last_angle != float(self.found_chamber[Turntable.CURRENT_ANGLE]):
+                last_angle = float(self.found_chamber[Turntable.CURRENT_ANGLE])
+            if last_angle == self.angle:
+                self.api_session.logger.warning("target angle %s reached in %s ms" %
+                                (self.angle, (now_ms - start_ms)))
+                break
+            time.sleep(check_ms / 1000)
+            now_ms = lanforge_api._now_ms()
+
+        logger.warning("done")
+
+    def set_speed(self, speed:float=None):
         """
+        Set speed in RPM
+        """
+        self.speed = speed
+    def set_angle(self, angle:float=None):
+        """
+        Sets the absolute rotation angle of the table
+        """
+        logger.warning("setting angle to %s" % angle)
+        self.angle = angle
 
+    def adjust_angle(self, angle:float=None):
+        """
+        Add or subtract from the current angle of the table
+        """
+        self.adjust = angle
+
+    def locate_chamber(self, chamber_name: str = None) -> bool:
+        """
+        Sets self.found_chamber with named chamber
         :param chamber_name: name/eid of chamber
         :return: False if unable to identify chamber and determine it has a turntable,
         True if able to identify chamber with a turntable
+
         """
         if not chamber_name:
             self.api_session.logger.error("No chamber name")
@@ -58,30 +176,44 @@ class turntable:
             self.api_session.logger.error("No chambers found")
             return False
 
-        pprint(["located chamber might be this:", result])
-
+        if self.debug:
+            pprint.pprint(["located chamber might be this:", result])
+        self.chamber_name = result["chamber"]
+        self.found_chamber = result
         return True
+
 
 def main():
     """
-    This is really just a simple loop
-
-    :return: void
+    Control a Turntable
     """
     parser = argparse.ArgumentParser(
         prog=__file__,
         formatter_class=argparse.RawTextHelpFormatter,
-        description='tests creating wanlink')
+        description='operate a turntable')
     parser.add_argument("--host", "--mgr", help='specify the GUI to connect to, assumes port 8080')
-    parser.add_argument("--debug", help='turn on debugging', action="store_true")
-    parser.add_argument("--chamber",
-                        help="""Chamber EID (1.1.1 or 1.1.Chamber-0). Turntables are features of ChamberView chambers.
-    There can be multiple chambers per resource, just like there can be multiple virtual routers per resource.
-    Do NOT use the name of the turntable you see in ChamberView (C0), use the full name
-    of the chamber from the Modify Chamber window (Chamber-0).""")
-    parser.add_argument("--info", help="Display chamber turntable information")
-    parser.add_argument("--angle", help="Turn table angle. Angle resolution is 1/10th of a degree.")
-    parser.add_argument("--speed", help="Turn table rotation speed, in RPM. Minimum is 0.1 RPM, maximum is 7 RPM")
+    parser.add_argument("--debug", action="store_true",
+                        help='turn on debugging')
+    parser.add_argument("--chamber", type=str,
+                        help="""Chamber name (e.g. Chamber-0). Turntables are features of ChamberView chambers.
+    There can be multiple resources per chamber, just like there can be multiple virtual routers per resource.
+    Do NOT use the short name of the chamber or turntable you see in ChamberView (C0), use the FULL NAME
+    of the turntable or chamber from the Modify Chamber window (Chamber-0).""")
+    parser.add_argument("--info", default=False, action="store_true",
+                        help="Display chamber turntable information")
+    parser.add_argument("--angle", type=float,
+                        help="""Set the turn table angle to an absolute position (between 0.0 and 359.9).
+     Angle resolution is 1/10th of a degree.""")
+    parser.add_argument("--adjust", type=float,
+                        help="""Adjust the turn table angle a few degrees relative to current position.
+    Negative degrees turn the table clockwise. Positive degrees turn the table anti-clockwise.
+    If the table is at 270deg, '--adjust -5' will set the position of the table to 265deg.
+     Angle resolution is 1/10th of a degree.""")
+    parser.add_argument("--speed", "--rpm", type=float,
+                        help="Turn table rotation speed, in RPM. Minimum is 0.1 RPM, maximum is 7 RPM")
+    parser.add_argument("--log_level")
+    parser.add_argument("--no_settle", default=False, action="store_true",
+                        help="Exit script before turntable reports no movement for more than two pollings.")
 
     args = parser.parse_args()
 
@@ -92,12 +224,33 @@ def main():
                                      stream_warnings=True,
                                      require_session=True,
                                      exit_on_error=True)
-    this_turntable = turntable(api_session=session, debug=args.debug)
+    session.logger.enable(reserved_tag="json_get")
+    session.logger.enable(reserved_tag="json_post")
 
+    if not args.chamber:
+        session.logger.error("No chamber name provided")
+        sys.exit(1)
+    this_turntable = Turntable(api_session=session, debug=args.debug)
     if not this_turntable.locate_chamber(chamber_name=args.chamber):
         print("Unable to find chamber [%s]" % args.chamber)
         sys.exit(1)
+    if args.info and args.info is True:
+        #print("==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ")
+        pprint.pprint(this_turntable.found_chamber)
+        #print("==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ")
+        sys.exit(0)
 
+    wait_to_settle: bool = True
+    if args.no_settle and args.no_settle is False:
+        wait_to_settle = args.no_settle
+
+    if args.speed is not None:
+        this_turntable.set_speed(args.speed)
+    if args.angle is not None:
+        this_turntable.set_angle(args.angle)
+    elif args.adjust is not None:
+        this_turntable.adjust_angle(args.adjust)
+    this_turntable.start(no_settle=wait_to_settle)
 
 
 if __name__ == "__main__":
