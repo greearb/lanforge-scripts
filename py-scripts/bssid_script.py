2aaa631c8351ee4b86b269eff6590e94a5dcde0d
Dipti Dhond <dipti.dhond@candelatech.com>
2023-03-08 15:58:53 -0800

    finish implementing the rest of the bssid_script. Added --dut_name argument. Took sta_scan_test pandas dataframe, put all bssids into a list, takes bssids from list and put into strings, concats strings to make create_chamberview_dut python script. Then run create_chamberview_dut script as a subprocess. WIP on the string concatentation of the create_chamberview_dut command
    Signed-off-by: Dipti Dhond <dipti.dhond@candelatech.com>

--- py-scripts/bssid_script.py
+++ py-scripts/bssid_script.py
@@ -1,31 +1,24 @@
 #!/usr/bin/env python3
-import sys
-import os
 import importlib
 import argparse
-import time
-import json
-from os import path
 import sta_scan_test
 import pandas as pd
 import subprocess
+import sys
+import os
 
-sys.path.append(os.path.join(os.path.abspath(__file__ + "../../../")))
 realm = importlib.import_module("py-json.realm")
 Realm = realm.Realm
 LFUtils = importlib.import_module("py-json.LANforge.LFUtils")
+sys.path.append(os.path.join(os.path.abspath(__file__ + "../../../")))
 
 if sys.version_info[0] != 3:
     print("This script requires Python 3")
     exit(1)
 
-#def get_bssid_from_df(self, dataframe):
-#    run_dut_bash_command=""
-#    subprocess.run
-
 def main():
     parser = Realm.create_basic_argparse(
-        prog='sta_scan_test.py',
+        prog='bssid_script.py',
         formatter_class=argparse.RawTextHelpFormatter,
         epilog='''\
         Used to scan for ssids after creating a station
@@ -48,7 +41,7 @@ def main():
                         default=None)
     parser.add_argument('--scan_time', help='Specify time in seconds to wait for scan to complete.  Default is 15',
                         default=15, type=int)
-
+    parser.add_argument('--dut_name', help='Specify the name the new DUT should be called. Default name is bssid_dut.', default="bssid_dut")
     args = parser.parse_args()
 
     station_list = args.sta_name
@@ -70,23 +63,41 @@ def main():
                             _debug_on=args.debug)
 
     if (not args.use_existing_station):
-        sta_scan.sta_scan_test.pre_cleanup()
+        sta_scan.pre_cleanup()
 
-        sta_scan.sta_scan_test.build()
+        sta_scan.build()
         # exit()
-        if not sta_scan.sta_scan_test.passes():
-            print(sta_scan.sta_scan_test.get_fail_message())
-            sta_scan.sta_scan_test.exit_fail()
+        if not sta_scan.passes():
+            print(sta_scan.get_fail_message())
+            sta_scan.exit_fail()
 
-    dataframe = sta_scan.sta_scan_test.start()
-    if dataframe is not None:
-        print("we got our dataframe")
-        print(dataframe)
+    df = sta_scan.start()
 
     if (not args.use_existing_station):
-        sta_scan_test.cleanup()
+        sta_scan.cleanup()
 
-    #get_bssid_from_df(dataframe)
+    #do dataframe manipulation
+    if not isinstance(df, pd.DataFrame):
+        print("Scan results are not in dataframe form.")
+    else:
+        ssid_df = df[df["ssid"] == args.ssid]
+        if not ssid_df.empty:
+            bssid_list = []
+            for bss in ssid_df["bss"]:
+                bssid_list.append(bss)
+
+    #run create_chamberview_dut script. (WIP) This section is a bit hardcoded at the moment, 
+    # can be edited to create num ssid lines based on bssids in list, and then concat those lines to python command
+
+    ssid_line_1 = "ssid_idx=0 ssid=Dut-SSID security=WPA2 password=lanforge123 bssid=" + bssid_list[0]
+    ssid_line_2 = "ssid_idx=1 ssid=Dut-SSID-5G security=WPA2 password=lanforge123 bssid=" + bssid_list[1]
+    full_dut_command = "./create_chamberview_dut.py --lfmgr localhost -o 8080 --dut_name" + args.dut_name + "--dut_flag='DHCPD-LAN' --dut_flag='DHCPD-WAN' --ssid '" + ssid_line_1 + "' --ssid '" + ssid_line_2 + "'"
+    if (args.debug):
+        print(ssid_line_1)
+        print(ssid_line_2)
+        print(full_dut_command)
+    subprocess.run(full_dut_command, shell=True)
+
 
 if __name__ == "__main__":
     main()
