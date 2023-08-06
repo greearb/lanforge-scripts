53d0349586cd35d19073f0cd090c976ad7b813a5
Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>
2023-08-06 12:05:13 -0600

    ct_us_004_func_wc_dp_run.bash : added bash to help kick off regression test as compared using a cron job .
    Signed-off-by: Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>

---
+++ py-scripts/tools/ct_us_004_func_wc_dp_run.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# allow commands to be printed to the terminal
+set -x
+
+#echo "Running Functional Tests"
+
+echo "Running Wifi Capacity Tests"
+./lf_check.py --db_override ./tools/CT_004_WC_PERF.db --json_rig ./ct_rig_json/ct_us_004_rig.json --json_dut ./ct_dut_json/ct_004_AX88U_dut.json --json_test ./ct_tests_json/ct_us_004/ct_perf_wc/ct_perf_wc_5g_004_W1_W2_W4_W5_W6_W7.json:wc_perf_5g_W1_W2_W4_W5_W6_W7,./ct_tests_json/ct_us_004/ct_perf_wc/ct_perf_wc_2g_004_W0_W2_W4_W5_W6_W7.json:wc_perf_2g_W0_W2_W4_W5_W6_W7 --path /home/lanforge/html-reports/ct_us_004 --log_level debug
+
+echo "Running Dataplane Tests"
+./lf_check.py --db_override ./tools/CT_004_DP_PERF.db --json_rig ./ct_rig_json/ct_us_004_rig.json --json_dut ./ct_dut_json/ct_004_AX88U_dut.json --json_test ./ct_tests_json/ct_us_004/ct_perf_dp/ct_perf_dp_2g_004_W0_W2_W4_W5_W6_W7_tx.json:dp_perf_2g_W0_W2_W4_W5_W6_W7_tx,./ct_tests_json/ct_us_004/ct_perf_dp/ct_perf_dp_2g_004_W0_W2_W4_W5_W6_W7_rx.json:dp_perf_2g_W0_W2_W4_W5_W6_W7_rx,./ct_tests_json/ct_us_004/ct_perf_dp/ct_perf_dp_5g_004_W1_W2_W4_W5_W6_W7_tx.json:dp_perf_5g_W1_W2_W4_W5_W6_W7_tx,./ct_tests_json/ct_us_004/ct_perf_dp/ct_perf_dp_5g_004_W1_W2_W4_W5_W6_W7_rx.json:dp_perf_5g_W1_W2_W4_W5_W6_W7_rx --path /home/lanforge/html-reports/ct_us_004 --log_level debug
+
