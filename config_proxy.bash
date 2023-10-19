fbed17a6ab47804b950565db296571bb91cd6446
Jed Reynolds <jreynolds@candelatech.com>
2023-10-19 15:58:20 -0700

    config_proxy.bash: creates an interactive shell with proxy variables set
    Signed-off-by: Jed Reynolds <jreynolds@candelatech.com>

--- config_proxy.bash
+++ config_proxy.bash
@@ -1,2 +1,50 @@
 #!/bin/bash
+# please read the below:
+# https://unix.stackexchange.com/questions/212894/whats-the-right-format-for-the-http-proxy-environment-variable-caps-or-no-ca
+# https://about.gitlab.com/blog/2021/01/27/we-need-to-talk-no-proxy/
+#
 
+if [[ -z "${1:-}" ]]; then
+  echo "Temporarily configure your proxy environment:"
+  echo "  * Establish a shell envronment with a proxy:"
+  echo "      $0 proxy.acme.com:3128"
+  echo "    Exit the shell to go back to the previous environment."
+  echo "  * Display proxy environment variables:"
+  echo "      $0 print"
+  echo "  * Create a shell environment without proxy variables:"
+  echo "      $0 blank"
+  exit
+fi
+qq='"'
+q="'"
+#set -veu
+# never proxy local addresses
+eth0_ip=$(ip ro sh | perl -ne '/default .*? dev (\S+)/ && print "$1"' | xargs ip -br a sho | awk '{print $3}')
+eth0_ip="${eth0_ip%/*}"
+no_proxy="127.0.0.1,127.0.0.1:8080,*.localdomain,.localdomain,localhost,$eth0_ip,$eth0_ip:8080"
+#echo "$no_proxy"
+
+case "$1" in
+  print)
+    echo "Proxies:"
+    printenv | grep -i proxy
+    exit 0
+    ;;
+  blank)
+    echo "unset http_proxy https_proxy HTTP_PROXY HTTPS_PROXY ALL_PROXY PROXY_ALL" > $HOME/.blank_rc
+    echo "export no_proxy=${q}$no_proxy${q} NO_PROXY=${q}$no_proxy${q}" >> $HOME/.blank_rc
+    echo "export PS1=${qq}[no proxies] \u@\$(hostname) \$ ${qq}" >> $HOME/.blank_rc
+    bash --rcfile $HOME/.blank_rc -i
+    ;;
+  *)
+    echo "Exporting proxy environment variables set to [$1]"
+    echo "export http_proxy=${q}$1${q} https_proxy=${q}$1${q} HTTP_PROXY=${q}$1${q} HTTPS_PROXY=${q}$1${q} ALL_PROXY=${q}$1${q} PROXY_ALL=${q}$1${q}" > $HOME/.proxy_rc
+    echo "export no_proxy=${q}$no_proxy${q} NO_PROXY=${q}$no_proxy${q}" >> $HOME/.proxy_rc
+    echo "Not proxying [$no_proxy]"
+    #export http_proxy="$1" https_proxy="$1" HTTP_PROXY="$1" HTTPS_PROXY="$1" ALL_PROXY="$1" PROXY_ALL="$1"
+    #export NO_PROXY="$no_proxy"
+    printenv | grep PS
+    echo "export PS1=${qq}[+proxy] \u@\$(hostname) \$ ${qq}" >> $HOME/.proxy_rc
+    bash --rcfile $HOME/.proxy_rc -i
+    ;;
+esac
