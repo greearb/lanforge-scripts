b972166d1135b1e4a0d32599fe6dde01581905ed
Dipti Dhond <dipti.dhond@candelatech.com>
2023-10-26 01:34:57 -0700

    custom_sta_telnet.py: change some spacing, change interest and current flags, they were incorrect
    Signed-off-by: Dipti Dhond <dipti.dhond@candelatech.com>

--- py-scripts/sandbox/custom_sta_telnet.py
+++ py-scripts/sandbox/custom_sta_telnet.py
@@ -13,13 +13,13 @@ MGR="192.168.102.211"
 PORT="4001"
 RESOURCE = 1
 RADIO = "wiphy1"
-STA_NAME = "sta0000"
+STA_NAME = "sta00011"
 
 #add sta args
 SSID= 'ssid'
 KEY = 'ssid-key'
 AP = 'DEFAULT'
-MAC = 'xx:xx:xx:*:*:xx'
+MAC = '00:11:22:33:44:55'
 MODE = 12
 ADD_STA_FLAGS = 33555456
 
@@ -43,9 +43,9 @@ CUSTOM_TEXT = 'openssl_ciphers=LANFORGE-123V-456'
 
 
 ADD_STA = "add_sta 1 " + str(RESOURCE) + " " + RADIO + " " + STA_NAME + " " + str(ADD_STA_FLAGS) + " " + SSID + " NA " + KEY + " " + AP + " NA " + MAC + " " + str(MODE)
-SET_PORT= "set_port 1 " + str(RESOURCE) + " " + STA_NAME + " NA NA NA " + str(CURRENT_FLAGS) + " NA NA NA NA " + str(INTEREST_FLAGS) + " " + str(REPORT_TIMER)
+SET_PORT= "set_port 1 " + str(RESOURCE) + " " + STA_NAME + " NA NA NA NA " + str(CURRENT_FLAGS) + " " + MAC + " NA NA NA " + str(INTEREST_FLAGS) + " " + str(REPORT_TIMER)
 SET_WIFI_EXTRA = "set_wifi_extra 1 " + str(RESOURCE) + " " + STA_NAME + " " + KEY_MANAGEMENT + " " + PAIRWISE_CIPHERS + " " + GROUP_CIPHERS + " NA NA NA " +  EAP_METHODS + " " + EAP_IDENTITY + " " + EAP_ANON_IDENTITY + " NA " + PHASE_2 + " " + EAP_PASSWORD
-SET_WIFI_CUSTOM =  "set_wifi_custom 1 " + str(RESOURCE) + " " + STA_NAME + " NA" + CUSTOM_TEXT
+SET_WIFI_CUSTOM =  "set_wifi_custom 1 " + str(RESOURCE) + " " + STA_NAME + " NA " + CUSTOM_TEXT
 
 # commented out lines are for if password is needed
 # user = raw_input("Enter your remote account: ")
@@ -64,6 +64,7 @@ tn = telnetlib.Telnet(MGR,port=PORT) # Telnet(host=None, port=0[, timeout])
 #    tn.read_until("Password: ")
 #    tn.write(password + "\n")
 for cmd in [ADD_STA, SET_PORT, SET_WIFI_EXTRA, SET_WIFI_CUSTOM]:
+    #print(cmd)
     cmd_as_bytes = str.encode(cmd)
     tn.read_until(b">>")
     # tn.write(b"set_endp_tx_bounds LT-sta0000-0-BE-A 2000000" +b"\n")
