ea05329430f3ce3445d4de1ab5b606f0a6419c0b
Dipti Dhond <dipti.dhond@candelatech.com>
2023-10-25 23:38:54 -0700

    custom_sta_telnet.py: add_sta, set_port, set_wifi_extra, set_wifi_custom, over telnet to customize a station
    Signed-off-by: Dipti Dhond <dipti.dhond@candelatech.com>

---
+++ py-scripts/sandbox/custom_sta_telnet.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+
+
+# https://docs.python.org/3.8/library/telnetlib.html
+
+# This python script will create a station with customized variables. The command lines will be constructed and then sent over telnet.
+
+# main idea is to provide easy way for user to automate changing station via python
+# import getpass
+import telnetlib
+
+MGR="192.168.102.211"
+PORT="4001"
+RESOURCE = 1
+RADIO = "wiphy1"
+STA_NAME = "sta0000"
+
+#add sta args
+SSID= 'ssid'
+KEY = 'ssid-key'
+AP = 'DEFAULT'
+MAC = 'xx:xx:xx:*:*:xx'
+MODE = 12
+ADD_STA_FLAGS = 33555456
+
+#set port
+CURRENT_FLAGS = 2147483648
+INTEREST_FLAGS = 67158018
+REPORT_TIMER = 1000
+
+#set wifi extra
+KEY_MANAGEMENT = "WPA-EAP"
+PAIRWISE_CIPHERS = "DEFAULT"
+GROUP_CIPHERS = "DEFAULT"
+EAP_METHODS = "EAP-TTLS"
+EAP_IDENTITY = "sm4"
+EAP_ANON_IDENTITY = "anonymous@anon.net"
+EAP_PASSWORD = "password1"
+PHASE_2 = "PHASE_2"
+
+#set wifi custom
+CUSTOM_TEXT = 'openssl_ciphers=LANFORGE-123V-456'
+
+
+ADD_STA = "add_sta 1 " + str(RESOURCE) + " " + RADIO + " " + STA_NAME + " " + str(ADD_STA_FLAGS) + " " + SSID + " NA " + KEY + " " + AP + " NA " + MAC + " " + str(MODE)
+SET_PORT= "set_port 1 " + str(RESOURCE) + " " + STA_NAME + " NA NA NA " + str(CURRENT_FLAGS) + " NA NA NA NA " + str(INTEREST_FLAGS) + " " + str(REPORT_TIMER)
+SET_WIFI_EXTRA = "set_wifi_extra 1 " + str(RESOURCE) + " " + STA_NAME + " " + KEY_MANAGEMENT + " " + PAIRWISE_CIPHERS + " " + GROUP_CIPHERS + " NA NA NA " +  EAP_METHODS + " " + EAP_IDENTITY + " " + EAP_ANON_IDENTITY + " NA " + PHASE_2 + " " + EAP_PASSWORD
+SET_WIFI_CUSTOM =  "set_wifi_custom 1 " + str(RESOURCE) + " " + STA_NAME + " NA" + CUSTOM_TEXT
+
+# commented out lines are for if password is needed
+# user = raw_input("Enter your remote account: ")
+# password = getpass.getpass()
+
+tn = telnetlib.Telnet(MGR,port=PORT) # Telnet(host=None, port=0[, timeout])
+
+# sample commands
+#CMD=b"set_endp_tx_bounds LT-sta0000-0-BE-A 200000 0"
+#cmd=b'set_endp_tx_bounds LT-sta0000-0-BE-A 0 300000'
+
+# reading login information
+# tn.read_until("login: ")xx:xx:xx:*:*:xx
+# tn.write(user + "\n")
+# if password:
+#    tn.read_until("Password: ")
+#    tn.write(password + "\n")
+for cmd in [ADD_STA, SET_PORT, SET_WIFI_EXTRA, SET_WIFI_CUSTOM]:
+    cmd_as_bytes = str.encode(cmd)
+    tn.read_until(b">>")
+    # tn.write(b"set_endp_tx_bounds LT-sta0000-0-BE-A 2000000" +b"\n")
+    tn.write(cmd_as_bytes +b"\n")
+    #tn.write(b"set_endp_tx_bounds LT-sta0000-0-BE-A 3000000" +b"\n")
+tn.write(b"exit\n")
+
+print(tn.read_all().decode('ascii'))
