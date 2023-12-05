d221b4352a7b48e5052e77c84e31f5ca492b0c51
Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>
2023-12-05 12:44:36 -0700

    ct_us_004_func_wc_dp_run.bash run 2g wc first and 5g second to match 001
    Signed-off-by: Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>

--- py-scripts/tools/ct_us_004_func_wc_dp_run.bash
+++ py-scripts/tools/ct_us_004_func_wc_dp_run.bash
@@ -8,7 +8,7 @@ set -x
 
 
 echo "Running Wifi Capacity Tests"
-./lf_check.py --db_override ./tools/CT_004_WC_PERF_START_AUG_2023.db --json_rig ./ct_rig_json/ct_us_004_rig.json --json_dut ./ct_dut_json/ct_004_AX88U_dut.json --json_test ./ct_tests_json/ct_us_004/ct_perf_wc/ct_perf_wc_5g_004_W1_W2_W4_W5_W6_W7.json:wc_perf_5g_W1_W2_W4_W5_W6_W7,./ct_tests_json/ct_us_004/ct_perf_wc/ct_perf_wc_2g_004_W0_W2_W4_W5_W6_W7.json:wc_perf_2g_W0_W2_W4_W5_W6_W7 --path /home/lanforge/html-reports/ct_us_004 --log_level debug
+./lf_check.py --db_override ./tools/CT_004_WC_PERF_START_AUG_2023.db --json_rig ./ct_rig_json/ct_us_004_rig.json --json_dut ./ct_dut_json/ct_004_AX88U_dut.json --json_test ./ct_tests_json/ct_us_004/ct_perf_wc/ct_perf_wc_2g_004_W0_W2_W4_W5_W6_W7.json:wc_perf_2g_W0_W2_W4_W5_W6_W7,./ct_tests_json/ct_us_004/ct_perf_wc/ct_perf_wc_5g_004_W1_W2_W4_W5_W6_W7.json:wc_perf_5g_W1_W2_W4_W5_W6_W7 --path /home/lanforge/html-reports/ct_us_004 --log_level debug
 
 echo "Running Dataplane Tests"
 ./lf_check.py --db_override ./tools/CT_004_DP_PERF_START_AUG_2023.db --json_rig ./ct_rig_json/ct_us_004_rig.json --json_dut ./ct_dut_json/ct_004_AX88U_dut.json --json_test ./ct_tests_json/ct_us_004/ct_perf_dp/ct_perf_dp_2g_004_W0_W2_W4_W5_W6_W7_rx.json:dp_perf_2g_W0_W2_W4_W5_W6_W7_rx,./ct_tests_json/ct_us_004/ct_perf_dp/ct_perf_dp_2g_004_W0_W2_W4_W5_W6_W7_tx.json:dp_perf_2g_W0_W2_W4_W5_W6_W7_tx,./ct_tests_json/ct_us_004/ct_perf_dp/ct_perf_dp_5g_004_W1_W2_W4_W5_W6_W7_rx.json:dp_perf_5g_W1_W2_W4_W5_W6_W7_rx,./ct_tests_json/ct_us_004/ct_perf_dp/ct_perf_dp_5g_004_W1_W2_W4_W5_W6_W7_tx.json:dp_perf_5g_W1_W2_W4_W5_W6_W7_tx --path /home/lanforge/html-reports/ct_us_004 --log_level debug
