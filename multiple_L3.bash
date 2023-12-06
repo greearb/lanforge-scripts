c6bf152f5d6897081ca190bc21e4b6d5546627f8
Jed Reynolds <jed@bitratchet.com>
2023-12-06 15:21:08 -0800

    multiple_L3.bash: monitors connections with monitor_cx.py
    Signed-off-by: Jed Reynolds <jed@bitratchet.com>

--- multiple_L3.bash
+++ multiple_L3.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -veux
+set -eu
 # Create multiple L3 cross-connects, monitor them for a while and exit
 KBPS=1000
 MBPS=1000000
@@ -8,6 +8,7 @@ GBPS=1000000000
 DBG=''
 # test prefix must be a single word, no punctuation or spaces
 TEST_PREFIX="spiderman"
+TEST_DURATION_SEC=20
 GUI="localhost"
 if [[ ! -z "${1:-}" ]]; then
     GUI="$1"
@@ -94,9 +95,11 @@ for i in $(seq 0 $LAST_STA_IDX); do
         --no_pre_cleanup --no_cleanup
     ./raw_cli.py --mgr "$GUI" --cmd nc_show_endpoints --arg "endpoint all"
 done
+CX_NAMES=()
 for i in $(seq 0 $LAST_STA_IDX); do
     short_sta="${STATIONS[$i]}"
     short_sta="${short_sta##*.}-0" # need to turn 1.1.sta0000 to sta0000-0
+    CX_NAMES+=("${TEST_PREFIX}_${i}-${short_sta}")
     ./testgroup.py --mgr "$GUI" $DBG \
         --group_name    "$TEST_PREFIX" \
         --add_cx        "${TEST_PREFIX}_${i}-${short_sta}" \
@@ -104,12 +107,59 @@ for i in $(seq 0 $LAST_STA_IDX); do
 done
 ./raw_cli.py --mgr "$GUI" --cmd show_group --arg "group $TEST_PREFIX"
 sleep 1
+
 ./testgroup.py --mgr "$GUI" $DBG \
-    --use_existing \
-    --group_name $TEST_PREFIX \
-    --start_group $TEST_PREFIX
-sleep 10
+    --group_name    $TEST_PREFIX \
+    --start_group   $TEST_PREFIX \
+    --use_existing
+# connections don't all start at once, so if we don't pause and refresh
+# then there's a good chance that monitor_cx.py is going to not see any
+# running endpoints
+sleep 1
+./raw_cli.py --mgr "$GUI" --cmd nc_show_endpoints --arg "endpoint all"
+
+# ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- #
+# Monitor Connections
+# ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- #
+comma_str="${CX_NAMES[@]}"
+comma_str="${comma_str// /,}"
+# Be careful, this filename gets overwritten!
+./monitor_cx.py --mgr "$GUI" \
+    --csv_file      "$HOME/report-data/${TEST_PREFIX}.csv" \
+    --cx_names      "$comma_str" \
+    --quit          all_cx_stopped \
+    --log_level     DEBUG \
+    &
+monitor_pid=$!
+
+countdown=$TEST_DURATION_SEC
+while (( $countdown >= 0)); do
+    echo -n "."
+    sleep 1
+    if kill -0 "$monitor_pid"; then
+        countdown=$(( countdown - 1))
+    else # \process has exited
+        remaining_sec=$(( $TEST_DURATION_SEC - $countdown))
+        if (( $remaining_sec > 0 )); then
+            echo "monitor_py exited $remaining_sec seconds early."
+        fi
+        countdown=0
+    fi
+done
+
+
+# ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- #
+#   Stop connections
+# ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- #
+echo ""
+echo "Quiescing connections..."
 ./testgroup.py --mgr "$GUI" \
-    --use_existing \
-    --group_name $TEST_PREFIX \
-    --quiesce_group $TEST_PREFIX
+    --group_name    $TEST_PREFIX \
+    --quiesce_group $TEST_PREFIX \
+    --use_existing
+
+sleep 3 # assumes 3sec quiesce time
+
+# wait for monitor_cx.py in background
+wait
+echo "Done with test. CSV results at ~/report-data/${TEST_PREFIX}.csv"
