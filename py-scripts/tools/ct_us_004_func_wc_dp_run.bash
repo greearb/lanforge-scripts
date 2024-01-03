51483fc10da362dcf2615382d4679c42b744df1b
Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>
2024-01-02 21:22:16 -0700

    ct_us_004_func_wc_dp_run.bash added --new_test_run
    Signed-off-by: Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>

--- py-scripts/tools/ct_us_004_func_wc_dp_run.bash
+++ py-scripts/tools/ct_us_004_func_wc_dp_run.bash
@@ -4,7 +4,7 @@
 set -x
 
 #echo "Running Functional Tests"
-./lf_check.py --db_override ./tools/CT_004_FUNCTIONAL_START_AUG_2023.db --json_rig ./ct_rig_json/ct_us_004_rig.json --json_dut ./ct_dut_json/ct_004_AX88U_dut.json --json_test ./ct_tests_json/ct_us_004/ct_functional/ct_functional.json:funct_tests  --path /home/lanforge/html-reports/ct_us_004 --log_level debug
+./lf_check.py --db_override ./tools/CT_004_FUNCTIONAL_START_AUG_2023.db --json_rig ./ct_rig_json/ct_us_004_rig.json --json_dut ./ct_dut_json/ct_004_AX88U_dut.json --json_test ./ct_tests_json/ct_us_004/ct_functional/ct_functional.json:funct_tests  --path /home/lanforge/html-reports/ct_us_004 --log_level debug --new_test_run
 
 
 echo "Running Wifi Capacity Tests"
