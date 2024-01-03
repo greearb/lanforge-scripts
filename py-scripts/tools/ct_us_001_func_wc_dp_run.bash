1497c27081c6af959743d8cf456412ce6b4c07d0
Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>
2024-01-02 21:21:26 -0700

    ct_us_001_func_wc_dp_run.bash : added new_test_run flag
    Signed-off-by: Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>

--- py-scripts/tools/ct_us_001_func_wc_dp_run.bash
+++ py-scripts/tools/ct_us_001_func_wc_dp_run.bash
@@ -4,7 +4,7 @@
 set -x
 
 echo "Running Functional Tests"
-./lf_check.py --db_override ./tools/CT_001_FUNCTIONALSTART_AUG_2023.db --json_rig ./ct_rig_json/ct_us_001_rig.json --json_dut ./ct_dut_json/ct_001_AX88U_dut.json --json_test ./ct_tests_json/ct_us_001/ct_functional/ct_functional.json:funct_tests  --path /home/lanforge/html-reports/ct_us_001 --log_level debug
+./lf_check.py --db_override ./tools/CT_001_FUNCTIONALSTART_AUG_2023.db --json_rig ./ct_rig_json/ct_us_001_rig.json --json_dut ./ct_dut_json/ct_001_AX88U_dut.json --json_test ./ct_tests_json/ct_us_001/ct_functional/ct_functional.json:funct_tests  --path /home/lanforge/html-reports/ct_us_001 --log_level debug --new_test_run
 
 echo "Running Wifi Capacity Tests"
 ./lf_check.py --db_override ./tools/CT_001_WC_PERF_START_AUG_2023.db --json_rig ./ct_rig_json/ct_us_001_rig.json --json_dut ./ct_dut_json/ct_001_AX88U_dut.json --json_test ./ct_tests_json/ct_us_001/ct_perf_wc/ct_perf_wc_2g_001_W0_W2_W4_W5_W7.json:wc_perf_2g_W0_W2_W4_W5_W7,./ct_tests_json/ct_us_001/ct_perf_wc/ct_perf_wc_5g_001_W1_W2_W4_W5_W7.json:wc_perf_5g_W1_W2_W4_W5_W7 --path /home/lanforge/html-reports/ct_us_001 --log_level debug
