db51491278e24927a5d925f348d3ba5c30e0f755
Alex Gavin <alex.gavin@candelatech.com>
2024-03-28 15:55:19 -0700

    lf_wifi_capacity_test.py: Fully remove InfluxDB code and comments
    Verified:
        ./lf_wifi_capacity_test.py \
            --mgr                   192.168.100.110 \
            --instance_name         wct-instance \
            --config_name           test_con \
            --upstream              1.1.eth2 \
            --test_rig              Example-Testbed \
            --pull_report \
            --local_lf_report_dir   /tmp/WiFiCapacity_reports \
            --raw_lines_file        WiFi_Capacity.cfg
    
    Signed-off-by: Alex Gavin <alex.gavin@candelatech.com>

--- py-scripts/lf_wifi_capacity_test.py
+++ py-scripts/lf_wifi_capacity_test.py
@@ -19,11 +19,7 @@ example 2:
 --instance_name wct_instance --config_name wifi_config --upstream 1.1.eth1 --batch_size 1 --loop_iter 1 \
 --protocol UDP-IPv4 --duration 6000 --pull_report --stations 1.1.sta0000,1.1.sta0001 \
 --create_stations --radio wiphy0 --ssid test-ssid --security open --paswd [BLANK] \
---test_rig Testbed-01 -test_tag TAG\
---influx_host c7-graphana --influx_port 8086 --influx_org Candela \
---influx_token=-u_Wd-L8o992701QF0c5UmqEp7w7Z7YOMaWLxOMgmHfATJGnQbbmYyNxHBR9PgD6taM_tcxqJl6U8DjU1xINFQ== \
---influx_bucket ben \
---influx_tag testbed Ferndale-01
+--test_rig Testbed-01 -test_tag TAG
 
 example 3:
 ./lf_wifi_capacity_test.py --mgr 192.168.200.165 --upstream 1.1.eth1 --batch_size 1,5 --protocol UDP-IPv4 --duration 30000
@@ -43,7 +39,6 @@ To Run this script gui should be opened with
 
  This is a test file which will run a wifi capacity test.
     ex. on how to run this script (if stations are available in lanforge):
-    The influx part can be skipped if you are not using influx/graphana.
 
     ./lf_wifi_capacity_test.py --mgr localhost --port 8080 --lf_user lanforge --lf_password lanforge \
              --instance_name this_inst --config_name test_con --upstream 1.1.eth2 --batch_size 1,5,25,50,100 --loop_iter 1 \
@@ -396,8 +391,6 @@ class WiFiCapacityTest(cv_test):
                  raw_lines=None,
                  raw_lines_file="",
                  sets=None,
-                 influx_host="localhost",
-                 influx_port=8086,
                  report_dir="",
                  graph_groups=None,
                  test_rig="",
@@ -446,8 +439,6 @@ class WiFiCapacityTest(cv_test):
         self.raw_lines = raw_lines
         self.raw_lines_file = raw_lines_file
         self.sets = sets
-        self.influx_host = influx_host,
-        self.influx_port = influx_port
         self.report_dir = report_dir
         self.graph_groups = graph_groups
         self.test_rig = test_rig
@@ -597,11 +588,7 @@ example 2:
 --instance_name wct_instance --config_name wifi_config --upstream 1.1.eth1 --batch_size 1 --loop_iter 1 \
 --protocol UDP-IPv4 --duration 6000 --pull_report --stations 1.1.sta0000,1.1.sta0001 \
 --create_stations --radio wiphy0 --ssid test-ssid --security open --paswd [BLANK] \
---test_rig Testbed-01 -test_tag TAG\
---influx_host c7-graphana --influx_port 8086 --influx_org Candela \
---influx_token=-u_Wd-L8o992701QF0c5UmqEp7w7Z7YOMaWLxOMgmHfATJGnQbbmYyNxHBR9PgD6taM_tcxqJl6U8DjU1xINFQ== \
---influx_bucket ben \
---influx_tag testbed Ferndale-01
+--test_rig Testbed-01 -test_tag TAG
 
 example 3:
 ./lf_wifi_capacity_test.py --mgr 192.168.200.165 --upstream 1.1.eth1 --batch_size 1,5 --protocol UDP-IPv4 --duration 30000
@@ -622,7 +609,6 @@ To Run this script gui should be opened with
 
 This is a test file which will run a wifi capacity test.
     ex. on how to run this script (if stations are available in lanforge):
-    The influx part can be skipped if you are not using influx/graphana.
 
     ./lf_wifi_capacity_test.py --mgr localhost --port 8080 --lf_user lanforge --lf_password lanforge \
              --instance_name this_inst --config_name test_con --upstream 1.1.eth2 --batch_size 1,5,25,50,100 --loop_iter 1 \
