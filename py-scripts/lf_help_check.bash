0b983b7910c3104df1a095562642569b9a69025d
Alex Gavin <alex.gavin@candelatech.com>
2024-08-07 23:08:33 -0700

    lf_help_check.bash: Update to exit non-zero on any failure
    Signed-off-by: Alex Gavin <alex.gavin@candelatech.com>

--- py-scripts/lf_help_check.bash
+++ py-scripts/lf_help_check.bash
@@ -1,8 +1,21 @@
 #!/bin/bash
 
+ANY_FAILED=0
 FILES=`ls *.py`
 for FILE in $FILES
 do
 	echo $FILE
-	(timeout 10 env DISPLAY=:1 python3 ./${FILE} --help > /dev/null  && echo PASSED) || echo "ERROR: FAILED ${FILE}"
+	timeout 10 env DISPLAY=:1 python3 ./${FILE} --help > /dev/null
+	if [[ $? -eq 0 ]]; then
+		echo "PASSED"
+	else
+		echo "ERROR: FAILED ${FILE}"
+		ANY_FAILED=1
+	fi
 done
+
+if [[ $ANY_FAILED -eq 1 ]]; then
+	exit 1
+else
+	exit 0
+fi
