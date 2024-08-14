06a6ebc80402524e007ccbcceb1a26e68a22aa1e
Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>
2024-08-14 00:06:12 -0600

    ct_us_007_dp_run.bash : removed test that was not part of the test suite
    Signed-off-by: Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>

--- py-scripts/tools/ct_us_007_dp_run.bash
+++ py-scripts/tools/ct_us_007_dp_run.bash
@@ -5,7 +5,7 @@ set -x
 
 echo "Running RVR tests"
 ./lf_check.py --db_override ./tools/CT_US_007_DP_PERF.db --json_rig ./ct_rig_json/ct_us_007_rig.json --json_dut ./ct_dut_json/ct_TPLINK_CE22_10_Gbps_eth3.json \
---json_test ./ct_tests_json/ct_us_007/ct_perf_dp/ct_perf_dp_2g_007_rx.json:rvr_perf_2g_mt7915e_W0_rx,\
+--json_test \
 ./ct_tests_json/ct_us_007/ct_perf_dp/ct_perf_dp_2g_W6_W10_UCP_tx.json:ct_perf_dp_2g_W6_W10_UCP_tx,\
 ./ct_tests_json/ct_us_007/ct_perf_dp/ct_perf_dp_5g_W6_W10_UCP_tx.json:ct_perf_dp_5g_W6_W10_UCP_tx,\
 ./ct_tests_json/ct_us_007/ct_perf_dp/ct_perf_dp_6g_W6_W10_UCP_tx.json:ct_perf_dp_6g_W6_W10_UCP_tx,\
