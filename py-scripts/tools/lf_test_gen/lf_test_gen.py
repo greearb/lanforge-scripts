523ddcca370f0cad1a4a82cb5787d884dbda9b77
Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>
2024-08-21 00:10:34 -0600

    lf_test_gen.py : pass in the dut_tab for 2g, 5g, and 6g combo boxes
    Signed-off-by: Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>

--- py-scripts/tools/lf_test_gen/lf_test_gen.py
+++ py-scripts/tools/lf_test_gen/lf_test_gen.py
@@ -256,7 +256,7 @@ class json_gen_gui():
         #
         # ------------------------------------------------------------------------------------
 
-        lf_dut_tab.lf_create_dut_tab(self.dut_tab, self.window_tooltip, self.current_working_directory) 
+        self.lf_dut_tab = lf_dut_tab.lf_create_dut_tab(self.dut_tab, self.window_tooltip, self.current_working_directory) 
 
         #self.lf_dut_tab_frame = tkinter.Frame(self.dut_tab)
         #self.lf_dut_tab_frame.pack()
@@ -629,7 +629,7 @@ or deselect to remove from the test json''')
         #  Create Wifi Capacity
         #
         # ------------------------------------------------------------------------------------
-        lf_wc_tab.lf_create_wc_tab(self.wc_tab, self.window_tooltip, self.current_working_directory) 
+        lf_wc_tab.lf_create_wc_tab(self.wc_tab, self.lf_dut_tab, self.window_tooltip, self.current_working_directory) 
 
         #self.lf_wc_tab_frame = tkinter.Frame(self.wc_tab)
         #self.lf_wc_tab_frame.pack()
