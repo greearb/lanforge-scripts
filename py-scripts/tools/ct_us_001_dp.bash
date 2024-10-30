9bb79f3808f9934a3f759055df3cddc58570cad9
Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>
2024-10-30 11:35:56 -0600

    ct_us_001_dp.bash , ct_us_001_func.bash , ct_us_001_wc.bash updating the device under test.
    Signed-off-by: Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>

--- py-scripts/tools/ct_us_001_dp.bash
+++ py-scripts/tools/ct_us_001_dp.bash
@@ -7,7 +7,7 @@ echo "Running Dataplane Tests"
 ./lf_check.py \
 --db_override ./tools/CT_001_DP_PERF_AUG_2023.db \
 --json_rig ./ct_rig_json/ct_us_001_rig.json \
---json_dut ./ct_dut_json/ct_001_AX88U_dut.json \
+--json_dut ./ct_dut_json/ct_001_AXE16000_dut.json \
 --json_test \
 ./ct_tests_json/ct_us_001/ct_perf_dp/ct_perf_dp_2g_001_W0_W2_W4_W5_W7_rx.json:dp_perf_2g_W0_W2_W4_W5_W7_rx,\
 ./ct_tests_json/ct_us_001/ct_perf_dp/ct_perf_dp_2g_001_W0_W2_W4_W5_W7_tx.json:dp_perf_2g_W0_W2_W4_W5_W7_tx,\
--- py-scripts/tools/ct_us_001_wc.bash
+++ py-scripts/tools/ct_us_001_wc.bash
@@ -6,7 +6,7 @@ echo "Running Wifi Capacity Tests"
 ./lf_check.py \
 --db_override ./tools/CT_001_WC_PERF_START_AUG_2023.db \
 --json_rig ./ct_rig_json/ct_us_001_rig.json \
---json_dut ./ct_dut_json/ct_001_AX88U_dut.json \
+--json_dut ./ct_dut_json/ct_001_AXE16000_dut.json \
 --json_test \
 ./ct_tests_json/ct_us_001/ct_perf_wc/ct_perf_wc_2g_001_W0_W2_W4_W5_W7.json:wc_perf_2g_W0_W2_W4_W5_W7,\
 ./ct_tests_json/ct_us_001/ct_perf_wc/ct_perf_wc_5g_001_W1_W2_W4_W5_W7.json:wc_perf_5g_W1_W2_W4_W5_W7 \
