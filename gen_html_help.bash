4ade93aeb5e566d94ff7e5d802b23c79dabb032d
Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>
2024-12-23 12:04:33 -0700

    gen_html_help.bash added lf_create_vap_cv.py
    Verified:
    ctlocal scripts_ug.php
    
    Signed-off-by: Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>

--- gen_html_help.bash
+++ gen_html_help.bash
@@ -25,6 +25,7 @@ scripts=(
     py-scripts/create_l3_stations.py
     py-scripts/create_l4.py
     py-scripts/create_vap.py
+    py-scripts/lf_create_vap_cv.py
     py-scripts/lf_dataplane_test.py
     py-scripts/lf_rvr_test.py
     py-scripts/lf_wifi_capacity_test.py
