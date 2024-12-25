fbb6db83764d1dbf9f897d7da6afd481669ba8bb
Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>
2024-12-24 17:56:55 -0700

    gen_html_help.bash added py-scripts/update_dependencies.py
    Verified:
    ctlocal scripts_ug.php
    
    Signed-off-by: Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>

--- gen_html_help.bash
+++ gen_html_help.bash
@@ -46,6 +46,7 @@ scripts=(
     py-scripts/sta_connect2.py
     py-scripts/test_l3.py
     py-scripts/test_l3_longevity.py
+    py-scripts/update_dependencies.py
 )
 ./gen_html_help.pl "${scripts[@]}" > $DESTF
 echo "...created $DESTF"
