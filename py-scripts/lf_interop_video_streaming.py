85aedc68e4300897c3e5b3e46d6a2904f1d1c4f8
Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>
2024-12-26 12:22:19 -0700

    py-scripts/lf_interop_video_streaming.py modified help_summary to action="store_true"
    Verified:
    ./lf_interop_video_streaming.py --help_summary
    
    Signed-off-by: Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>

--- py-scripts/lf_interop_video_streaming.py
+++ py-scripts/lf_interop_video_streaming.py
@@ -1076,7 +1076,7 @@ def main():
     parser.add_argument('--no_laptops', help="--to not use laptops", action = 'store_false')
     parser.add_argument('--postcleanup', help="Cleanup the cross connections after test is stopped", action = 'store_true')
     parser.add_argument('--precleanup', help="Cleanup the cross connections before test is started", action = 'store_true')
-    parser.add_argument('--help_summary', help='Show summary of what this script does', default=None)
+    parser.add_argument('--help_summary', help='Show summary of what this script does', action="store_true")
     args = parser.parse_args()
 
     if args.help_summary:
