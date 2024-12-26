09d8ccb7d894b1f72e63f532f17961eb7e50c4b0
Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>
2024-12-26 16:52:39 -0700

    gen_html_help.bash added py-scripts/lf_kpi_csv.py
    Verified:
    ./lf_kpi_csv.py --help_summary
    
    Signed-off-by: Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>

--- gen_html_help.bash
+++ gen_html_help.bash
@@ -45,6 +45,7 @@ scripts=(
     py-scripts/lf_interop_real_browser_test.py
     py-scripts/lf_interop_throughput.py
     py-scripts/lf_json_api.py
+    py-scripts/lf_kpi_csv.py
     py-scripts/lf_rfgen_info.py
     py-scripts/lf_rvr_test.py
     py-scripts/lf_sniff_radio.py
