64f35a8cc7f66bed09ddc456442e30d1ea6ed880
Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>
2024-12-26 09:47:23 -0700

    py-scripts/lf_rfgen_info.py removed flake8: noqa and updated script to be flake8 complient
    Verified:
    flake8
    ./lf_rfgen_info.py --mgr 192.168.0.31
    
    Signed-off-by: Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>

--- py-scripts/lf_rfgen_info.py
+++ py-scripts/lf_rfgen_info.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-# flake8: noqa
 
 '''
 NAME:
@@ -32,31 +31,33 @@ import importlib
 logger = logging.getLogger(__name__)
 lf_logger_config = importlib.import_module("lf_logger_config")
 
+
 class lf_rfgen_info():
     def __init__(self,
-                _mgr='localhost',
-                _port='4001'):
+                 _mgr='localhost',
+                 _port='4001'):
         self.mgr = _mgr
         self.port = _port
         self.rfgen_info = ''
 
     def get_rfgen_info(self):
-        tn = telnetlib.Telnet(self.mgr,port=self.port) # Telnet(host=None, port=0[, timeout])
+        tn = telnetlib.Telnet(self.mgr, port=self.port)  # Telnet(host=None, port=0[, timeout])
 
         # command to read all
-        CMD=b'show_rfgen'
+        CMD = b'show_rfgen'
 
         tn.read_until(b">>")
-        tn.write(CMD +b"\n")
+        tn.write(CMD + b"\n")
         tn.write(b"exit\n")
 
-        self.rfgen_info=tn.read_all().decode('ascii')
+        self.rfgen_info = tn.read_all().decode('ascii')
 
         logger.info(self.rfgen_info)
         return self.rfgen_info
 
+
 def main():
-    help_summary = f'''\
+    help_summary = '''\
 This script will read the configuration settings of the RF-Generators connected to a LANforge.
 '''
 
@@ -97,37 +98,29 @@ NOTES:
     parser.add_argument('--log_level',
                         default=None,
                         help='Set logging level: debug | info | warning | error | critical')
-    
     # help summary
     parser.add_argument('--help_summary',
                         default=None,
                         action="store_true",
                         help='Show summary of what this script does')
 
-
-    
     args = parser.parse_args()
 
     if args.help_summary:
         print(help_summary)
         exit(0)
 
-
     logger_config = lf_logger_config.lf_logger_config()
 
     if args.log_level:
         logger_config.set_level(level=args.log_level)
 
-    rfgen = lf_rfgen_info(_mgr=args.mgr,
-                            _port=args.port)
+    rfgen = lf_rfgen_info(_mgr=args.mgr, _port=args.port)
 
     rf_info = rfgen.get_rfgen_info()
 
     logger.info(rf_info)
 
 
-
-
 if __name__ == '__main__':
     main()
-
