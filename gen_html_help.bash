388068c2580bc457f241986d952b15887f21291f
Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>
2024-12-27 09:55:31 -0700

    gen_html_help.bash added lf_test_generic.py
    Verified:
    ctlocal ./scripts_ug.php
    
    Signed-off-by: Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>

--- gen_html_help.bash
+++ gen_html_help.bash
@@ -50,6 +50,7 @@ scripts=(
     py-scripts/lf_rfgen_info.py
     py-scripts/lf_rvr_test.py
     py-scripts/lf_sniff_radio.py
+    py-scripts/lf_test_generic.py
     py-scripts/lf_tr398v4_test.py
     py-scripts/lf_wifi_capacity_test.py
     py-scripts/raw_cli.py
