31b9c64f9900d51c491d4e70e01435b1a7d0b8cf
Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>
2024-12-28 11:29:08 -0700

    gen_html_help.bash addes py-scripts/sta_scan_test.py
    Verified
    ctlocal scripts_ug.php
    
    Signed-off-by: Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>

--- gen_html_help.bash
+++ gen_html_help.bash
@@ -68,6 +68,7 @@ scripts=(
     py-scripts/rvr_scenario.py
     py-scripts/sta_connect2.py
     py-scripts/sta_connect_multi_example.py
+    py-scripts/sta_scan_test.py
     py-scripts/test_l3.py
     py-scripts/test_l3_longevity.py
     py-scripts/update_dependencies.py
