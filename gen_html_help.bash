e7f34461c747003e8e4e3035bb0d383ab58ae0fc
Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>
2024-12-27 17:35:25 -0700

    gen_html_help.bash added py-scripts/bssid_to_dut.py
    Verified:
    ./bssid_to_dut.py --help_summary
    
    Signed-off-by: Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>

--- gen_html_help.bash
+++ gen_html_help.bash
@@ -18,6 +18,7 @@ fi
 #DESTF=/var/www/html/greearb/lf/scripts_ug.php
 
 scripts=(
+    py-scripts/bssid_to_dut.py
     py-scripts/chamber_ctl.py
     py-scripts/create_bond.py
     py-scripts/create_bridge.py
