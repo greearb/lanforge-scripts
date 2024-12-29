b36c18223781ee303cc78b1e2390e3532dea9548
Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>
2024-12-29 11:52:53 -0700

    gen_html_help.bash py-scripts/lf_snp_test.py
    Verified:
    ctlocal ./scripts_ug.php
    
    Signed-off-by: Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>

--- gen_html_help.bash
+++ gen_html_help.bash
@@ -55,6 +55,7 @@ scripts=(
     py-scripts/lf_rfgen_info.py
     py-scripts/lf_rvr_test.py
     py-scripts/lf_sniff_radio.py
+    py-scripts/lf_snp_test.py
     py-scripts/lf_test_generic.py
     py-scripts/lf_tr398_test.py
     py-scripts/lf_tr398v2_test.py
