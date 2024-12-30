a538a134302377434c7db578c85fcc1c0b87fc9b
Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>
2024-12-30 13:23:50 -0700

    gen_html_help.bash added py-scripts/lf_macvlan.py
    Verified:
    ctlocal ./scripts/ug.php
    
    Signed-off-by: Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>

--- gen_html_help.bash
+++ gen_html_help.bash
@@ -67,6 +67,7 @@ scripts=(
     py-scripts/lf_json_util.py
     py-scripts/lf_kpi_csv.py
     py-scripts/lf_logger_config.py
+    py-scripts/lf_macvlan.py
     py-scripts/lf_mesh_test.py
     py-scripts/lf_pcap.py
     py-scripts/lf_rfgen_info.py
