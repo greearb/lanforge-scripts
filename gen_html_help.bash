2c5e5440c1afc4847a26620f3c5162e149d1fd61
Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>
2024-12-31 09:39:17 -0700

    gen_html_help.bash added py-scripts/modify_vap.py
    Verified:
    ctlocal ./scripts_ug.php
    
    Signed-off-by: Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>

--- gen_html_help.bash
+++ gen_html_help.bash
@@ -95,6 +95,7 @@ scripts=(
     py-scripts/lf_wifi_capacity_test.py
     py-scripts/measure_station_time_up.py
     py-scripts/modify_station.py
+    py-scripts/modify_vap.py
     py-scripts/monitor_cx.py
     py-scripts/raw_cli.py
     py-scripts/run_cv_scenario.py
