eb4b0f353d673f3923adda50a20d5351acb1af71
Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>
2024-12-30 18:17:55 -0700

    gen_html_help.bash added py-scripts/lf_setup_radius_server.py
    Verified:
    ctlocal ./scripts_ug.php
    
    Signed-off-by: Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>

--- gen_html_help.bash
+++ gen_html_help.bash
@@ -82,6 +82,7 @@ scripts=(
     py-scripts/lf_rssi_process.py
     py-scripts/lf_rvr_test.py
     py-scripts/lf_rx_sensitivity_test.py
+    py-scripts/lf_setup_radius_server.py
     py-scripts/lf_sniff_radio.py
     py-scripts/lf_snp_test.py
     py-scripts/lf_test_generic.py
