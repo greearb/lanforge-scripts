b2a5cf5719a72f46eeb48acc3b8cb6baa8b51064
Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>
2025-01-28 15:56:10 -0700

    007 mt7921 is replaced by mt7925e_W6 *.json  and *.bash
    Signed-off-by: Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>

--- py-scripts/tools/ct_us_007_rvr_run.bash
+++ py-scripts/tools/ct_us_007_rvr_run.bash
@@ -11,30 +11,28 @@ echo "Running RVR tests"
 --json_test \
 ./ct_tests_json/ct_us_007/ct_perf_rvr/ct_perf_rvr_2g_007_rx.json:rvr_perf_2g_mt7915e_W0_rx,\
 ./ct_tests_json/ct_us_007/ct_perf_rvr/ct_perf_rvr_2g_007_rx.json:rvr_perf_2g_ax210_W2_rx,\
-./ct_tests_json/ct_us_007/ct_perf_rvr/ct_perf_rvr_2g_007_rx.json:rvr_perf_2g_mt7921_W6_rx,\
+./ct_tests_json/ct_us_007/ct_perf_rvr/ct_perf_rvr_2g_007_rx.json:rvr_perf_2g_mt7925e_W6_rx,\
 ./ct_tests_json/ct_us_007/ct_perf_rvr/ct_perf_rvr_2g_007_rx.json:rvr_perf_2g_mt7922_W7_rx,\
 ./ct_tests_json/ct_us_007/ct_perf_rvr/ct_perf_rvr_5g_007_rx.json:rvr_perf_5g_1_mt7915e_W0_rx,\
 ./ct_tests_json/ct_us_007/ct_perf_rvr/ct_perf_rvr_5g_007_rx.json:rvr_perf_5g_1_ax210_W2_rx,\
-./ct_tests_json/ct_us_007/ct_perf_rvr/ct_perf_rvr_5g_007_rx.json:rvr_perf_5g_1_mt7921_W6_rx,\
+./ct_tests_json/ct_us_007/ct_perf_rvr/ct_perf_rvr_5g_007_rx.json:rvr_perf_5g_1_mt7925e_W6_rx,\
 ./ct_tests_json/ct_us_007/ct_perf_rvr/ct_perf_rvr_5g_007_rx.json:rvr_perf_5g_1_mt7922_W7_rx,\
 ./ct_tests_json/ct_us_007/ct_perf_rvr/ct_perf_rvr_6g_007_rx.json:rvr_perf_6g_ax210_W2_rx,\
-./ct_tests_json/ct_us_007/ct_perf_rvr/ct_perf_rvr_6g_007_rx.json:rvr_perf_6g_mt7921_W6_rx,\
+./ct_tests_json/ct_us_007/ct_perf_rvr/ct_perf_rvr_6g_007_rx.json:rvr_perf_6g_mt7925e_W6_rx,\
 ./ct_tests_json/ct_us_007/ct_perf_rvr/ct_perf_rvr_6g_007_rx.json:rvr_perf_6g_mt7922_W7_rx,\
 ./ct_tests_json/ct_us_007/ct_perf_rvr/ct_perf_rvr_2g_5g_6g_007_rx.json:rvr_perf_2g_5g_6g_ax210_W2_W3_W4_rx,\
 ./ct_tests_json/ct_us_007/ct_perf_rvr/ct_perf_rvr_2g_007_tx.json:rvr_perf_2g_mt7915e_W0_tx,\
 ./ct_tests_json/ct_us_007/ct_perf_rvr/ct_perf_rvr_2g_007_tx.json:rvr_perf_2g_ax210_W2_tx,\
-./ct_tests_json/ct_us_007/ct_perf_rvr/ct_perf_rvr_2g_007_tx.json:rvr_perf_2g_mt7921_W6_tx,\
+./ct_tests_json/ct_us_007/ct_perf_rvr/ct_perf_rvr_2g_007_tx.json:rvr_perf_2g_mt7925e_W6_tx,\
 ./ct_tests_json/ct_us_007/ct_perf_rvr/ct_perf_rvr_2g_007_tx.json:rvr_perf_2g_mt7922_W7_tx,\
 ./ct_tests_json/ct_us_007/ct_perf_rvr/ct_perf_rvr_5g_007_tx.json:rvr_perf_5g_1_mt7915e_W0_tx,\
 ./ct_tests_json/ct_us_007/ct_perf_rvr/ct_perf_rvr_5g_007_tx.json:rvr_perf_5g_1_ax210_W2_tx,\
-./ct_tests_json/ct_us_007/ct_perf_rvr/ct_perf_rvr_5g_007_tx.json:rvr_perf_5g_1_mt7921_W6_tx,\
+./ct_tests_json/ct_us_007/ct_perf_rvr/ct_perf_rvr_5g_007_tx.json:rvr_perf_5g_1_mt7925e_W6_tx,\
 ./ct_tests_json/ct_us_007/ct_perf_rvr/ct_perf_rvr_5g_007_tx.json:rvr_perf_5g_1_mt7922_W7_tx,\
 ./ct_tests_json/ct_us_007/ct_perf_rvr/ct_perf_rvr_6g_007_tx.json:rvr_perf_6g_ax210_W2_tx,\
-./ct_tests_json/ct_us_007/ct_perf_rvr/ct_perf_rvr_6g_007_tx.json:rvr_perf_6g_mt7921_W6_tx,\
+./ct_tests_json/ct_us_007/ct_perf_rvr/ct_perf_rvr_6g_007_tx.json:rvr_perf_6g_mt7925e_W6_tx,\
 ./ct_tests_json/ct_us_007/ct_perf_rvr/ct_perf_rvr_6g_007_tx.json:rvr_perf_6g_mt7922_W7_tx,\
 ./ct_tests_json/ct_us_007/ct_perf_rvr/ct_perf_rvr_2g_5g_6g_007_tx.json:rvr_perf_2g_5g_6g_ax210_W2_W3_W4_tx \
 --path /home/lanforge/html-reports/ct_us_007 \
 --log_level debug \
 --production
-
-
