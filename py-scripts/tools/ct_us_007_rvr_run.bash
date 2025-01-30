26aa5ca4199cc3cf075163b198d22bcb1eb8e182
Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>
2025-01-30 12:03:37 -0700

    py-scripts/tools/ct_us_007_rvr_run.bash fixed editing in bash script
    Signed-off-by: Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>

--- py-scripts/tools/ct_us_007_rvr_run.bash
+++ py-scripts/tools/ct_us_007_rvr_run.bash
@@ -22,7 +22,7 @@ echo "Running RVR tests"
 ./ct_tests_json/ct_us_007/ct_perf_rvr/ct_perf_rvr_6g_007_rx.json:rvr_perf_6g_ax210_W2_rx,\
 ./ct_tests_json/ct_us_007/ct_perf_rvr/ct_perf_rvr_6g_007_rx.json:rvr_perf_6g_mt7925e_W6_rx,\
 ./ct_tests_json/ct_us_007/ct_perf_rvr/ct_perf_rvr_6g_007_rx.json:rvr_perf_6g_mt7922_W7_rx,\
-./ct_tests_json/ct_us_007/ct_perf_rvr/ct_perf_rvr_6g_007_rx.json:,ct_perf_rvr_6g_be200_W10_UDP_rx\
+./ct_tests_json/ct_us_007/ct_perf_rvr/ct_perf_rvr_6g_007_rx.json:ct_perf_rvr_6g_be200_W10_UDP_rx,\
 ./ct_tests_json/ct_us_007/ct_perf_rvr/ct_perf_rvr_2g_5g_6g_007_rx.json:rvr_perf_2g_5g_6g_ax210_W2_W3_W4_rx,\
 ./ct_tests_json/ct_us_007/ct_perf_rvr/ct_perf_rvr_2g_007_tx.json:rvr_perf_2g_mt7915e_W0_tx,\
 ./ct_tests_json/ct_us_007/ct_perf_rvr/ct_perf_rvr_2g_007_tx.json:rvr_perf_2g_ax210_W2_tx,\
