f3660ffe744d0dd625307a56e93012803fbc340d
Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>
2025-02-13 16:23:34 -0700

    py-scripts/tools/lf_test_gen/lf_create_radio_frame.py
    Verify:
    ./lf_test_gen.py --radio_count 66
    
    Signed-off-by: Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>

--- py-scripts/tools/lf_test_gen/lf_create_radio_frame.py
+++ py-scripts/tools/lf_test_gen/lf_create_radio_frame.py
@@ -39,7 +39,7 @@ class lf_create_radio_frame():
         self.lf_radio_frame.grid(row=0, column=0, sticky="news", padx=20, pady=10)
 
         # 4. Create a Canvas and Scrollbar
-        self.radio_canvas = tkinter.Canvas(self.lf_radio_frame, width=1200, height=360)
+        self.radio_canvas = tkinter.Canvas(self.lf_radio_frame, width=1400, height=360)
         self.radio_scrollbar = tkinter.Scrollbar(self.lf_radio_frame, orient='vertical', command=self.radio_canvas.yview)
         self.radio_canvas.configure(yscrollcommand=self.radio_scrollbar.set)
 
@@ -78,26 +78,29 @@ class lf_create_radio_frame():
         self.radio_label = tkinter.Label(self.radio_content_frame, text="Radio")
         self.radio_label.grid(row=1, column=1)
 
-        self.radio_type_label = tkinter.Label(self.radio_content_frame, text="Radio Type")
+        self.radio_type_label = tkinter.Label(self.radio_content_frame, text="Radio Port")
         self.radio_type_label.grid(row=1, column=2)
 
-        self.radio_type_label = tkinter.Label(self.radio_content_frame, text="Radio Model")
+        self.radio_type_label = tkinter.Label(self.radio_content_frame, text="Radio Type")
         self.radio_type_label.grid(row=1, column=3)
 
-        self.radio_type_label = tkinter.Label(self.radio_content_frame, text="Radio Max Sta")
+        self.radio_type_label = tkinter.Label(self.radio_content_frame, text="Radio Model")
         self.radio_type_label.grid(row=1, column=4)
 
-        self.radio_type_label = tkinter.Label(self.radio_content_frame, text="Radio Batch")
+        self.radio_type_label = tkinter.Label(self.radio_content_frame, text="Radio Max Sta")
         self.radio_type_label.grid(row=1, column=5)
 
+        self.radio_type_label = tkinter.Label(self.radio_content_frame, text="Radio Batch")
+        self.radio_type_label.grid(row=1, column=6)
+
         self.radio_2g_label = tkinter.Label(self.radio_content_frame, text="2g")
-        self.radio_2g_label.grid(row=1, column=6)
+        self.radio_2g_label.grid(row=1, column=7)
 
         self.radio_5g_label = tkinter.Label(self.radio_content_frame, text="5g")
-        self.radio_5g_label.grid(row=1, column=7)
+        self.radio_5g_label.grid(row=1, column=8)
 
         self.radio_6g_label = tkinter.Label(self.radio_content_frame, text="6g")
-        self.radio_6g_label.grid(row=1, column=8)
+        self.radio_6g_label.grid(row=1, column=9)
 
         # Capabilities
         # https://stackoverflow.com/questions/28736028/python-tkinter-reference-in-comboboxes-created-in-for-loop
@@ -106,6 +109,7 @@ class lf_create_radio_frame():
 
         self.use_radio_var_dict = {}
         self.radio_dict = {}
+        self.radio_port_dict = {}
         self.radio_type_dict = {}
         self.radio_model_dict = {}
         self.radio_max_sta_dict = {}
@@ -139,38 +143,47 @@ class lf_create_radio_frame():
             radio_entry.grid(row=radio + 2, column=1)
             self.window_tooltip.bind(radio_entry, 'Enter LANforge radio shelf.resource.radio, example: 1.1.wiphy0')
 
+            radio_port_entry_var = tkinter.StringVar()
+            self.radio_port_dict[radio] = radio_port_entry_var
+            radio_port_entry_var.set("")
+            radio_port_entry = tkinter.Entry(self.radio_content_frame, textvariable=self.radio_port_dict[radio])
+            radio_port_entry.grid(row=radio + 2, column=2)
+            self.window_tooltip.bind(radio_port_entry, 'Enter Radio Port, numberic value')
+
             radio_type_entry_var = tkinter.StringVar()
             self.radio_type_dict[radio] = radio_type_entry_var
             radio_type_entry_var.set("")
             radio_type_entry = tkinter.Entry(self.radio_content_frame, textvariable=self.radio_type_dict[radio])
-            radio_type_entry.grid(row=radio + 2, column=2)
+            radio_type_entry.grid(row=radio + 2, column=3)
             self.window_tooltip.bind(radio_type_entry, 'Enter Radio Type, example: 802.11abgn-AX')
 
             radio_model_entry_var = tkinter.StringVar()
             self.radio_model_dict[radio] = radio_model_entry_var
             radio_model_entry_var.set("")
             radio_model_entry = tkinter.Entry(self.radio_content_frame, textvariable=self.radio_model_dict[radio])
-            radio_model_entry.grid(row=radio + 2, column=3)
+            radio_model_entry.grid(row=radio + 2, column=4)
             self.window_tooltip.bind(radio_model_entry, 'Enter Radio Model Type, example: AX210')
 
             radio_max_sta_entry_var = tkinter.StringVar()
             self.radio_max_sta_dict[radio] = radio_max_sta_entry_var
             radio_max_sta_entry_var.set("")
             radio_max_sta_entry = tkinter.Entry(self.radio_content_frame, textvariable=self.radio_max_sta_dict[radio])
-            radio_max_sta_entry.grid(row=radio + 2, column=4)
+            radio_max_sta_entry.grid(row=radio + 2, column=5)
             self.window_tooltip.bind(radio_max_sta_entry, 'Enter Radio Maximum Stations supported')
 
             radio_batch_entry_var = tkinter.StringVar()
             self.radio_batch_dict[radio] = radio_batch_entry_var
             radio_batch_entry_var.set("")
             radio_batch_entry = tkinter.Entry(self.radio_content_frame, textvariable=self.radio_batch_dict[radio])
-            radio_batch_entry.grid(row=radio + 2, column=5)
+            radio_batch_entry.grid(row=radio + 2, column=6)
+            self.window_tooltip.bind(radio_batch_entry, 'Enter Radio Maximum Batch supported')
+
 
             use_radio_2g_var = tkinter.StringVar(value="Do Not Use")
             self.use_radio_2g_var_dict[radio] = use_radio_2g_var
             use_radio_2g_check = tkinter.Checkbutton(self.radio_content_frame, text="2g", variable=self.use_radio_2g_var_dict[radio],
                                                      onvalue="Use", offvalue="Do Not Use")
-            use_radio_2g_check.grid(row=radio + 2, column=6)
+            use_radio_2g_check.grid(row=radio + 2, column=7)
             self.window_tooltip.bind(use_radio_2g_check, '''Read Radio info will select 2g band as capability if applicable
 The check box may be selected to allow the band to be included in the test json
 or deselect to remove from the test json''')
@@ -179,7 +192,7 @@ or deselect to remove from the test json''')
             self.use_radio_5g_var_dict[radio] = use_radio_5g_var
             use_radio_5g_check = tkinter.Checkbutton(self.radio_content_frame, text="5g", variable=self.use_radio_5g_var_dict[radio],
                                                      onvalue="Use", offvalue="Do Not Use")
-            use_radio_5g_check.grid(row=radio + 2, column=7)
+            use_radio_5g_check.grid(row=radio + 2, column=8)
             self.window_tooltip.bind(use_radio_5g_check, '''Read Radio info will select 5g band as capability if applicable
 The check box may be selected to allow the band to be included in the test json
 or deselect to remove from the test json''')
@@ -188,7 +201,7 @@ or deselect to remove from the test json''')
             self.use_radio_6g_var_dict[radio] = use_radio_6g_var
             use_radio_6g_check = tkinter.Checkbutton(self.radio_content_frame, text="6g", variable=self.use_radio_6g_var_dict[radio],
                                                      onvalue="Use", offvalue="Do Not Use")
-            use_radio_6g_check.grid(row=radio + 2, column=8)
+            use_radio_6g_check.grid(row=radio + 2, column=9)
             self.window_tooltip.bind(use_radio_6g_check, '''Read Radio info will select 6g band as capability if applicable
 The check box may be selected to allow the band to be included in the test json
 or deselect to remove from the test json''')
@@ -249,6 +262,7 @@ or deselect to remove from the test json''')
         for key in self.lanforge_radio_json:
             if 'wiphy' in key:
                 self.radio_dict[radio].set(self.lanforge_radio_json[key]['entity id'])
+                self.radio_port_dict[radio].set(self.lanforge_radio_json[key]['port'])
                 self.radio_type_dict[radio].set(self.lanforge_radio_json[key]['capabilities'])
                 radio_name_tmp = self.lanforge_radio_json[key]['driver'].split('Driver:', maxsplit=1)[-1].split('Bus:', maxsplit=1)[0]
                 self.radio_max_sta_dict[radio].set(self.lanforge_radio_json[key]['max_sta'])
@@ -344,6 +358,7 @@ or deselect to remove from the test json''')
 
             self.use_radio_var_dict[radio].set("Do Not Use")
             self.radio_dict[radio].set("")
+            self.radio_port_dict[radio].set("")
             self.radio_type_dict[radio].set("")
             self.radio_model_dict[radio].set("")
             self.radio_max_sta_dict[radio].set("")
