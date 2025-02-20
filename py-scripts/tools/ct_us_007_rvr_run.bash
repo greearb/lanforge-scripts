5ab2dadb8212888dcb21fd5db826604ece31b505
Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>
2025-02-20 12:40:32 -0700

    py-scripts/tools/ct_us_007_rvr_run.bash updated to add new test
    Signed-off-by: Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>

--- py-scripts/tools/ct_us_007_rvr_run.bash
+++ py-scripts/tools/ct_us_007_rvr_run.bash
@@ -37,7 +37,7 @@ echo "Running RVR tests"
 ./ct_tests_json/ct_us_007/ct_perf_rvr/ct_perf_rvr_6g_007_tx.json:rvr_perf_6g_ax210_W2_tx,\
 ./ct_tests_json/ct_us_007/ct_perf_rvr/ct_perf_rvr_6g_007_tx.json:rvr_perf_6g_mt7925e_W6_tx,\
 ./ct_tests_json/ct_us_007/ct_perf_rvr/ct_perf_rvr_6g_007_tx.json:rvr_perf_6g_mt7922_W7_tx,\
-./ct_tests_json/ct_us_007/ct_perf_rvr/ct_perf_rvr_6g_007_tx.json:ct_perf_rvr_5g_be200_W10_UDP_tx,\
+./ct_tests_json/ct_us_007/ct_perf_rvr/ct_perf_rvr_6g_007_tx.json:ct_perf_rvr_6g_be200_W10_UDP_tx,\
 ./ct_tests_json/ct_us_007/ct_perf_rvr/ct_perf_rvr_2g_5g_6g_007_tx.json:rvr_perf_2g_5g_6g_ax210_W2_W3_W4_tx \
 --path /home/lanforge/html-reports/ct_us_007 \
 --log_level debug \
