ed5c43d0811d4b98fe512b3eef7d5bce7aa8db47
Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>
2025-03-07 17:53:18 -0700

    py-scripts/tools/ct_us_005_ap_auto.bash initial commit
    Signed-off-by: Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>

---
+++ py-scripts/tools/ct_us_005_ap_auto.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# allow commands to be printed to the terminal
+
+echo "Running ap_auto_basic_cx"
+./ct_us_005_ap_auto_basic_cx.bash
+
+echo "Running ap_auto_long_term"
+./ct_us_005_ap_auto_long_term.bash
