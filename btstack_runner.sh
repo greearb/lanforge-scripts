6d45136e8354b73ca50cef0354592cb60f72c43c
Carson Vandegriffe <carson.vandegriffe@candelatech.com>
2025-04-16 09:33:06 -0700

    btstack_runner.sh: Make argument specification more readable, add events-file argument.

--- btstack_runner.sh
+++ btstack_runner.sh
@@ -1,8 +1,44 @@
 #! /usr/bin/bash
-# example usage: btstack 02 f8:e3:22:b2:2c
+##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### #####
+## btstack_runner.sh                                                       ##
+##                                                                         ##
+## Use this script to start a Bluetooth Keyboard connection,               ##
+##     via btstack operating on a USB dongle.                              ##
+##                                                                         ##
+##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### #####
 
-echo "arg1: selected USB adapter path: $1"
-echo "arg2: target device's BT MAC addr: $2"
+usage="$0 -u 01-02 -d f8:e3:22:b2:2c -e /home/lanforge/vr_conf/mgt_pipe_1_helper
+ -u: usb path of bluetooth dongle
+ -d: bluetooth mac of target device
+ -e: file for writing out important btstack events
+"
+
+OPTIONAL_ARGS=''
+
+while getopts ":d:e:u:" opts; do
+    case "$opts" in
+        d) TARGET_DEVICE="$OPTARG";;
+        e) EVENTS_FNAME="$OPTARG";;
+        u) USB_PATH="$OPTARG";;
+        *) echo "Unknown Option [$opts]"
+    esac
+done
+
+[ -z "$TARGET_DEVICE" ] && {
+    echo -e "-d: TARGET DEVICE required\n"
+    echo "$usage"
+    exit 1
+}
+
+[ -z "$USB_PATH" ] && {
+    echo -e "-u USB PATH of controlling BT dongle required\n"
+    echo "$usage"
+    exit 1
+}
+
+if [ -n "$EVENTS_FNAME" ]; then
+    OPTIONAL_ARGS+="-e $EVENTS_FNAME"
+fi
 
 if [ ! -d "/home/lanforge/btstack/" ]; then
     mkdir -p /home/lanforge/btstack
@@ -10,7 +46,7 @@ fi
 
 CHILD_PID=''
 #setup PIDF removal on-exit
-PIDFNAME=/home/lanforge/btstack/btstack-$2.pid
+PIDFNAME="./btstack/btstack-$TARGET_DEVICE.pid"
 on_exit() {
     kill -s SIGINT $CHILD_PID
     rm $PIDFNAME
@@ -24,8 +60,9 @@ echo $$ > $PIDFNAME
 for (( ; ; ))
 do
     # start btstack
-    echo "starting btstack w/ cmd: btstack -u $1 -d $2"
-    (btstack -u $1 -d $2) &
+    CMD="btstack -u $USB_PATH -d $TARGET_DEVICE $OPTIONAL_ARGS"
+    echo "starting btstack w/ cmd: $CMD"
+    ($CMD) &
     CHILD_PID=$!
     wait $!
 
