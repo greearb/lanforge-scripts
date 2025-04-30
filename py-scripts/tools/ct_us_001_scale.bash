92bfb27a7542ad5715dfeb1a167da3ac336ed59a
Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>
2025-04-30 17:40:53 -0600

    py-scripts/tools/ct_us_001_scale.bash
    initial commit
    
    Signed-off-by: Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>

---
+++ py-scripts/tools/ct_us_001_scale.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# allow commands to be printed to the terminal
+#
+set -x
+
+echo "Running Dataplane Tests"
+./lf_check.py \
+--db_override ./tools/CT_001_SCALE_2025.db \
+--json_dut ./ct_dut_json/ct_001_AXE16000_dut.json \
+--json_rig ./ct_rig_json/ct_us_001_rig.json \
+--json_test \
+./ct_tests_json/ct_us_001/ct_scale/ct_scale_001.json:ct_scale_001 \
+--path /home/lanforge/html-reports/ct_us_001 \
+--log_level debug \
+--production
