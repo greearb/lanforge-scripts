0121184e82fb4f051d1ff1a8eb987900e41fbdc9
Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>
2025-05-22 18:45:18 -0600

    py-scripts/toos/ct_tests_json/ct_us_002/ct_port_reset ct_port_reset_2g_5g_6g_002.json port_reset_002.txt ct_us_002_cv.bash
    port reset test automation
    
    Signed-off-by: Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>

---
+++ py-scripts/tools/ct_us_002_cv.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# allow commands to be printed to the terminal
+set -x
+
+echo "Running Port Reset"
+./lf_check.py \
+--db_override ./tools/CT_002_CV_MAY_2025.db \
+--json_rig ./ct_rig_json/ct_us_002_rig.json \
+--json_dut ./ct_dut_json/ct_002_AX16K_dut.json \
+--json_test \
+./ct_tests_json/ct_us_002/ct_port_reset/ct_port_reset_2g_5g_6g_002.json:cv_tests \
+--path /home/lanforge/html-reports/ct_us_002 \
+--log_level debug \
+--new_test_run
+# --production
