c7827445dc25b8a269537115fca831f95ea905f3
Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>
2025-05-23 12:06:05 -0600

    py-scripts/tools/ct_us_002_cv.bash : update syntax
    Verified via Automation:
    
    Signed-off-by: Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>

--- py-scripts/tools/ct_us_002_cv.bash
+++ py-scripts/tools/ct_us_002_cv.bash
@@ -10,7 +10,7 @@ echo "Running Port Reset"
 --json_rig ./ct_rig_json/ct_us_002_rig.json \
 --json_dut ./ct_dut_json/ct_002_AX16K_dut.json \
 --json_test \
-./ct_tests_json/ct_us_002/ct_port_reset/ct_port_reset_2g_5g_6g_002.json:cv_tests, \
+./ct_tests_json/ct_us_002/ct_port_reset/ct_port_reset_2g_5g_6g_002.json:cv_tests,\
 ./ct_tests_json/ct_us_002/ct_scale/ct_scale_002.json:ct_scale \
 --path /home/lanforge/html-reports/ct_us_002 \
 --log_level debug \
