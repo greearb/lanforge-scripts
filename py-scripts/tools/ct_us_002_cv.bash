5995d6836299b59ed8e6963f849042f5d67d6f2b
Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>
2025-05-23 12:02:13 -0600

    py-scripts/tools/ct_us_002_cv.bash added scale to the ct_us_002_cv.bash
    Signed-off-by: Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>

--- py-scripts/tools/ct_us_002_cv.bash
+++ py-scripts/tools/ct_us_002_cv.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # allow commands to be printed to the terminal
+
 set -x
 
 echo "Running Port Reset"
@@ -9,7 +10,8 @@ echo "Running Port Reset"
 --json_rig ./ct_rig_json/ct_us_002_rig.json \
 --json_dut ./ct_dut_json/ct_002_AX16K_dut.json \
 --json_test \
-./ct_tests_json/ct_us_002/ct_port_reset/ct_port_reset_2g_5g_6g_002.json:cv_tests \
+./ct_tests_json/ct_us_002/ct_port_reset/ct_port_reset_2g_5g_6g_002.json:cv_tests, \
+./ct_tests_json/ct_us_002/ct_scale/ct_scale_002.json:ct_scale \
 --path /home/lanforge/html-reports/ct_us_002 \
 --log_level debug \
 --new_test_run
