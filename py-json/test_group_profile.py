3d61f707e3e9799ffbacba5e8bd4b4c1d3b22840
Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>
2025-06-28 13:02:34 -0600

    py-json/test_group_profile.py flake8 compliance without individual line exceptions.
    Verification:
    ./testgroup.py\
     --mgr 192.168.50.103\
     --num_stations 2\
     --ssid ax88u_5g\
     --passwd lf_ax88u_5g\
     --security wpa2\
     --radio wiphy1\
     --add_group\
     --group_name group1\
     --start_group group1
    
    Signed-off-by: Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>

--- py-json/test_group_profile.py
+++ py-json/test_group_profile.py
@@ -11,7 +11,7 @@ LFCliBase = lfcli_base.LFCliBase
 
 
 class TestGroupProfile(LFCliBase):
-    def __init__(self, lfclient_host, lfclient_port, local_realm, test_group_name=None, debug_=False):  # noqa: 501
+    def __init__(self, lfclient_host, lfclient_port, local_realm, test_group_name=None, debug_=False):
         super().__init__(lfclient_host, lfclient_port, debug_)
         self.local_realm = local_realm
         self.group_name = test_group_name
@@ -19,44 +19,44 @@ class TestGroupProfile(LFCliBase):
 
     def start_group(self):
         if self.group_name is not None:
-            self.local_realm.json_post("/cli-json/start_group", {"name": self.group_name})  # noqa: 501
-            self.local_realm.json_post("/cli-json/start_group", {"name": self.group_name})  # noqa: 501
+            self.local_realm.json_post("/cli-json/start_group", {"name": self.group_name})
+            self.local_realm.json_post("/cli-json/start_group", {"name": self.group_name})
         else:
             raise ValueError("test_group name must be set.")
 
     def quiesce_group(self):
         if self.group_name is not None:
-            self.local_realm.json_post("/cli-json/quiesce_group", {"name": self.group_name})  # noqa: 501
+            self.local_realm.json_post("/cli-json/quiesce_group", {"name": self.group_name})
         else:
             raise ValueError("test_group name must be set.")
 
     def stop_group(self):
         if self.group_name is not None:
-            self.local_realm.json_post("/cli-json/stop_group", {"name": self.group_name})  # noqa: 501
+            self.local_realm.json_post("/cli-json/stop_group", {"name": self.group_name})
         else:
             raise ValueError("test_group name must be set.")
 
     def create_group(self):
         if self.group_name is not None:
-            self.local_realm.json_post("/cli-json/add_group", {"name": self.group_name})  # noqa: 501
+            self.local_realm.json_post("/cli-json/add_group", {"name": self.group_name})
         else:
             raise ValueError("test_group name must be set.")
 
     def rm_group(self):
         if self.group_name is not None:
-            self.local_realm.json_post("/cli-json/rm_group", {"name": self.group_name})  # noqa: 501
+            self.local_realm.json_post("/cli-json/rm_group", {"name": self.group_name})
         else:
             raise ValueError("test_group name must be set.")
 
     def add_cx(self, cx_name):
-        self.local_realm.json_post("/cli-json/add_tgcx", {"tgname": self.group_name, "cxname": cx_name})  # noqa: 501
+        self.local_realm.json_post("/cli-json/add_tgcx", {"tgname": self.group_name, "cxname": cx_name})
 
     def rm_cx(self, cx_name):
-        self.local_realm.json_post("/cli-json/rm_tgcx", {"tgname": self.group_name, "cxname": cx_name})  # noqa: 501
+        self.local_realm.json_post("/cli-json/rm_tgcx", {"tgname": self.group_name, "cxname": cx_name})
 
     def check_group_exists(self):
         test_groups = self.local_realm.json_get("/testgroups/all")
-        # This breaks on only one existing group, this expects an array not a single object  # noqa: 501
+        # This breaks on only one existing group, this expects an array not a single object
         if test_groups is not None and "groups" in test_groups:
             test_groups = test_groups["groups"]
             for group in test_groups:
