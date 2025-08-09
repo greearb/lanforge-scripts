22844fec661a8e94b55e04af3445b81826e4b433
Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>
2025-08-09 08:29:13 -0600

    py-scripts/tools/ct_us_009_wc_BE800.bash initial commit of wifi capacity tests for ct_us_009
    Signed-off-by: Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>

---
+++ py-scripts/tools/ct_us_009_wc_BE800.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# allow commands to be printed to the terminal
+set -x
+
+echo "Running Wifi Capacity tests"
+./lf_check.py \
+--db_override ./tools/CT_US_009_WC_PERF_BE800.db \
+--json_rig ./ct_rig_json/ct_us_009_TP_LINK_BE800_rig.json \
+--json_dut ./ct_dut_json/ct_009_TP_LINK_BE800_dut.json \
+--json_test \
+./ct_tests_json/ct_us_009/ct_perf_wc/ct_perf_wc_2g_W0_W1_W2_W3_W4_W5_W6_W7_W8_DL_UL_TCP_UDP.json:ct_perf_wc_2g_W0_W1_W2_W3_W4_W5_W6_W7_W8_DL_UL_TCP_UDP,\
+./ct_tests_json/ct_us_009/ct_perf_wc/ct_perf_wc_6g_W0_W1_W2_W3_W4_W5_W6_W7_W10_DL_UL_TCP_UDP.json:ct_perf_wc_5g_W0_W1_W2_W3_W4_W5_W6_W7_W9_DL_UL_TCP_UDP,\
+./ct_tests_json/ct_us_009/ct_perf_wc/ct_perf_wc_6g_W0_W1_W2_W3_W4_W5_W6_W7_W10_DL_UL_TCP_UDP.json:ct_perf_wc_6g_W0_W1_W2_W3_W4_W5_W6_W7_W10_DL_UL_TCP_UDP,\
+--path /home/lanforge/html-reports/ct_us_009 \
+--log_level debug
