7e69a2a95400066d3c436d7edf20031933d53821
Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>
2025-08-09 08:59:26 -0600

    py-scripts/tools/ct_us_009_rvr_BE800.bash initial commit for ct_us_009 rvr BE800.bash
    Signed-off-by: Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>

---
+++ py-scripts/tools/ct_us_009_rvr_BE800.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# allow commands to be printed to the terminal
+set -x
+
+echo "Running Rate vs Range tests"
+./lf_check.py \
+--db_override ./tools/ct_us_009_RVR_PERF_BE19000.db \
+--json_rig ./ct_rig_json/ct_us_009_rig_TP_BE19000.json \
+--json_dut ./ct_dut_json/ct_008_TP_BE19000_dut.json \
+--json_test \
+./ct_tests_json/ct_us_009/ct_perf_rvr/ct_perf_rvr_2g_TP_LINK_W0_W1_W2_W3_W4_W5_W6_W7_W8_UDP_tx.json:ct_perf_rvr_2g_W0_W1_W2_W3_W4_W5_W6_W7_W8_UDP_tx,\
+./ct_tests_json/ct_us_009/ct_perf_rvr/ct_perf_rvr_5g_TP_LINK_W0_W1_W2_W3_W4_W5_W6_W7_W9_UDP_tx.json:ct_perf_rvr_5g_W0_W1_W2_W3_W4_W5_W6_W7_W9_UDP_tx,\
+./ct_tests_json/ct_us_009/ct_perf_rvr/ct_perf_rvr_6g_TP_LINK_W0_W1_W2_W3_W4_W5_W6_W7_W10_UDP_tx.json:ct_perf_rvr_6g_W0_W1_W2_W3_W4_W5_W6_W7_W10_UDP_tx,\
+./ct_tests_json/ct_us_009/ct_perf_rvr/ct_perf_rvr_2g_TP_LINK_W0_W1_W2_W3_W4_W5_W6_W7_W8_UDP_rx.json:ct_perf_rvr_2g_W0_W1_W2_W3_W4_W5_W6_W7_W8_UDP_rx,\
+./ct_tests_json/ct_us_009/ct_perf_rvr/ct_perf_rvr_5g_TP_LINK_W0_W1_W2_W3_W4_W5_W6_W7_W9_UDP_rx.json:ct_perf_rvr_5g_W0_W1_W2_W3_W4_W5_W6_W7_W9_UDP_rx,\
+./ct_tests_json/ct_us_009/ct_perf_rvr/ct_perf_rvr_6g_TP_LINK_W0_W1_W2_W3_W4_W5_W6_W7_W10_UDP_rx.json:ct_perf_rvr_6g_W0_W1_W2_W3_W4_W5_W6_W7_W10_UDP_rx,\
+./ct_tests_json/ct_us_009/ct_perf_rvr/ct_perf_rvr_2g_TP_LINK_W0_W1_W2_W3_W4_W5_W6_W7_W8_TCP_tx.json:ct_perf_rvr_2g_W0_W1_W2_W3_W4_W5_W6_W7_W8_TCP_tx,\
+./ct_tests_json/ct_us_009/ct_perf_rvr/ct_perf_rvr_5g_TP_LINK_W0_W1_W2_W3_W4_W5_W6_W7_W9_TCP_tx.json:ct_perf_rvr_5g_W0_W1_W2_W3_W4_W5_W6_W7_W9_TCP_tx,\
+./ct_tests_json/ct_us_009/ct_perf_rvr/ct_perf_rvr_6g_TP_LINK_W0_W1_W2_W3_W4_W5_W6_W7_W10_TCP_tx.json:ct_perf_rvr_6g_W0_W1_W2_W3_W4_W5_W6_W7_W10_TCP_tx,\
+./ct_tests_json/ct_us_009/ct_perf_rvr/ct_perf_rvr_2g_TP_LINK_W0_W1_W2_W3_W4_W5_W6_W7_W8_TCP_rx.json:ct_perf_rvr_2g_W0_W1_W2_W3_W4_W5_W6_W7_W8_TCP_rx,\
+./ct_tests_json/ct_us_009/ct_perf_rvr/ct_perf_rvr_5g_TP_LINK_W0_W1_W2_W3_W4_W5_W6_W7_W9_TCP_rx.json:ct_perf_rvr_5g_W0_W1_W2_W3_W4_W5_W6_W7_W9_TCP_rx,\
+./ct_tests_json/ct_us_009/ct_perf_rvr/ct_perf_rvr_6g_TP_LINK_W0_W1_W2_W3_W4_W5_W6_W7_W10_TCP_rx.json:ct_perf_rvr_6g_W0_W1_W2_W3_W4_W5_W6_W7_W10_TCP_rx,\
+--path /home/lanforge/html-reports/ct_us_009 \
+--log_level debug
