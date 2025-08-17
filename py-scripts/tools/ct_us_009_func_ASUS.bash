ead5bf5baad97f1975f1fc5f3bba4a864d8bc903
Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>
2025-08-17 10:27:14 -0600

    py-scripts/tools/ct_us_009_func_ASUS.bash initial commit
    Signed-off-by: Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>

---
+++ py-scripts/tools/ct_us_009_func_ASUS.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# allow commands to be printed to the terminal
+set -x
+
+echo "Running Functional tests"
+./lf_check.py \
+--db_override ./tools/ct_us_009_FUNC_ASUS_BE96U.db \
+--json_rig ./ct_rig_json/ct_us_009_ASUS_BE96U_rig.json \
+--json_dut ./ct_dut_json/ct_009_ASUS_BE96U_dut.json \
+--json_test \
+./ct_tests_json/ct_us_009/ct_perf_functional/ASUS_BE96U/ct_test_l3.json:ct_test_l3
+--path /home/lanforge/html-reports/ct_us_009 \
+--log_level debug
