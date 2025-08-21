9bf3238eea6dbdf62c1eda5280a24e2e7c5ecb39
Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>
2025-08-21 09:41:58 -0600

    py-scripts/tools/ct_us_009_func_ASUS.bash added test_l3 test
    Signed-off-by: Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>

--- py-scripts/tools/ct_us_009_func_ASUS.bash
+++ py-scripts/tools/ct_us_009_func_ASUS.bash
@@ -9,6 +9,6 @@ echo "Running Functional tests"
 --json_rig ./ct_rig_json/ct_us_009_ASUS_BE96U_rig.json \
 --json_dut ./ct_dut_json/ct_009_ASUS_BE96U_dut.json \
 --json_test \
-./ct_tests_json/ct_us_009/ct_perf_functional/ASUS_BE96U/ct_test_l3.json:ct_test_l3
+./ct_tests_json/ct_us_009/ct_perf_functional/ASUS_BE96U/ct_test_l3.json:ct_test_l3 \
 --path /home/lanforge/html-reports/ct_us_009 \
 --log_level debug
