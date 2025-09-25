608cbadc5b7de58a6fbc4831ad04f926c0b4edce
Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>
2025-09-24 18:28:08 -0600

    py-scripts/lf_rvr_test.py changed logger.info to logger.error updated log message to be actionable.
    Verified:
    --help_summary - to check for syntax
    
    Signed-off-by: Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>

--- py-scripts/lf_rvr_test.py
+++ py-scripts/lf_rvr_test.py
@@ -549,7 +549,14 @@ def main():
     if CV_Test.kpi_results_present():
         logger.info("lf_rvr_test_test generated kpi.csv")
     else:
-        logger.info("FAILED: lf_rvr_test did not generate kpi.csv)")
+        logger.error('''\
+        The test has finished but did not complete successfully,
+        and no KPI.csv file could be generated. Possible causes
+        could be displayed by the GUI CV test, a station could
+        have the wrong SSID, passphrase or attempting to connect
+        to the wrong BSSID. Please check error messages outputted
+        earlier by this script, or check for exceptions in journalctl.
+        ''')
         exit(1)
 
 
