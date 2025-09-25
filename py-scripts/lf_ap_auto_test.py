4416814df4927288c2dbd79c42e1ba1ca2fc06b9
Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>
2025-09-24 18:25:32 -0600

    py-scripts/lf_ap_auto_test.py changed logger.info to logger.error updated log message to be actionable.
    Verified:
    --help_summary - to check for syntax
    
    Signed-off-by: Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>

--- py-scripts/lf_ap_auto_test.py
+++ py-scripts/lf_ap_auto_test.py
@@ -511,7 +511,14 @@ the options and how best to input data.
     if CV_Test.kpi_results_present():
         logger.info("lf_ap_auto_test generated kpi.csv")
     else:
-        logger.info("FAILED: lf_ap_auto_test did not generate kpi.csv)")
+        logger.error('''\
+        The test has finished but did not complete successfully,
+        and no KPI.csv file could be generated. Possible causes
+        could be displayed by the GUI CV test, a station could
+        have the wrong SSID, passphrase or attempting to connect
+        to the wrong BSSID. Please check error messages outputted
+        earlier by this script, or check for exceptions in journalctl.
+        ''')
         exit(1)
 
     if CV_Test.passes():
