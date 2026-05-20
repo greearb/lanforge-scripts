15d9dd915883a432a8c5654841b4df07601f2499
Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>
2026-05-19 19:16:35 -0600

    py-scripts/tools/ct_us_009_wc_BE800_MTK7996_BE200_STRESS.bash initial commit stress test for MTK7996 and BE200
    Signed-off-by: Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>

---
+++ py-scripts/tools/ct_us_009_wc_BE800_MTK7996_BE200_STRESS.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# allow commands to be printed to the terminal
+set -x
+
+echo "Running Wifi Capacity tests"
+./lf_check.py \
+--db_override ./tools/CT_US_009_WC_PERF_BE800.db \
+--json_rig ./ct_rig_json/ct_us_009_TP_LINK_BE800_rig.json \
+--json_dut ./ct_dut_json/ct_009_TP_LINK_BE800_dut.json \
+--json_test \
+./ct_tests_json/ct_us_009/ct_perf_wc/TP_LINK_BE800/ct_perf_wc_6g_W0_W1_W2_W3_W4_W5_W6_W7_W8_W9_W10_DL_UL_TCP_STRESS.json:ct_perf_wc_6g_W0_W1_W2_W3_W4_W5_W6_W7_W8_W9_W10_DL_UL_TCP_STRESS \
+--path /home/lanforge/html-reports/ct_us_009 \
+--log_level debug
